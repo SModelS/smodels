a947412eede5cf369ca45bc2da2cae9e27d28259
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-04-04 09:34:36 +0000

    ignores
    git-svn-id: file:///home/svnuser/SMSDecomposition@24 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- SMSdraw.py
+++ SMSdraw.py
@@ -1,14 +1,19 @@
 def draw ( top, filename="bla.pdf", elementnr=0 ):
   """ plot a lessagraph, write into pdf file called <filename> """
   from pyfeyn.user import FeynDiagram,Vertex,Point,Fermion,Scalar,CIRCLE,SQUARE,\
-    HATCHED135,Circle
+    HATCHED135,Circle,pyx
   fd = FeynDiagram()
+  f=1.0
 
-  in1  = Point(-2.2, -1.8)
-  in2  = Point(-2.2, 1.8)
-  fusion = Point(-2, 0)
-  vtx1 = Circle(0,.5, radius=0.3).setFillStyle(HATCHED135)
+  in1  = Point(-1*f, -.75*f)
+  in2  = Point(-1*f, 1.75*f)
+  vtx1 = Circle(0,.5*f, radius=0.3*f).setFillStyle(HATCHED135)
   P1a = Fermion(in1, vtx1 ).addLabel("P$_1$")
+  P1a.addParallelArrow( pos=.44,displace=.0003,length=pyx.unit.length(1.75*f), size=.0001)
+  P1a.addParallelArrow( pos=.44,displace=-.0003,length=pyx.unit.length(1.75*f), size=.0001)
+  P2a = Fermion(in2, vtx1 ).addLabel("P$_2$",displace=.3)
+  P2a.addParallelArrow( pos=.44,displace=.0003,length=pyx.unit.length(1.75*f), size=.0001)
+  P2a.addParallelArrow( pos=.44,displace=-.0003,length=pyx.unit.length(1.75*f), size=.0001) 
 
   nbranches=len(top.B)
   # print nbranches,"branches"
@@ -20,17 +25,21 @@ def draw ( top, filename="bla.pdf", elementnr=0 ):
     for ( nvtx,particles) in enumerate(branch.vertparts):
       mark=None
       if particles>0: mark=CIRCLE
-      v1=Vertex ( nvtx+1,ct,mark=mark)
+      v1=Vertex ( f*(nvtx+1),f*ct,mark=mark)
       f1 = Scalar  ( lastVertex,v1) ## .addLabel ( "x")
+      if nvtx==0:
+        b=.25
+        if ct==1: b=-.25
+        f1.bend(b)
       lastVertex=v1
       # print "particles",particles,"ct=",ct
-      y=-1 ## y of end point of SM particle
-      if ct==1: y=2
-      dx=(particles-1)*(-.5) ## delta_x 
+      y=-1.0*f ## y of end point of SM particle
+      if ct==1: y=2.*f
+      dx=(particles-1)*(-.5)*f ## delta_x 
       for i in range(particles):
-        p=Point ( nvtx + 1 +  dx + i, y )
+        p=Point ( f*(nvtx + 1 +  dx + i), f*y )
         label=branch.ElList[elementnr].particles[i].replace("jet","q")
-        f=Fermion(v1,p).addLabel ( label )
+        ff=Fermion(v1,p).addLabel ( label )
          
     #pl = Point ( nvtx+2,ct )
     #fl = Scalar ( lastVertex,pl ) ## .addLabel( "lsp" )
