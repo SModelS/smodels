9730ef61404fabbaf396340641f9d0342fdcb55c
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-04-12 13:12:06 +0000

    adding support for units
    git-svn-id: file:///home/svnuser/SMSDecomposition@75 c47c41a0-46cc-4d19-81ea-ce3a071b7174

---
+++ SMSUnits.py
@@ -0,0 +1,19 @@
+from unum import Unum
+
+# description see
+# http://home.scarlet.be/be052320/Unum.html
+# http://home.scarlet.be/be052320/docs.html
+
+# install it with:
+# sudo pip install ez_setup
+# sudo easy_install Unum
+# or sth similar
+
+fb=Unum.unit('fb')
+pb=Unum.unit('pb', 1000 * fb)
+
+eV=Unum.unit('eV')
+keV=Unum.unit('keV',10**3*eV)
+MeV=Unum.unit('MeV',10**6*eV)
+GeV=Unum.unit('GeV',10**9*eV)
+TeV=Unum.unit('TeV',10**12*eV)
