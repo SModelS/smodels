48f9596160f95ae58391a5ea44e1d4cf816ea7ea
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-04-30 10:54:31 +0000

    DoCompress, DoInvisible no globals needed
    git-svn-id: file:///home/svnuser/SMSDecomposition@115 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- SMSglobals.py
+++ SMSglobals.py
@@ -2,17 +2,16 @@
 from SMSHelpers import addunit
 
 ListOfAnalyses=[]
+DoCompress = False     #Flag to turn on/off compression
+DoInvisible = False    #Flag to turn on/off invisible compression
 
 def initglob():
     global ncomp         #Overall counter for compressed topologies
     global min_lep_pt, min_tau_pt, min_jet_pt, min_b_pt, min_pt
     global evcount,nComp,nInvis
     global minmassgap
-    global DoCompress, DoInvisible
     global DistAnalyses
 
-    DoCompress = False     #Flag to turn on/off compression
-    DoInvisible = False    #Flag to turn on/off invisible compression
     evcount = 0
     nComp = 0              #Counter for mass compressed topologies
     nInvis = 0             #Counter for invisible compressed topologies
