df85938f836cb377a30fb15266bf4d33711f3e5b
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-02 13:14:55 +0000

    checked flag
    git-svn-id: file:///home/svnuser/SMSDecomposition@124 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- SMSStatistics.py
+++ SMSStatistics.py
@@ -6,7 +6,7 @@ SMSResults.ResultsForSqrts ( 0 )
 
 All=SMSResults.getAllResults()
 
-Count={"total":0, "public":0, "haveconstraint": 0, "2012": 0, "2011": 0, "ATLAS8TeV": 0, "8TeV":0 }
+Count={"total":0, "public":0, "haveconstraint": 0, "2012": 0, "2011": 0, "ATLAS8TeV": 0, "8TeV":0, "checked": 0 }
 
 for analysis in All:
   for topo in All[analysis]:
@@ -18,6 +18,8 @@ for analysis in All:
     Count["haveconstraint"]+=1
     run=SMSResults.getRun ( analysis )
     Count[run]+=1
+    checked=SMSResults.getCheckedBy ( analysis )
+    if checked: Count["checked"]+=1
 
 
 print Count["total"],"results total ..."
@@ -28,3 +30,4 @@ print Count["2011"]+Count["2012"]+Count["8TeV"],"are CMS results"
 print Count["ATLAS8TeV"],"are ATLAS results"
 print Count["2011"]+Count["2012"],"are 7 TeV results"
 print Count["8TeV"]+Count["ATLAS8TeV"],"are 8 TeV results"
+print Count["checked"], "out of these", Count["haveconstraint"],"have been verified so far"
