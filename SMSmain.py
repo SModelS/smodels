b638f7e3f96561b6491f53f4f4edd630e3b169de
lessa <lessa@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-22 20:50:00 +0000

    2-space indent in main
    git-svn-id: file:///home/svnuser/SMSDecomposition@231 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- SMSmain.py
+++ SMSmain.py
@@ -15,13 +15,13 @@ DoFactory = True
 #PYTHIA must have MSTP(42)=0 ! no mass smearing (narrow width approximation)
 #Creat analyses list:
 if DoFactory:
-    SMSglobals.ListOfAnalyses = SMSAnalysisFactory.load()
-else:    
-    SMSglobals.ListOfAnalyses = SMSanalyses.load()
+  SMSglobals.ListOfAnalyses = SMSAnalysisFactory.load()
+else:  
+  SMSglobals.ListOfAnalyses = SMSanalyses.load()
 
 
 #Generate events and compute cross-sections:
-nevts = 100
+nevts = 10000
 #slhafile = "AndreSLHA/andrePT4.slha"
 slhafile = "AndreSLHA/DESY_stop.slha"
 Wv = SMSxsec.pytinit(nevts,slhafile,rpythia = True, donlo = True)
@@ -39,10 +39,10 @@ minmassgap = addunit(10.,'GeV')
 
 DoSLHAdec = True
 if DoSLHAdec:
-    sigmacut = addunit(0.1,'fb')  # Maximum cross-section*BR to be included
-    SMSTopList = SMSDecomp.SLHAdecomp(slhafile,Xsec,sigmacut,DoCompress,DoInvisible,minmassgap)
+  sigmacut = addunit(0.1,'fb')  # Maximum cross-section*BR to be included
+  SMSTopList = SMSDecomp.SLHAdecomp(slhafile,Xsec,sigmacut,DoCompress,DoInvisible,minmassgap)
 else:
-    SMSTopList = SMSDecomp.LHEdecomp(lhefile,W,nevts,DoCompress,DoInvisible,minmassgap)
+  SMSTopList = SMSDecomp.LHEdecomp(lhefile,W,nevts,DoCompress,DoInvisible,minmassgap)
 
 
 EvTop_table = PrettyTable(["Topology","#Vertices", "#Insertions", "#Elements", "Sum of weights"])
@@ -52,22 +52,22 @@ eltot = 0
 totweight = []
 #Print Results:
 for i in range(len(SMSTopList)):
-    weightlist = [el.weight for el in SMSTopList[i].ElList]
-    sumw = SMSmethods.sumweights(weightlist)
-    totweight.append(sumw)
-    EvTop_table.add_row([i,SMSTopList[i].vertnumb,SMSTopList[i].vertparts,len(SMSTopList[i].ElList),wrap(MyPrettyPrinter().pformat(sumw),width=30)])
-    eltot += len(SMSTopList[i].ElList)
+  weightlist = [el.weight for el in SMSTopList[i].ElList]
+  sumw = SMSmethods.sumweights(weightlist)
+  totweight.append(sumw)
+  EvTop_table.add_row([i,SMSTopList[i].vertnumb,SMSTopList[i].vertparts,len(SMSTopList[i].ElList),wrap(MyPrettyPrinter().pformat(sumw),width=30)])
+  eltot += len(SMSTopList[i].ElList)
 
  
-            
-#Print element list for Topology[i]:    
-    if i >= 0:           
-        for j in range(len(SMSTopList[i].ElList)):
-            EvElement_table.add_row([i,j,SMSTopList[i].ElList[j].B[0].particles,SMSTopList[i].ElList[j].B[1].particles,wrap(MyPrettyPrinter().pformat(SMSTopList[i].ElList[j].B[0].masses),width=25),wrap(MyPrettyPrinter().pformat(SMSTopList[i].ElList[j].B[1].masses),width=25),wrap(MyPrettyPrinter().pformat(SMSTopList[i].ElList[j].weight),width=30)])
-        EvElement_table.add_row(["---","---","---","---","---","---","---"])    
-            
-            
-print "Number of Global topologies = ",len(SMSTopList)          
+      
+#Print element list for Topology[i]:  
+  if i >= 0:       
+    for j in range(len(SMSTopList[i].ElList)):
+      EvElement_table.add_row([i,j,SMSTopList[i].ElList[j].B[0].particles,SMSTopList[i].ElList[j].B[1].particles,wrap(MyPrettyPrinter().pformat(SMSTopList[i].ElList[j].B[0].masses),width=25),wrap(MyPrettyPrinter().pformat(SMSTopList[i].ElList[j].B[1].masses),width=25),wrap(MyPrettyPrinter().pformat(SMSTopList[i].ElList[j].weight),width=30)])
+    EvElement_table.add_row(["---","---","---","---","---","---","---"])  
+      
+      
+print "Number of Global topologies = ",len(SMSTopList)      
 print(EvTop_table)
 print "Total Number of Elements = ",eltot
 print "Total weight = ",SMSmethods.sumweights(totweight)
@@ -79,24 +79,24 @@ print '\n \n \n'
 #sys.exit()
 #Add event topologies to analyses:
 for Analysis in SMSglobals.ListOfAnalyses:
-    SMSmethods.AddToAnalysis(SMSTopList,Analysis)
+  SMSmethods.AddToAnalysis(SMSTopList,Analysis)
 
 #Print analyses output:
 AnElement_table = PrettyTable(["Analyses","Element","Masses","Element Weight"])  
 
 for Ana in SMSglobals.ListOfAnalyses:
-    label = Ana.label
-    ifirst = True
-    for i in range(len(Ana.Top.ElList)):
-        ptcs = [Ana.Top.ElList[i].B[0].particles,Ana.Top.ElList[i].B[1].particles]
-        for j in range(len(Ana.Top.ElList[i].B[0].masses)):
-            mass = [Ana.Top.ElList[i].B[0].masses[j],Ana.Top.ElList[i].B[1].masses[j]]
-            if not ifirst: label = ""
-            if j != 0: ptcs = ""
-            ifirst = False
-            AnElement_table.add_row([label,ptcs,wrap(MyPrettyPrinter().pformat(mass),width=100),wrap(MyPrettyPrinter().pformat(Ana.Top.ElList[i].weight[j]),width=30)])
-    AnElement_table.add_row(["---","---","---","---"])  
-        
+  label = Ana.label
+  ifirst = True
+  for i in range(len(Ana.Top.ElList)):
+    ptcs = [Ana.Top.ElList[i].B[0].particles,Ana.Top.ElList[i].B[1].particles]
+    for j in range(len(Ana.Top.ElList[i].B[0].masses)):
+      mass = [Ana.Top.ElList[i].B[0].masses[j],Ana.Top.ElList[i].B[1].masses[j]]
+      if not ifirst: label = ""
+      if j != 0: ptcs = ""
+      ifirst = False
+      AnElement_table.add_row([label,ptcs,wrap(MyPrettyPrinter().pformat(mass),width=100),wrap(MyPrettyPrinter().pformat(Ana.Top.ElList[i].weight[j]),width=30)])
+  AnElement_table.add_row(["---","---","---","---"])  
+    
 
 #print(AnElement_table)
 
@@ -104,42 +104,42 @@ for Ana in SMSglobals.ListOfAnalyses:
 print '\n \n \n'
 #sys.exit()
 
-        
+    
 #Compute theoretical predictions to analyses results:
 for Analysis in SMSglobals.ListOfAnalyses:
-    print "---------------Analysis Label = ",Analysis.label
-    Results_table = PrettyTable(["Result","Conditions","Mass","Theoretical Value","Experimental Limits"])
-    
-    
-    if max(Analysis.Top.vertnumb) > 3:
-        print "Skipping analysis",Analysis.label,"with more than 3 vertices"
-        continue
+  print "---------------Analysis Label = ",Analysis.label
+  Results_table = PrettyTable(["Result","Conditions","Mass","Theoretical Value","Experimental Limits"])
+  
+  
+  if max(Analysis.Top.vertnumb) > 3:
+    print "Skipping analysis",Analysis.label,"with more than 3 vertices"
+    continue
 
 
-    for res in Analysis.results.keys():
-        theoRes = SMSmethods.EvalRes(res,Analysis)  #Theoretical values for result and conditions
+  for res in Analysis.results.keys():
+    theoRes = SMSmethods.EvalRes(res,Analysis)  #Theoretical values for result and conditions
+    
+
+    try:
+      plot = Analysis.plots[res]
+    except KeyError:  
+      plot = []
         
+    for imass in range(len(theoRes)):
+      mass = theoRes[imass]['mass']
+      tvalue = theoRes[imass]['result']
+      conds = theoRes[imass]['conditions']
+      sigmalimit = SMSgetlimit.GetPlotLimit(mass,plot,Analysis)
+      
+      if sigmalimit and len(sigmalimit) > 0:
+        Results_table.add_row([wrap(MyPrettyPrinter().pformat(res),width=30),wrap(MyPrettyPrinter().pformat(conds),width=30),wrap(MyPrettyPrinter().pformat(mass),width=30),wrap(MyPrettyPrinter().pformat(tvalue),width=30),wrap(MyPrettyPrinter().pformat(sigmalimit[0]),width=30)])
+        for ilim in range(1,len(sigmalimit)):
+          Results_table.add_row(["","","","",wrap(MyPrettyPrinter().pformat(sigmalimit[ilim]),width=30)])
+      else:
+        Results_table.add_row([wrap(MyPrettyPrinter().pformat(res),width=30),wrap(MyPrettyPrinter().pformat(conds),width=30),wrap(MyPrettyPrinter().pformat(mass),width=30),wrap(MyPrettyPrinter().pformat(tvalue),width=30),wrap(MyPrettyPrinter().pformat(sigmalimit),width=30)])
+      Results_table.add_row(["---------","---------","---------","---------","---------"])
 
-        try:
-            plot = Analysis.plots[res]
-        except KeyError:    
-            plot = []
-                
-        for imass in range(len(theoRes)):
-            mass = theoRes[imass]['mass']
-            tvalue = theoRes[imass]['result']
-            conds = theoRes[imass]['conditions']
-            sigmalimit = SMSgetlimit.GetPlotLimit(mass,plot,Analysis)
-            
-            if sigmalimit and len(sigmalimit) > 0:
-                Results_table.add_row([wrap(MyPrettyPrinter().pformat(res),width=30),wrap(MyPrettyPrinter().pformat(conds),width=30),wrap(MyPrettyPrinter().pformat(mass),width=30),wrap(MyPrettyPrinter().pformat(tvalue),width=30),wrap(MyPrettyPrinter().pformat(sigmalimit[0]),width=30)])
-                for ilim in range(1,len(sigmalimit)):
-                    Results_table.add_row(["","","","",wrap(MyPrettyPrinter().pformat(sigmalimit[ilim]),width=30)])
-            else:
-                Results_table.add_row([wrap(MyPrettyPrinter().pformat(res),width=30),wrap(MyPrettyPrinter().pformat(conds),width=30),wrap(MyPrettyPrinter().pformat(mass),width=30),wrap(MyPrettyPrinter().pformat(tvalue),width=30),wrap(MyPrettyPrinter().pformat(sigmalimit),width=30)])
-            Results_table.add_row(["---------","---------","---------","---------","---------"])
-
-    print(Results_table)
+  print(Results_table)
 
 
 sys.exit()
