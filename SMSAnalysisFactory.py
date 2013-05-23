ff2a97e6f799d41b80af3dd7e80d33df2c965563
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-23 10:57:58 +0000

    starting to put the result retrieval code into a separate python module
    git-svn-id: file:///home/svnuser/SMSDecomposition@244 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- SMSAnalysisFactory.py
+++ SMSAnalysisFactory.py
@@ -31,7 +31,8 @@ def load( anas = None, topos=None ):
       (the database naming convention is used). 
       If topos is given as a list, then only these topos are considered """
   from SMSmethods import EAnalysis
-  import SMSResults, types
+  from DatabaseLookup import SMSResults
+  import types
 
   ## lets make sure the user can also supply a single topo/ana
   ## without having to code an array
