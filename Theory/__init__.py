f179c8a695a52d0bc46e936f663a5e937a8bf8e3
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-23 20:18:39 +0000

    define what we import per default
    git-svn-id: file:///home/svnuser/SMSDecomposition@283 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Theory/__init__.py
+++ Theory/__init__.py
@@ -1,2 +1,5 @@
 """ this module is intended to contain everything related to theory: cross section calculation code, 
-    as well as the sms decomposition code """
+    as well as the sms decomposition code and some more tools """
+
+# we import only what we want to be usable from outside this module
+import SLHATools, SMSxsec, SMSmethods, SMSDecomp, LHEReader
