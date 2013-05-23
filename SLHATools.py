adb1046db87c01c2432a2cdcb77ec1da32e52c96
proschofsky <proschofsky@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-23 08:45:23 +0000

    git-svn-id: file:///home/svnuser/SMSDecomposition@234 c47c41a0-46cc-4d19-81ea-ce3a071b7174

---
+++ SLHATools.py
@@ -0,0 +1,27 @@
+import pyslha2
+import tempfile
+
+def createSLHAFile(topo, masses, filename = None):
+   """ creates an SLHA File for a certain Tx name and certain masses.
+       topo is a string with the Tx name. masses is a dictionary {pid: mass}
+       where pid is an integer and mass is an integer or float (by default
+       the mass is set to 100000.). filename is a string, by default a unique
+       random filename will be generated. return is the filename in string 
+       format"""
+
+   slha = pyslha2.readSLHAFile('regression/slha/%s.slha' %topo)
+
+   for pid in masses:
+      slha[0]['MASS'].entries[pid] = masses[pid]
+
+   for pid in slha[0]['MASS'].entries:
+      if not masses.has_key(pid):
+         slha[0]['MASS'].entries[pid] = 1.00000000e+05
+
+   if filename:
+      pyslha2.writeSLHAFile(filename, slha)
+      return filename
+   else:
+      filename = tempfile.mkstemp()
+      pyslha2.writeSLHAFile(filename[1], slha)
+      return filename[1]
