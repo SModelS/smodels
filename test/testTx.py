e22609084f263a8f58e6b6a110ed9b17b35844b5
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-24 09:31:12 +0000

    abandoning regression
    git-svn-id: file:///home/svnuser/SMSDecomposition@308 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- test/testTx.py
+++ test/testTx.py
@@ -11,9 +11,8 @@ from TestTools import ok
 topolist = ['T1','T2','T1tttt', 'T2tt','T3W', 'T5WW', 'TChiWZ', 'T1bbbb', 'T2bb', 'T5WZ', 'T3Wb', 'T3Z', 'T5ZZ', 'T6bbZZ', 'TChiWW', 'TSlepSlep']
 
 for topo in topolist:
-        File=open("%s_1.lhe" % topo)
-        reader = LHEReader.LHEReader("%s_1.lhe" % topo)
-        Event = reader.next()
-        SMSTop = TopologyBuilder.fromEvent(Event, {})
-	res = TxNames.getTx(SMSTop[0].leadingElement())
-	print "Checking %7s: %s" % ("["+res+"]",ok(res,topo))
+  reader = LHEReader.LHEReader("../lhe/%s_1.lhe" % topo)
+  Event = reader.next()
+  SMSTop = TopologyBuilder.fromEvent(Event, {})
+  res = TxNames.getTx(SMSTop[0].leadingElement())
+  print "Checking %7s: %s" % ("["+res+"]",ok(res,topo))
