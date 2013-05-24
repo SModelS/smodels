f885843c3b3ce8db3eda2a3a8e26128a01cb8f12
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-24 10:21:52 +0000

    sms data objects started, currently only EAnalysis resides there
    git-svn-id: file:///home/svnuser/SMSDecomposition@315 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Theory/SMSmethods.py
+++ Theory/SMSmethods.py
@@ -19,10 +19,9 @@ PtcDic={
 "e" : ["e+","e-"], "mu" : ["mu+", "mu-"], "ta" : ["ta+","ta-"], "l+" : ["e+","mu+"],"l-" : ["e-","mu-"],"l" : ["e-","mu-","e+","mu+"], "W" : ["W+","W-"], "t" : ["t+","t-"], "L+" : ["e+","mu+","ta+"], "L-" : ["e-","mu-","ta-"], "L" : ["e+","mu+","ta+","e-","mu-","ta-"]
 }
 
-#--------------------Classes:
-  
 
 class BElement:
+  """ FIXME whats a BElement? """
   def __init__(self):
     self.masses = []
     self.particles = []
@@ -34,6 +33,7 @@ class BElement:
     return ret
   
 class EElement:
+  """ Event Element. """
   def __init__(self):
     self.B = []
     self.weight = []
@@ -106,85 +106,7 @@ class GTop:
     return True
   
    
-class EAnalysis:  
-  def __init__(self):
-    self.label = ""
-    self.Top = GTop()
-    self.sqrts = 0
-    self.lum = 0
-    self.results = {}
-    self.plots = {}
-    self.run = ""
-    self.masscomp = 0.2
 
-
-#Given the constraints dictionary, automatically fill the element list with the
-#elements corresponding to the strings in the dictionary, skipping repeated ones
-  def GenerateElements(self):
-     
-    ListOfStrs = []
-    vertnumb = self.Top.vertnumb
-    vertparts = self.Top.vertparts
-#Syntax check:    
-    for k in range(len(vertnumb)):
-      if len(vertparts[k]) != vertnumb[k]:
-        print "GenerateElements: Inconsistent data: ninsertions=%d len(insertions)=%d for ``%s''." % ( vertnumb[k], len(vertparts[k]), self.Top )
-        return False
-    
-#Get all element strings:    
-    inelements = self.results.items()
-    
-    for iii in range(len(inelements)):
-      for ii in range(2):  
-        con = inelements[iii][ii].replace(" ","")
-        while "[" in con:  #String has element        
-          st = con[con.find("[[["):con.find("]]]")+3] #Get duplet
-          con = con.replace(st,"")  # Remove element duplet
-          ptclist = eltostr(st)   # Get particle list
-#Syntax checks:
-          for ib in range(2):
-            for ipt in range(len(ptclist[ib])):
-              if len(ptclist[ib][ipt]) != vertparts[ib][ipt]:
-                print "GenerateElements: Wrong syntax2"
-                return False
-              for ptc in ptclist[ib][ipt]:
-                if not ptc in Reven.values() and not PtcDic.has_key(ptc):
-                  print "GenerateElements: Unknown particle ",ptc
-                  return False    
-          ListOfStrs.append(st)
-     
-#Remove repeated elements:
-    ListOfStrs = set(ListOfStrs)
-#Now add all elements to element list    
-    while len(ListOfStrs) > 0:
-      ptclist = eltostr(ListOfStrs.pop()) 
-      NewEl = EElement()
-      NewEl.B = [BElement(),BElement()]      
-      for ib in range(2):
-        NewEl.B[ib].particles = ptclist[ib]
-      self.Top.ElList.append(NewEl)
-  
-  
-
-#Check if the plots listed in results exist
-  def GetPlots(self,verbose=True):
-    from Experiment import SMSResults
-
-    run = self.run    
-    if run == "": run = None  #If run has not been defined, use latest
-    for res in self.results.keys():
-      if not self.plots.has_key(res):
-        if verbose: print "SMSmethods.py: GetPlots: Plot for result",res,"in Analysis",self.label,"not found"
-        topo = ""
-        ana = []
-      else:
-        topo = self.plots[res][0]
-        analyses = self.plots[res][1]
-        
-      for ana in analyses:
-        if not SMSResults.exists(ana,topo,run):
-          if verbose: print "SMSmethods.py: GetPlots: Histogram for ",topo," in ",ana," for run ",run," not found"
-      
 #If two masses in InTop are degenerate, return compressed topology
 def MassCompTop(InTop,mingap):
      
