15dd5f631afb22b69833923006373a90e7f6cb69
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-24 14:24:37 +0000

    make sure we load ROOT only when we need it
    git-svn-id: file:///home/svnuser/SMSDecomposition@318 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Experiment/ROOTTools.py
+++ Experiment/ROOTTools.py
@@ -1,16 +1,15 @@
 """ collection of tools needed for use and manipulation of ROOT files """
 
-import ROOT
-
 
 def getTGraphfromContour(exclhisto):
-   """ returns the contour of an exclusion histogram as TGraph"""
-   ROOT.gROOT.SetBatch()
-   c1 = ROOT.TCanvas()
-   c1.cd()
-   exclhisto.Draw("CONT LIST")
-   ROOT.gPad.Update()
-   ROOT.gROOT.GetListOfSpecials().ls()
-   gr = ROOT.gROOT.GetListOfSpecials().FindObject('contours')('TList').At(0)
-   return gr
- 
+  import ROOT
+  """ returns the contour of an exclusion histogram as TGraph"""
+  ROOT.gROOT.SetBatch()
+  c1 = ROOT.TCanvas()
+  c1.cd()
+  exclhisto.Draw("CONT LIST")
+  ROOT.gPad.Update()
+  ROOT.gROOT.GetListOfSpecials().ls()
+  gr = ROOT.gROOT.GetListOfSpecials().FindObject('contours')('TList').At(0)
+  return gr
+
