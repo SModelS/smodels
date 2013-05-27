b19d148dbe6b1cbc527054ee1d0f22b5cc6f9306
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-27 15:49:09 +0000

    verbose introduced
    git-svn-id: file:///home/svnuser/SMSDecomposition@357 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- test/TestTools.py
+++ test/TestTools.py
@@ -4,6 +4,8 @@ green='\033[0;32m'
 red='\033[0;31m'
 reset='\033[;0m'
 
-def ok ( A,B ):
+def ok ( A,B,verbose=True ):
   if A==B: return "%sok.%s" % ( green, reset )
-  return "%sfailed. [%s]%s" % ( red, B, reset )
+  if verbose:
+    return "%sfailed. [%s]%s" % ( red, B, reset )
+  return "%sfailed. %s" % ( red, reset )
