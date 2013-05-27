77a9dd1c95f6a0c0ed608d7992dd9b0488044605
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-27 11:04:56 +0000

    VariousHelpers started
    git-svn-id: file:///home/svnuser/SMSDecomposition@335 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Theory/SMSmethods.py
+++ Theory/SMSmethods.py
@@ -901,20 +901,3 @@ def Ceval(instring,El):
     run = 'similar(' + str(simels) + ')'
     run = run.replace("'","")
   return eval(run)
-
-  
-#Goes through all analyses in the list and returns the maximum luminosity.
-#If one of the luminosities has not been define, return None
-def getMaxLum(List):
-  
-  maxlum = addunit(0.,'fb-1')
-  for Ana in List:
-    if type(Ana.lum) == type(1) or type(Ana.lum) == type(1.): Ana.lum = addunit(Ana.lum,'fb-1')
-    if Ana.lum.asNumber():      
-      maxlum = max(maxlum,Ana.lum)
-    else:
-      return None
-  return maxlum
-    
-
-
