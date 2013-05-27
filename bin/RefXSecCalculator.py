fa14f3262ef65b5eb9a90a25065958cb2545231b
proschofsky <proschofsky@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-27 11:56:11 +0000

    git-svn-id: file:///home/svnuser/SMSDecomposition@344 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- bin/RefXSecCalculator.py
+++ bin/RefXSecCalculator.py
@@ -9,6 +9,8 @@ import Tools.PhysicsUnits as UNIT
 
 topo = 'T2bb'
 nevts = 100
+pidmom = 1000005 #sbottom
+pidlsp = 1000022 #lsp
 
 SMS.considerRuns(["8TeV", "2012", "2011"])
 analyses = SMS.getAnalyses(topo)
@@ -20,27 +22,37 @@ x_up = 0.
 y_up = 0.
 
 for ana in analyses:
-   print ana
+#   print ana
    if x_low > UNIT.rmvunit(SMS.getLowX(ana, topo), 'GeV'):
       x_low = UNIT.rmvunit(SMS.getLowX(ana, topo), 'GeV')
-   print 'xl',x_low, UNIT.rmvunit(SMS.getLowX(ana, topo), 'GeV')
+#   print 'xl',x_low, UNIT.rmvunit(SMS.getLowX(ana, topo), 'GeV')
    if x_up < UNIT.rmvunit(SMS.getUpX(ana, topo), 'GeV'):
       x_up = UNIT.rmvunit(SMS.getUpX(ana, topo), 'GeV')
-   print 'xu',x_up, UNIT.rmvunit(SMS.getUpX(ana, topo), 'GeV')
-   if y_low > UNIT.rmvunit(SMS.getLowY(ana, topo), 'GeV'):
-      y_low = UNIT.rmvunit(SMS.getLowY(ana, topo), 'GeV')
-   print 'yl',y_low, UNIT.rmvunit(SMS.getLowY(ana, topo), 'GeV')
-   if y_up < UNIT.rmvunit(SMS.getUpY(ana, topo), 'GeV'):
-      y_up = UNIT.rmvunit(SMS.getUpY(ana, topo), 'GeV')
-   print 'yu',y_up, UNIT.rmvunit(SMS.getUpY(ana, topo), 'GeV')
+#   print 'xu',x_up, UNIT.rmvunit(SMS.getUpX(ana, topo), 'GeV')
+#   if y_low > UNIT.rmvunit(SMS.getLowY(ana, topo), 'GeV'):
+#      y_low = UNIT.rmvunit(SMS.getLowY(ana, topo), 'GeV')
+#   print 'yl',y_low, UNIT.rmvunit(SMS.getLowY(ana, topo), 'GeV')
+#   if y_up < UNIT.rmvunit(SMS.getUpY(ana, topo), 'GeV'):
+#      y_up = UNIT.rmvunit(SMS.getUpY(ana, topo), 'GeV')
+#   print 'yu',y_up, UNIT.rmvunit(SMS.getUpY(ana, topo), 'GeV')
 
-print x_low, x_up, y_low, y_up
-
-
+#print x_low, x_up, y_low, y_up
+print x_low, x_up
+mlsp = 0.
+i = 0.
+step = 25.
+m = 0.
 #y_low, y_up unnoetig weil lsp masse fix angenommen (sinnvolle annahme?)
-#slha fuer massen erstellen (createSLHAFile())
-#slhafile = SLHA.createSLHAFile(topo, masses)
-#dic = XSEC.pytinit(nevts, slhafile) # im loop fuer mothermass im bin abstand (25 GeV???)
-
-#xs7 = dic['Xsecdic']['7 TeV (NLO)'][(pdg1,pdg2)] # wie komm ich zu den pdgs???????
-#xs8 = dic['Xsecdic']['8 TeV (NLO)'][(pdg1,pdg2)]
+#while m < x_up-12.5:
+while i < 2:
+   m = (x_low+12.5)+step*i
+   i += 1
+#   print m
+   masses = {pidlsp: mlsp, pidmom: m}
+   slhafile = SLHA.createSLHAFile(topo, masses)
+   dic = XSEC.pytinit(nevts, slhafile) # im loop fuer mothermass im bin abstand (25 GeV???)
+   print dic
+   if dic['Xsecdic']['7 TeV (NLO)'].has_key((pdgmom,pdgmom)):
+      xs7 = dic['Xsecdic']['7 TeV (NLO)'][(pdgmom,pdgmom)]
+      print xs7
+   xs8 = dic['Xsecdic']['8 TeV (NLO)'][(pdg1,pdg2)]
