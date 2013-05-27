703be9eaf26c59d72e2c38a2d67919dd3735f62d
proschofsky <proschofsky@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-27 09:24:08 +0000

    git-svn-id: file:///home/svnuser/SMSDecomposition@329 c47c41a0-46cc-4d19-81ea-ce3a071b7174

---
+++ bin/RefXSecCalculator.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import ROOT
+import Theory.SMSxsec as XSEC
+import Theory.SLHATools as SLHA
+import Experiment.SMSResults as SMS
+import Tools.PhysicsUnits as UNIT
+
+topo = 'T2bb'
+nevts = 100
+
+SMS.considerRuns(["2011", "2012", "8TeV"])
+analyses = SMS.getAnalyses(topo)
+print analyses
+
+x_low = 100000.
+y_low = 100000.
+x_up = 0.
+y_up = 0.
+
+for ana in analyses:
+   print ana
+   if x_low > UNIT.rmvunit(SMS.getLowX(ana, topo), 'GeV'):
+      x_low = UNIT.rmvunit(SMS.getLowX(ana, topo), 'GeV')
+   print 'xl',x_low, UNIT.rmvunit(SMS.getLowX(ana, topo), 'GeV')
+   if x_up < UNIT.rmvunit(SMS.getUpX(ana, topo), 'GeV'):
+      x_up = UNIT.rmvunit(SMS.getUpX(ana, topo), 'GeV')
+   print 'xu',x_up, UNIT.rmvunit(SMS.getUpX(ana, topo), 'GeV')
+   if y_low > UNIT.rmvunit(SMS.getLowY(ana, topo), 'GeV'):
+      y_low = UNIT.rmvunit(SMS.getLowY(ana, topo), 'GeV')
+   print 'yl',y_low, UNIT.rmvunit(SMS.getLowY(ana, topo), 'GeV')
+   if y_up < UNIT.rmvunit(SMS.getUpY(ana, topo), 'GeV'):
+      y_up = UNIT.rmvunit(SMS.getUpY(ana, topo), 'GeV')
+   print 'yu',y_up, UNIT.rmvunit(SMS.getUpY(ana, topo), 'GeV')
+
+print x_low, x_up, y_low, y_up
+
+
+#y_low, y_up unnoetig weil lsp masse fix angenommen (sinnvolle annahme?)
+#slha fuer massen erstellen (createSLHAFile())
+#slhafile = SLHA.createSLHAFile(topo, masses)
+#dic = XSEC.pytinit(nevts, slhafile) # im loop fuer mothermass im bin abstand (25 GeV???)
+
+#xs7 = dic['Xsecdic']['7 TeV (NLO)'][(pdg1,pdg2)] # wie komm ich zu den pdgs???????
+#xs8 = dic['Xsecdic']['8 TeV (NLO)'][(pdg1,pdg2)]
