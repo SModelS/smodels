d7f972bbc09c10a596708092665d060810c15643
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-28 15:32:07 +0000

    sort
    git-svn-id: file:///home/svnuser/SMSDecomposition@419 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- regression/run.py
+++ regression/run.py
@@ -20,8 +20,12 @@ if len(sys.argv)>1:
     run ( int(i) )
   sys.exit(0)
 
+Files=[]
 for File in os.listdir("."):
   if File[-3:]!=".py": continue
   if File[:4]!="test": continue
   Nr=int(File[4:-3])
+  Files.append(Nr)
+Files.sort()
+for Nr in Files:
   run ( Nr )
