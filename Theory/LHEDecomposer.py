06a4b48ef404aa07a21b987de5c8f8f9b4ff2eed
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-28 09:13:40 +0000

    nevents done via reader
    git-svn-id: file:///home/svnuser/SMSDecomposition@381 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Theory/LHEDecomposer.py
+++ Theory/LHEDecomposer.py
@@ -1,19 +1,16 @@
-def decompose(lhefile,W,nevts,DoCompress=False,DoInvisible=False,minmassgap=-1):
-  """ Do LHE-based decomposition.
-      lhefile = LHE file with pythia events
-      W = dictionary with event weights
-      nevts = number of generated events """
+def decompose(lhefile,W,nevts=None,DoCompress=False,DoInvisible=False,minmassgap=-1):
+  """ Do LHE-based decomposition.  lhefile = LHE file with pythia events, W =
+    dictionary with event weights, nevts = (maximum) number of generated events.  
+    If nevts = None, process all events from file.
+    Output is a TopologyList object """
   import LHEReader, TopologyBuilder
-
   reader = LHEReader.LHEReader(lhefile,nevts)
   SMSTopList=TopologyList ( )
-  for iev in range(nevts):
-##Read event  
-    Event = reader.next()
-##Get mother PDGs:
+  for Event in reader:
+    ## Get mother PDGs:
     momPDG = tuple(Event.getMom())
     PList = Event.particles
-#Get event weight list:
+    # Get event weight list:
     weight = {}
     for k in W.keys(): 
       if W[k].has_key(momPDG):
@@ -22,7 +19,7 @@ def decompose(lhefile,W,nevts,DoCompress=False,DoInvisible=False,minmassgap=-1):
         print "LHEdecomp: Error getting weight"
         return False
 
-#Get event topology  
+    # Get event topology  
     SMSTopListEv = TopologyBuilder.fromEvent(Event, weight, DoCompress, DoInvisible, minmassgap)
   
     SMSTopList.addList ( SMSTopListEv )
