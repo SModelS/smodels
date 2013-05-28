52ac5c986241e91f2ebe6f2e7eb84c214096f107
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-28 16:40:38 +0000

    two more methods
    git-svn-id: file:///home/svnuser/SMSDecomposition@425 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Theory/CrossSection.py
+++ Theory/CrossSection.py
@@ -11,7 +11,13 @@ class CrossSection:
   def items ( self ): return self.data.items()
   def __str__ ( self ): return str(self.data)
 
+  def weights ( self ): return self.data["Wdic"]
+  def crossSections ( self ): return self.data["Xsecdic"]
+
   def lhefile ( self, sqrts ):
+    from Tools.PhysicsUnits import rmvunit
+    sqrts=rmvunit(sqrts,"TeV")
     if sqrts==7: return self.data["lhe7file"]
     if sqrts==8: return self.data["lhe8file"]
+    print "[CrossSection.py] lhefile for",sqrts,"does not exist."
     return None
