d2e5da5da74e25afa8c042a006f4ce2a46ef9aa8
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-28 11:11:22 +0000

    use directory names in SMSxsec computations
    git-svn-id: file:///home/svnuser/SMSDecomposition@396 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Theory/SMSXSecs.py
+++ Theory/SMSXSecs.py
@@ -1,22 +1,38 @@
-def computeXSecs(nevts,slhafile,rpythia = True, donlo = True):
+def computeXSecs(nevts,slhafile,rpythia = True, donlo = True, basedir=None):
   """ Runs pythia at 7 and 8 TeV and compute weights for each production
   process. Returns a dictionary with weights at 7 and 8 TeV and the
   event file to be used for SMS decomposition.
+  basedir= directory base. If None, then the we look for the
+  SMSDecomposition directory in the current working directory.
   If runpythia = False, read old fort_8,7.68 files and assumes 
   total xsec = 1 (only useful for fast debugging) """
 
   import shutil, LHEReader, NLLXSec
   from Tools.PhysicsUnits import addunit
+
+  import os
+  if basedir==None:
+    basedir=os.getcwd()
+    if basedir.find("SMSDecomposition"):
+      basedir=basedir[:basedir.find("SMSDecomposition")+16]+"/"
+    
+  nllbase=basedir+"/nllfast"
+  installdir=basedir
+  datadir="%s/data/" % basedir
+  etcdir="%s/etc/" % basedir
   
   if rpythia:
 #run pythia at 8 TeV:
-    D = runpythia(slhafile,nevts,8)
-    shutil.copy2("./data/fort.68", "./data/fort_8.68")
+    D = runPythia( slhafile,nevts,8,
+                   datadir=datadir,etcdir=etcdir,installdir=installdir)
+    shutil.copy2("%s/fort.68" % datadir, "%s/fort_8.68" % datadir )
     total_cs8 = addunit(D["xsecfb"],'fb')
   
 #run pythia at 7 TeV:
-    D = runpythia(slhafile,nevts,7)
-    shutil.copy2("./data/fort.68", "./data/fort_7.68")
+    D = runPythia(slhafile,nevts,7,
+                   datadir=datadir,etcdir=etcdir,installdir=installdir)
+
+    shutil.copy2("%s/fort.68" % datadir, "%s/fort_7.68" % datadir )
     total_cs7 = addunit(D["xsecfb"],'fb')
   else:
     total_cs8 = addunit(1.,'fb')
@@ -25,10 +41,10 @@ def computeXSecs(nevts,slhafile,rpythia = True, donlo = True):
 
 #Get 8 TeV event decomposition:
   n8_evts={}
-  reader = LHEReader.LHEReader("./data/fort_8.68",nevts)
-  for iev in range(nevts):
-    Event = reader.next()
-    mompdg = Event.getMom()    
+  reader = LHEReader.LHEReader("%s/fort_8.68" % datadir,nevts)
+  for event in reader:
+    mompdg = event.getMom()    
+    if not mompdg: continue
     getprodcs(mompdg[0], mompdg[1], n8_evts)
 
   weight_8 = {}
@@ -39,30 +55,28 @@ def computeXSecs(nevts,slhafile,rpythia = True, donlo = True):
 
 #Get 7 TeV event decomposition:
   n7_evts={}
-  reader = LHEReader.LHEReader("./data/fort_7.68",nevts)
-  for iev in range(nevts):
-    Event = reader.next()
-    mompdg = Event.getMom()
+  reader = LHEReader.LHEReader("%s/fort_7.68" % datadir,nevts)
+  for event in reader:
+    mompdg = event.getMom()
     getprodcs(mompdg[0], mompdg[1], n7_evts)
 
   weight_7 = {}
   sigma_7 = {}
   for key in n7_evts.keys():
     if n8_evts.has_key(key):
-      weight_7.update({key : n7_evts[key]*total_cs7/(nevts*n8_evts[key])}) #Weight for one event
-      sigma_7.update({key : n7_evts[key]*total_cs7/nevts})  #Production cross-section
+      weight_7[key]=n7_evts[key]*total_cs7/(nevts*n8_evts[key])
+      sigma_7[key]=n7_evts[key]*total_cs7/nevts # Production cross-section
 
-      
-#Make sure both dictionaries have the same number of entries
+  #Make sure both dictionaries have the same number of entries
   for key in sigma_7.keys() + sigma_8.keys():
     if not sigma_7.has_key(key): 
-      sigma_7.update({key : addunit(0.,'fb')})
-      weight_7.update({key : addunit(0.,'fb')})
+      sigma_7[key]=addunit(0.,'fb')
+      weight_7[key]=addunit(0.,'fb')
     if not sigma_8.has_key(key): 
-      sigma_8.update({key : addunit(0.,'fb')})
-      weight_8.update({key : addunit(0.,'fb')})
+      sigma_8[key]=addunit(0.,'fb')
+      weight_8[key]=addunit(0.,'fb')
 
-#Get NLO cross-sections from NLLfast:
+  #Get NLO cross-sections from NLLfast:
   if donlo:
     sigma_8NLO = {}
     sigma_7NLO = {}
@@ -71,7 +85,7 @@ def computeXSecs(nevts,slhafile,rpythia = True, donlo = True):
 
     for key in sigma_8.keys():
 
-      nllres = NLLXSec.getNLLresult(key[0],key[1],slhafile)
+      nllres = NLLXSec.getNLLresult(key[0],key[1],slhafile,base=nllbase)
       k7 = 1.
       k8 = 1.
    
@@ -88,18 +102,15 @@ def computeXSecs(nevts,slhafile,rpythia = True, donlo = True):
       LO8 = weight_8[key]
       NLO7 = LO7*k7
       NLO8 = LO8*k8
-      weight_7NLO.update({key : NLO7})
-      weight_8NLO.update({key : NLO8}) 
+      weight_7NLO[key]=NLO7
+      weight_8NLO[key]=NLO8
 
       LO7 = sigma_7[key]
       LO8 = sigma_8[key]
       NLO7 = LO7*k7
       NLO8 = LO8*k8
-      sigma_7NLO.update({key : NLO7})
-      sigma_8NLO.update({key : NLO8})
-
-
-
+      sigma_7NLO[key]=NLO7
+      sigma_8NLO[key]=NLO8
 
 
 #Weight and production cross-section dictionaries
@@ -136,31 +147,18 @@ def getprodcs(pdgm1, pdgm2, sigma):
   
   return sigma
 
-def runpythia(slhafile,nevts = 10000, Sqrts = 7):
-  import sys, os
-
-  workdir = os.getcwd()  
-  pyslhadir = workdir + "/pyslha-1.4.3"
-  sys.path.append(pyslhadir)
-  
-  #run pythia
-  print "running pythia"
-  D = runPythiaLHE(nevts,slhafile, sqrts=Sqrts )
-  print "done running"
-  return D
-
-def runPythiaLHE ( n, slhafile, sqrts=7 ):
+def runPythia ( slhafile, n, sqrts=7, datadir="./data/", etcdir="./etc/",
+                installdir="./" ):
   """ run pythia_lhe with n events, at sqrt(s)=sqrts.
       slhafile is inputfile
       datadir is where this all should run,
+      etcdir is where external_lhe.template is to be picked up,
       installdir is where pythia_lhe is to be found.  """
   import commands
-  print "try to run pythia_lhe at sqrt(s)=%d with %d events" % (sqrts,n)
-  datadir="data/"
-  etcdir="etc/"
+  print "[SMSXSecs.runPythia] try to run pythia_lhe at sqrt(s)=%d with %d events" % (sqrts,n)
   o=commands.getoutput ( "cp %s %s/fort.61" % ( slhafile, datadir ) )
   if len(o)>0:
-    print "[SMSXSecs.py] runPythiaLHE error",o
+    print "[SMSXSecs.py] runPythia error",o
   f=open(etcdir+"/external_lhe.template") 
   lines=f.readlines()
   f.close()
@@ -169,8 +167,8 @@ def runPythiaLHE ( n, slhafile, sqrts=7 ):
     out=line.replace("NEVENTS",str(n)).replace("SQRTS",str(1000*sqrts))
     g.write ( out )
   g.close()
-  o=commands.getoutput ( "cd %s; ../pythia_lhe < external_lhe.dat" % \
-     ( datadir ) )
+  o=commands.getoutput ( "cd %s; %s/pythia_lhe < external_lhe.dat" % \
+     ( datadir, installdir ) )
   lines=o.split( "\n" )
   xsecfb=None
   for line in lines:
