7a39a7d943a69496a60168f0d1d90dd22747d4ad
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-28 15:30:25 +0000

    adding
    git-svn-id: file:///home/svnuser/SMSDecomposition@418 c47c41a0-46cc-4d19-81ea-ce3a071b7174

---
+++ regression/test3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+""" starting from a Tx.lhe file, we SmodelS-decompose the event, and 
+obtain its Tx name from the SModelS description of the event: a closure test. """
+
+import set_path
+from Experiment import TxNames
+from Theory import LHEReader, TopologyBuilder
+from TestTools import ok
+
+topolist = ['T1','T2','T1tttt', 'T2tt','T3W', 'T5WW', 'TChiWZ', 'T1bbbb', 'T2bb', 'T5WZ', 'T3Wb', 'T3Z', 'T5ZZ', 'T6bbZZ', 'TChiWW', 'TSlepSlep']
+
+for topo in topolist:
+  reader = LHEReader.LHEReader("../lhe/%s_1.lhe" % topo)
+  Event = reader.next()
+  SMSTop = TopologyBuilder.fromEvent(Event, {})
+  res = TxNames.getTx(SMSTop[0].leadingElement())
+  print "Checking %7s: %s" % ("["+res+"]",ok(res,topo))
