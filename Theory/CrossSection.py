103bdd5a7898c26f3b917a6faa3f770812f8bfdd
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-29 09:49:05 +0000

    skeleton method for doris
    git-svn-id: file:///home/svnuser/SMSDecomposition@442 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Theory/CrossSection.py
+++ Theory/CrossSection.py
@@ -14,6 +14,18 @@ class CrossSection:
   def weights ( self ): return self.data["Wdic"]
   def crossSections ( self ): return self.data["Xsecdic"]
 
+  def crossSectionLightSquarks ( self, order="NLO", sqrts=8 ):
+    return None
+    """
+    squarks=[100001,100002,...]
+    allxsecs=self.crossSections()[order]
+    Sum=0
+    for all (key,value) in allxsecs.items():
+      if abs(key[0]) in squarks and abs(key[1]) in squarks:
+        Sum+=value
+    return Sum
+    """
+
   def lhefile ( self, sqrts ):
     from Tools.PhysicsUnits import rmvunit
     sqrts=rmvunit(sqrts,"TeV")
