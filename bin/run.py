d425313c9c4fef7694943c2e0d1a741086849a92
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-05-31 13:23:40 +0000

    upper limit queried
    git-svn-id: file:///home/svnuser/SMSDecomposition@460 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- bin/run.py
+++ bin/run.py
@@ -2,59 +2,44 @@
 
 """ another sandbox to try things out """
 
-import set_path, argparse, tempfile
-from Theory import LHEDecomposer, SMSDataObjects, XSecComputer, CrossSection
-from Experiment import TxNames
-import Experiment.SMSAnalysisFactory as Analyses
-from Tools import SMSFeynmanGraphs
+import set_path, tempfile
+from Theory import LHEDecomposer, XSecComputer
+from Experiment import TxNames, SMSAnalysisFactory, SMSResults
 
 print "[run.py] loading analyses ...."
-ListOfAnalyses = Analyses.load( topos="T2bb" )
-print "[run.py] done loading %d analyses" % len(ListOfAnalyses)
+analyses = SMSAnalysisFactory.load( anas="alphaT8TeV", topos="T2bb" )
+print "[run.py] done loading %d analyses" % len(analyses)
 
 nevts=1000
 slhafile="../slha/T2bb.slha"
+
 Tmp=tempfile.mkdtemp()
- 
-print "[run.py] now run pythia"
+print "[run.py] now run pythia in",Tmp
 Wv=XSecComputer.compute(nevts,slhafile,rpythia = True, donlo = True, datadir=Tmp)
 print "[run.py] done running pythia"
 
 lhefile=Wv.lhefile ( 8 )
-# lhefile="T2_100.lhe"
 topos=LHEDecomposer.decompose ( lhefile, Wv.weights(), nevts=nevts )
 XSecComputer.clean ( Tmp )
 
-for topo in topos:
-    topo.checkConsistency(verbose=True)
-    print "topo.leadingElement=",topo.leadingElement()
-    for element in topo.elements():
-      print "======================="
-      tx=TxNames.getTx(element)
-      print "element=",element
-      print "B0=",element.B[0]
-      print "tx=",tx
-
 print
-print
-for topo in topos.topos:
-  print "topos SMS topology",topo
-  element=topo.leadingElement() 
-  print "topos leading element",element
-  tx=TxNames.getTx(element)
-  print "topos Tx=",tx
-  
-for Analysis in ListOfAnalyses:
-  Analysis.evaluateResults()
+for Analysis in analyses:
   Analysis.add ( topos )
-  print "------------- Analysis Label = ",Analysis.label, Analysis.sqrts, Analysis.Top
-  print "    `-- element ",Analysis.Top.leadingElement()
-  print "    `-- ",Analysis.Top.leadingElement().B[0]
-  d="[[[jet]],[[jet]]]"
-  #d="[[['jet','jet'],['jet']],[['jet']]]"
-  # S=SMSmethods.strtoel ( d )
-  e=SMSDataObjects.EElement ( d )
-  SMSFeynmanGraphs.asciidraw ( e )
-  # print "S=",S,"e=",e,Analysis.Top.leadingElement()==e
-  #print "              S=``%s'' -> ``%s''" % ( S, Analysis.Top.leadingElement().B[0] )
-
+  print "========================================="
+  print "[run.py] analysis ",Analysis
+  lead=Analysis.Top.leadingElement()
+  print "[run.py]  element ",lead
+  print "[run.py]   masses ",lead.B[0].masses,lead.B[1].masses
+  print "[run.py]    plots ",Analysis.plots
+  for (constraint,condition) in Analysis.results.items():
+    print "[run.py] -- constraint=",constraint,"condition=",condition
+    theoRes=Analysis.evaluateResult( constraint )
+    print "[run.py] -- plot=",Analysis.plots[constraint]
+    print "[run.py] -- len theoRes=",len(theoRes)
+    print "[run.py] --    result 0=",theoRes[0]
+    Tx0=Analysis.plots[constraint][0]
+    ana0=Analysis.plots[constraint][1][0]
+    masses=lead.B[0].masses[0] ## ,lead.B[1].masses[0]
+    print "[run.py] --    plot 0=",Tx0,ana0,masses
+    ul=SMSResults.getSmartUpperLimit(ana0,Tx0,masses)
+    print "[run.py] upper limit=",ul
