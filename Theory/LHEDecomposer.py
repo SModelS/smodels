0d95e68f5322a2171a6441a2d47585a6d94b84eb
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-06-04 12:04:53 +0000

    ignore if weight missing
    git-svn-id: file:///home/svnuser/SMSDecomposition@500 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Theory/LHEDecomposer.py
+++ Theory/LHEDecomposer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 """
-.. module:: LHEDecomposition
+.. module:: LHEDecomposer
     :synopsis: I have no idea ...
 
 .. moduleauthor:: someone <email@example.com>
@@ -9,10 +9,14 @@
 """
 
 def decompose(lhefile,W,nevts=None,doCompress=False,doInvisible=False,minmassgap=-1):
-  """ Do LHE-based decomposition.  lhefile = LHE file with pythia events, W =
-    dictionary with event weights, nevts = (maximum) number of generated events.  
-    If nevts = None, process all events from file.
-    Output is a TopologyList object """
+  """ Do LHE-based decomposition. 
+
+    :param lhefile: LHE file with e.g. pythia events
+    :param W: dictionary with event weights
+    :param nevts: (maximum) number of generated events. If None, all events from \
+      file are processed.
+    :returns: a TopologyList object 
+    """
   import LHEReader, TopologyBuilder, SMSDataObjects
   reader = LHEReader.LHEReader(lhefile,nevts)
   SMSTopList=SMSDataObjects.TopologyList ( )
@@ -26,7 +30,8 @@ def decompose(lhefile,W,nevts=None,doCompress=False,doInvisible=False,minmassgap
         weight[k]=W[k][momPDG]
       else:
         print "[LHEDecomposer] Error getting weight for",k,momPDG
-        return None
+        # weight[k]=0.
+        # return None
 
     # Get event topology  
     SMSTopListEv = TopologyBuilder.fromEvent(Event, weight, doCompress, doInvisible, minmassgap)
