1fe60b8506db58293d399b7a152fdebc4bef12a8
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-06-05 12:09:54 +0000

    renamed lhe files
    git-svn-id: file:///home/svnuser/SMSDecomposition@509 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- bin/UED.py
+++ bin/UED.py
@@ -7,26 +7,16 @@ from Theory import LHEDecomposer, XSecComputer
 from Experiment import TxNames, SMSAnalysisFactory, SMSResults, LimitGetter
 from Tools.PhysicsUnits import fb
 
-print "[run.py] loading analyses ...."
+print "[UED.py] loading analyses ...."
 analyses = SMSAnalysisFactory.load( anas="alphaT8TeV", topos="T2" )
-print "[run.py] done loading %d analyses" % len(analyses)
+print "[UED.py] done loading %d analyses" % len(analyses)
 
 nevts=1
-#slhafile="../slha/T2bb.slha"
+lhefile="../lhe/ued_1.lhe"
 
-#Tmp=tempfile.mkdtemp()
-#print "[run.py] now run pythia in",Tmp
-#Wv=XSecComputer.compute(nevts,slhafile,rpythia = True, donlo = True, datadir=Tmp)
-#print "[run.py] done running pythia"
-
-#lhefile=Wv.lhefile ( 8 )
-lhefile="../lhe/unweighted_events_sms.lhe"
-
-#weights= {'7 TeV (LO)': {(-1000005, 1000005): 1.15E-01 [fb], (1000022, 1000022): 4.63E-01 [fb]}, '8 TeV (LO)': {(-1000005, 1000005): 1.90E-01 [fb], (1000022, 1000022): 1.90E-01 [fb]}, '7 TeV (NLL)': {(1000022, 1000022): 4.63E-01 [fb], (-1000005, 1000005): 2.05E-01 [fb]}, '8 TeV (NLL)': {(1000022, 1000022): 1.90E-01 [fb], (-1000005, 1000005): 3.27E-01 [fb]}}
 weights= { '8 TeV (NLL)': { (-6100002, 6100002): .463 * fb } }
 
 topos=LHEDecomposer.decompose ( lhefile, weights, nevts=nevts )
-#XSecComputer.clean ( Tmp )
 
 print
 for Analysis in analyses:
