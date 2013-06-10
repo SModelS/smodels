e92633de912e93968914c75e3117c2d3a5cb255d
laa <laa@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-06-10 08:54:03 +0000

    add text option in runRecreate.py
    git-svn-id: file:///home/svnuser/SMSDecomposition@562 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- test/makeAllPlots.py
+++ test/makeAllPlots.py
@@ -13,6 +13,7 @@ argparser.add_argument('-mz','--mz',help='intermediate mass information')
 argparser.add_argument('-axes','--axes',help='axes information')
 argparser.add_argument('-n','--nevts',help='number of events per point in refXSec [10000]', type=int, default=10000)
 argparser.add_argument('-b','--binsize',help='binsize in GeV', type=int)
+argparser.add_argument('-text','--text',help='write numbers in 100 GeV distances',action='store_true')
 args=argparser.parse_args()
 
 anas = SMSResults.getAnalyses(args.topo)
@@ -28,7 +29,8 @@ for ana in anas:
   if args.mz: options=options+" -mz %s" % args.mz
   if args.axes: options=options+" -axes %s" % args.axes
   if args.nevts: options=options+" -n %d" % args.nevts
-  if args.binsize: options=options+" -b %d" % args.binsize 
+  if args.binsize: options=options+" -b %d" % args.binsize
+  if args.text: options=options+" -text"
   os.system("./runRecreate.py %s" % options)
   if not args.binsize:
     plotname= "%s_%s_%devts.png" %(ana,toponame,args.nevts)
