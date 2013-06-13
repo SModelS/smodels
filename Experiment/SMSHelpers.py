2501d1ac1790a7967eb96f6781089f4a3dfe6094
laa <laa@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-06-13 08:12:36 +0000

    fix root interpolation for different binning
    git-svn-id: file:///home/svnuser/SMSDecomposition@637 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Experiment/SMSHelpers.py
+++ Experiment/SMSHelpers.py
@@ -187,7 +187,15 @@ def getUpperLimitAtPoint ( histo, mx, my, interpolate=False ):
     return None
   if not histo: return None ## 'no histogram'
   if interpolate:
-    return histo.Interpolate ( rmvunit(mx,'GeV'), rmvunit(my,'GeV') )
+    hInf = histo.Clone()
+    lastBinContent=0
+    for bx in range(1,hInf.GetXaxis().GetNbins()+1):
+      for by in range(1,hInf.GetYaxis().GetNbins()+1):
+        if not hInf.GetBinContent(bx,by): hInf.SetBinContent(bx,by,lastBinContent)
+        else: lastBinContent=hInf.GetBinContent(bx,by)
+    if histo.GetBinContent(histo.GetXaxis().FindBin(rmvunit(mx,'GeV')),histo.GetYaxis().FindBin(rmvunit(my,'GeV'))):
+      return hInf.Interpolate ( rmvunit(mx,'GeV'), rmvunit(my,'GeV') )
+    else: return None
   xmax=histo.GetXaxis().GetNbins()
   xbin=histo.GetXaxis().FindBin(rmvunit(mx,'GeV'))
   if xbin==0 or xbin>xmax:
