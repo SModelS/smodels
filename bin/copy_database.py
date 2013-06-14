2a34480eeb720174ea2402198b18118574aab474
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-06-14 14:43:13 +0000

    mkdir if it doesnt exist
    git-svn-id: file:///home/svnuser/SMSDecomposition@664 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- bin/copy_database.py
+++ bin/copy_database.py
@@ -9,6 +9,9 @@ if len(sys.argv)<2:
 dest=sys.argv[1]
 print "Installing the database to %s:" % dest
 
+if not os.path.exists ( dest ):
+  os.mkdir ( dest )
+
 DB="/afs/hephy.at/user/w/walten/public/sms"
 
 Dirs=[ "2011", "2012", "RPV7", "RPV8", "ATLAS8TeV","8TeV" ]
