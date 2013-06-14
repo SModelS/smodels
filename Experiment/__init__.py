13bbaa74d88cd83c9126bafd902511dedc770254
wmagerl <wmagerl@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-06-14 13:45:48 +0000

    Updated package init file docstrings
    git-svn-id: file:///home/svnuser/SMSDecomposition@660 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Experiment/__init__.py
+++ Experiment/__init__.py
@@ -1,4 +1,7 @@
-""" this module is intended to contain everything that has to do with the experimental results """
+#!/usr/bin/env python
+
+"""This package is intended to contain everything that has to do with the experimental results
+"""
 
 # we import only those units that are intended to be used outside of this module
 import SMSResults, SMSAnalysisFactory, LimitGetter, TxNames
