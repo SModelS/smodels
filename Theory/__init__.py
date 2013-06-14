e08524b297f84ca9480b4fcbc27b5455c2790f17
wmagerl <wmagerl@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-06-14 13:46:23 +0000

    Updated package init file docstrings
    git-svn-id: file:///home/svnuser/SMSDecomposition@661 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Theory/__init__.py
+++ Theory/__init__.py
@@ -1,7 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-""" this module is intended to contain everything related to theory: cross section calculation code, 
-    as well as the sms decomposition code and some more tools """
+"""This Package is intended to contain everything related to theory:
+   * cross section calculation code
+   * sms decomposition code
+   * some more tools
+"""
 
 # we import only what we want to be usable from outside this module
 import SLHATools, XSecComputer, LHEReader, LHEDecomposer, SLHADecomposer, ClusterTools, ParticleNames
--- Tools/__init__.py
+++ Tools/__init__.py
@@ -1,3 +1,6 @@
-""" this module contains all code that cannot be classified as being part of 'Experiment' or 'Theory' """
+#!/usr/bin/env python
+
+"""This package contains all code that cannot be classified as being part of *Experiment* or *Theory*
+"""
 
 import FeynmanGraphs
