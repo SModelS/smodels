30e25e16bf8a2f705352e839b4698365189f07d3
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-06-26 08:47:16 +0000

    scp mode added
    git-svn-id: file:///home/svnuser/SMSDecomposition@687 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- bin/copy_database.py
+++ bin/copy_database.py
@@ -2,11 +2,22 @@
 
 import os, sys
 
-if len(sys.argv)<2:
-  print "Usage:",sys.argv[0],"<destination_directory>"
+def usage():
+  print "Usage:",sys.argv[0]," [-h] [-scp] <destination_directory>"
+  print "        -scp: use scp to smodels instead of local cp"
+  print "        -h: show this help"
   sys.exit(0)
 
-dest=sys.argv[1]
+if len(sys.argv)<2:
+  usage()
+
+useScp=False
+
+for i in sys.argv[1:]:
+  if i=="-scp": useScp=True
+  if i=="-h": usage()
+
+dest=sys.argv[-1]
 print "Installing the database to %s:" % dest
 
 if not os.path.exists ( dest ):
@@ -16,9 +27,15 @@ DB="/afs/hephy.at/user/w/walten/public/sms"
 
 Dirs=[ "2011", "2012", "RPV7", "RPV8", "ATLAS8TeV","8TeV" ]
 
-cmd="scp -r"
-for Dir in Dirs:
-  cmd+=" %s/%s " % (DB, Dir)
-cmd+= dest
-print cmd
-os.system ( cmd )
+if useScp:
+  for Dir in Dirs:
+    cmd="scp -r smodels.hephy.at:%s/%s %s " % (DB, Dir, dest)
+    print cmd
+    os.system ( cmd )
+else:
+  cmd="cp -r"
+  for Dir in Dirs:
+    cmd+=" %s/%s " % (DB, Dir)
+  cmd+= dest
+  print cmd
+  os.system ( cmd )
