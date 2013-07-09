d5d72bd251677bfc26659b5b83340429551b1a75
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-07-09 15:53:39 +0000

    different limit getter call
    git-svn-id: file:///home/svnuser/SMSDecomposition@743 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- bin/UED.py
+++ bin/UED.py
@@ -4,17 +4,23 @@
 
 import set_path, tempfile
 from Theory import LHEDecomposer, XSecComputer
-from Experiment import TxNames, SMSAnalysisFactory, SMSResults, LimitGetter
+from Experiment import TxNames, SMSAnalysisFactory, SMSResults, LimitGetter, SMSHelpers
 from Tools.PhysicsUnits import fb
 from Tools import FeynmanGraphs
+
+## SMSHelpers.Base="/home/lisa/daten/smodels"
 
 print "[UED.py] loading analyses ...."
 ## analyses = SMSAnalysisFactory.load( anas="alphaT8TeV", topos="T2" )
 analyses = SMSAnalysisFactory.load()
+#analyses = SMSAnalysisFactory.load( topos="T2" )
+
 print "[UED.py] done loading %d analyses" % len(analyses)
 
-nevts=1
+nevts=10
 lhefile="../lhe/ued_2.lhe" ## thats the lhe file we're using
+#lhefile="../lhe/firstry.lhe" ## thats the lhe file we're using
+
 
 ## we create the correct object manually
 Wv=XSecComputer.loFromLHE ( lhefile, totalxsec = None ) 
@@ -23,6 +29,7 @@ print "[UED.py] weights=",Wv[0]
 
 ## now create the list of topologies
 topos=LHEDecomposer.decompose ( lhefile, weights, nevts=nevts )
+for topo in topos: FeynmanGraphs.asciidraw ( topo.leadingElement() )
 
 for topo in topos:
   for element in topo.elements():
@@ -32,6 +39,6 @@ for topo in topos:
 print
 for Analysis in analyses:
   Analysis.add ( topos ) ## add all the theory information to the analysis objcts
-  lims=LimitGetter.limit ( Analysis ) ## obtain the relevant limits
+  lims=LimitGetter.limit ( Analysis, addTheoryPredictions=[ '8 TeV (LO)' ] ) ## obtain the relevant limits
   if len(lims): print "[UED.py] lims=",lims
 
