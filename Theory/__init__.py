6a27665078ca3e81139bdf3955796f2081e7a98e
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-07-09 16:09:56 +0000

    fixed comments
    git-svn-id: file:///home/svnuser/SMSDecomposition@747 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Theory/__init__.py
+++ Theory/__init__.py
@@ -3,8 +3,8 @@
 """This Package is intended to contain everything related to theory:
 
    * cross section calculation code
-   * sms decomposition code
-   * some more tools
+   * sms decomposition code (LHE-based, SLHA-based)
+   * some more tools, e.g. for reading/writing slha files, or particle names, ...
 """
 
 # we import only what we want to be usable from outside this module
