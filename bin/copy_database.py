c79bf0f81625b4fb0c81f0a4b9b76d92d97fb683
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-07-10 10:32:57 +0000

    introduced -r argument
    git-svn-id: file:///home/svnuser/SMSDecomposition@760 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- bin/copy_database.py
+++ bin/copy_database.py
@@ -5,19 +5,22 @@
 import os, sys
 
 def usage():
-  print "Usage:",sys.argv[0]," [-h] [-scp] <destination_directory>"
+  print "Usage:",sys.argv[0]," [-h] [-r] [-scp] <destination_directory>"
   print "        -scp: use scp to smodels instead of local cp"
   print "        -h: show this help"
+  print "        -r: remove old local database, if exists"
   sys.exit(0)
 
 if len(sys.argv)<2:
   usage()
 
 useScp=False
+force=False
 
 for i in sys.argv[1:]:
   if i=="-scp": useScp=True
   if i=="-h": usage()
+  if i=="-r": force=True
 
 dest=sys.argv[-1]
 print "Installing the database to %s:" % dest
@@ -31,13 +34,26 @@ Dirs=[ "2011", "2012", "RPV7", "RPV8", "ATLAS8TeV","8TeV" ]
 
 if useScp:
   for Dir in Dirs:
+    Target="%s/%s" % (dest, Dir)
+    if os.path.exists ( Target ):
+      print "Warning:",Target,"exists already."
+      if force:
+        print "Requested removal of",Target
+        os.system ( "rm -rf %s" % Target )
     cmd="scp -r smodels.hephy.at:%s/%s %s " % (DB, Dir, dest)
     print cmd
     os.system ( cmd )
 else:
   cmd="cp -r"
   for Dir in Dirs:
-    cmd+=" %s/%s " % (DB, Dir)
+    Target="%s/%s" % (dest, Dir)
+    print "Dir",Target
+    if os.path.exists ( Target ):
+      print "Warning:",Target,"exists already."
+      if force:
+        print "Requested removal of",Target
+        os.system ( "rm -rf %s" % Target )
+    cmd+=" %s/%s " % ( DB, Dir )
   cmd+= dest
   print cmd
   os.system ( cmd )
