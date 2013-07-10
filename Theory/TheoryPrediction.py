c6fc936f65a849e19a3d65a342702954474c65a8
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-07-10 11:02:10 +0000

    commented out unused code
    git-svn-id: file:///home/svnuser/SMSDecomposition@766 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- Theory/TheoryPrediction.py
+++ Theory/TheoryPrediction.py
@@ -45,6 +45,9 @@ class XSecPredictionForCluster:
     
   def oldformat(self):
     """ Returns a dictionary with the old output format """
+    import logging
+    log = logging.getLogger(__name__)
+    log.warning('XSecPredictionForCluster.oldformat() is obsolete')
     resdic = {}
     resdic['mass'] = self.mass
     resdic['result'] = self.result_dic.values()[0]
@@ -66,74 +69,73 @@ class XSecPredictionForCluster:
 
     return self.result_dic.values()[0]
 
-
-class TheoryPrediction:
-  """ a wrapper for the result of EAnalysis.computeTheoryPredictions,
-      make it easier to access the theoretical xsec prediction for 
-      a particular EElement """
-
-  def equal ( self, m1, m2 ):
-    from Tools.PhysicsUnits import rmvunit
-    """ is array of masses m1 equal to array m2? """
-    if len(m1)!=len(m2): return False
-    for (i,m) in enumerate(m1):
-      d=abs(rmvunit(m,"GeV")-rmvunit(m2[i],"GeV"))
-      if d>1e-1: return False
-    return True
-
-  def findIn ( self, m1, dmass ):
-    """ search for array of masses m1 in array of array of masses dmass """
-    for m2 in dmass:
-      if self.equal ( m1, m2 ): return True
-    return False
-
-  def __init__ ( self, data ):
-    self.data=data
-  # make it behave much like a dictionary
-  def __len__ ( self ): return len(self.data)
-  def __getitem__ ( self, i ): return self.data[i]
-  def items ( self ): return self.data.items()
-  def __str__ ( self ): return str(self.data)
-
-  def predictionFor ( self, m1=None, m2=None, sqrts=None, order=None, condition=None ):
-    """ get the theory xsec prediction for specific conditions:
-        m1: get it for this array of masses 
-        m2: specify also second array of masses for other branch.
-        sqrts: 7 or 8 
-        order: LO or NLO
-        condition: the condition as is in the database 
-
-        :returns: reference cross section, with units, None if no result available.
-    """
-    runs=None
-    if sqrts!=None and order!=None:
-      runs = [ "%d TeV (%s)" % ( int(sqrts), order ) ]
-    if sqrts!=None and order==None:
-      runs= [ "%d TeV (NLL)" % ( int(sqrts) ), "%d TeV (LO)" % int (sqrts) ]
-    if sqrts==None and order!=None:
-      runs= [ "7 TeV (%s)" % ( order), "8 TeV (%s)" % order ]
-
-    # print "runs=",runs
-    ## print "[TheoryPrediction] runs=",runs
-    ret=None
-    count=0
-    for d in self.data:
-      ## check if condition matches, if given
-      if condition!="None" and not condition in d['conditions']: continue
-      ## check if masses match, if given
-      if m1!=None and not self.findIn ( m1, d['mass'] ): continue
-      if m2!=None and not self.findIn ( m2, d['mass'] ): continue
-      #print "match!"
-      res=d['result']
-      for (key,value) in res.items():
-        if runs==None or key in runs:
-          count+=1
-          ret=value
-          # print "[TheoryPrediction.py] match:",res
-    if count>1:
-      print "[TheoryPrediction.py] error: more than one result matches description."
-    if count==0:
-      print "[TheoryPrediction.py] error: no result matches description m1=",m1,"m2=",m2,"sqrts=",sqrts,"order=",order,"condition=",condition
-    return ret
-
-
+# FIXME obsolete??
+#class TheoryPrediction:
+#  """ a wrapper for the result of EAnalysis.computeTheoryPredictions,
+#      make it easier to access the theoretical xsec prediction for 
+#      a particular EElement """
+#
+#  def equal ( self, m1, m2 ):
+#    from Tools.PhysicsUnits import rmvunit
+#    """ is array of masses m1 equal to array m2? """
+#    if len(m1)!=len(m2): return False
+#    for (i,m) in enumerate(m1):
+#      d=abs(rmvunit(m,"GeV")-rmvunit(m2[i],"GeV"))
+#      if d>1e-1: return False
+#    return True
+#
+#  def findIn ( self, m1, dmass ):
+#    """ search for array of masses m1 in array of array of masses dmass """
+#    for m2 in dmass:
+#      if self.equal ( m1, m2 ): return True
+#    return False
+#
+#  def __init__ ( self, data ):
+#    self.data=data
+#  # make it behave much like a dictionary
+#  def __len__ ( self ): return len(self.data)
+#  def __getitem__ ( self, i ): return self.data[i]
+#  def items ( self ): return self.data.items()
+#  def __str__ ( self ): return str(self.data)
+#
+#  def predictionFor ( self, m1=None, m2=None, sqrts=None, order=None, condition=None ):
+#    """ get the theory xsec prediction for specific conditions:
+#        m1: get it for this array of masses 
+#        m2: specify also second array of masses for other branch.
+#        sqrts: 7 or 8 
+#        order: LO or NLO
+#        condition: the condition as is in the database 
+#
+#        :returns: reference cross section, with units, None if no result available.
+#    """
+#    runs=None
+#    if sqrts!=None and order!=None:
+#      runs = [ "%d TeV (%s)" % ( int(sqrts), order ) ]
+#    if sqrts!=None and order==None:
+#      runs= [ "%d TeV (NLL)" % ( int(sqrts) ), "%d TeV (LO)" % int (sqrts) ]
+#    if sqrts==None and order!=None:
+#      runs= [ "7 TeV (%s)" % ( order), "8 TeV (%s)" % order ]
+#
+#    # print "runs=",runs
+#    ## print "[TheoryPrediction] runs=",runs
+#    ret=None
+#    count=0
+#    for d in self.data:
+#      ## check if condition matches, if given
+#      if condition!="None" and not condition in d['conditions']: continue
+#      ## check if masses match, if given
+#      if m1!=None and not self.findIn ( m1, d['mass'] ): continue
+#      if m2!=None and not self.findIn ( m2, d['mass'] ): continue
+#      #print "match!"
+#      res=d['result']
+#      for (key,value) in res.items():
+#        if runs==None or key in runs:
+#          count+=1
+#          ret=value
+#          # print "[TheoryPrediction.py] match:",res
+#    if count>1:
+#      print "[TheoryPrediction.py] error: more than one result matches description."
+#    if count==0:
+#      print "[TheoryPrediction.py] error: no result matches description m1=",m1,"m2=",m2,"sqrts=",sqrts,"order=",order,"condition=",condition
+#    return ret
+#
