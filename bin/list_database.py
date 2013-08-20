5e684831eac9844a5b731ee2dc582242e7eee9a8
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-08-20 09:12:41 +0000

    cont
    git-svn-id: file:///home/svnuser/SMSDecomposition@858 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- bin/list_database.py
+++ bin/list_database.py
@@ -5,36 +5,83 @@ what digitized results are not yet published """
 
 import set_path
 from Experiment import SMSResults, SMSHelpers
+from Tools.PhysicsUnits import rmvunit
+  
+fields=3
 
 def header ( File, latex=True ):
+  File.write ( "\\documentclass{article}\n" )
+  File.write ( "\\begin{document}\n" )
+  File.write ( "\\thispagestyle{empty}\n" )
   File.write ( "\\begin{table}\n" )
-  File.write ( "\\begin{tabular}{|c|c|c|}\n" )
+  flds="{"
+  for i in range(fields):
+    flds+="c|"
+  flds=flds[:-1]
+  flds+="}"
+  File.write ( "\\begin{tabular}%s\n" % flds )
+  if fields==2:
+    File.write ( "{\\bf analysis} & {\\bf topologies} \\\\ \\hline \n" )
+  else:
+    File.write ( "{\\bf analysis} & $\\sqrt{s}$ & {\\bf topologies} \\\\ \\hline \n" )
 
 def footer ( File, latex=True ):
   File.write ( "\\end{tabular}\n" )
   File.write ( "\\end{table}\n" )
+  File.write ( "\\end{document}" )
 
-def printResult ( File, analysis, topo, latex=True ):
-  sqrts=SMSResults.getSqrts ( analysis )
+def compileTex ( Filename="database.tex" ):
+  import os
+  os.system ( "pdflatex database.tex" )
+  os.system ( "convert database.pdf database.png" )
+
+
+def printResult ( File, analysis, topos, latex=True ):
+  sqrts=int(rmvunit(SMSResults.getSqrts ( analysis ),"TeV"))
   pas=SMSResults.getPAS ( analysis )
-  constr=SMSResults.getConstraints ( analysis, topo )
-  File.write ( "%s & %s & %s \\\\ \n" % ( analysis, topo, pas ) )
+  Topos=""
+
+  ctr=0
+  for topo in topos:
+    constr=SMSResults.getConstraints ( analysis, topo )
+    sqrts=SMSResults.getSqrts ( analysis, topo )
+    if ctr>2:
+      Topos+="\\\\ & "
+      ctr=0
+    if constr: 
+      Topos+=topo+", "
+      ctr+=1
+    
+  if len(Topos)>2: Topos=Topos[:-2]
+  if fields==2:
+    File.write ( "%s & %s \\\\ \n" % ( pas, Topos ) )
+  else:
+    File.write ( "%s & %s & %s \\\\ \n" % ( pas, sqrts, Topos ) )
+  # File.write ( "%s & %s & %s \\\\ \n" % ( analysis, topo, pas ) )
 
 def run( latex=True ):
   File=open("database.tex","w")
   header ( File, latex )
-  results=[]
+  results={}
   All=SMSResults.getAllResults()
   for analysis in All:
     for topo in All[analysis]:
       if not SMSResults.isPublic ( analysis ): continue
       constr=SMSResults.getConstraints ( analysis, topo )
       if not constr: continue
+      sqrts=SMSResults.getSqrts ( analysis )
       run=SMSResults.getRun ( analysis )
-      results.append ( [ analysis, topo ] )
-  for result in results:
-    printResult ( File, result[0], result[1], latex )
+      if not results.has_key ( analysis ): results[analysis]=[]
+      results[analysis].append ( topo )
+
+  keys=results.keys()
+  keys.sort()
+  print keys
+  for analysis in keys:
+    topos=results[analysis]
+    printResult ( File, analysis, topos, latex )
   footer ( File, latex )
   File.close()
+  compileTex()
 
 run()
