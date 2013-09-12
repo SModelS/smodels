96341cf25cbd4469046bf61ad47c9e1b513d821c
walten <walten@c47c41a0-46cc-4d19-81ea-ce3a071b7174>
2013-09-12 08:40:24 +0000

    cleanup destination
    git-svn-id: file:///home/svnuser/SMSDecomposition@886 c47c41a0-46cc-4d19-81ea-ce3a071b7174

--- bin/copy_database.py
+++ bin/copy_database.py
@@ -11,6 +11,44 @@ def usage():
   print "        -r: remove old local database, if exists"
   sys.exit(0)
 
+def localCopy ( dest, Dirs, force ):
+  cmd="cp -r"
+  for Dir in Dirs:
+    Target="%s/%s" % (dest, Dir)
+    print "Dir",Target
+    if os.path.exists ( Target ):
+      print "Warning:",Target,"exists already."
+      if force:
+        print "Requested removal of",Target
+        os.system ( "rm -rf %s" % Target )
+  #  if not os.path.exists ( Target ):
+  #    os.mkdir ( Target )
+    cmd+=" %s/%s " % ( DB, Dir )
+  cmd+= dest
+  print cmd
+  os.system ( cmd )
+  stripDatabase ( dest, Dirs )
+
+def stripAnalysis ( path ):
+  Files=os.listdir ( path )
+  print "stripping",path,Files
+  for F in Files:
+    if not F in [ "sms.py", "sms.root", "info.txt" ]:
+    ## if F in [ "orig", "old", "convert.py", "draw.py", "Standardizer.py", "convert.py~", "#Standardizer.py#", "info.txt_", "info.old", "Standardizer.pyc", "#convert.py#", "results" ]:
+      cmd="rm -rf %s/%s" % ( path, F )
+      print cmd
+      os.system ( cmd )
+
+def stripDatabase ( dest, Dirs ):
+  for Dir in Dirs:
+    path=dest + "/" + Dir
+    anas= os.listdir ( path )
+    for ana in anas:
+      if ana.lower() in [ "todo", "readme", "old", "bad" ]: continue
+      if ana[0]==".": continue
+      if ana[-3:] in [ ".py", ".sh" ]: continue
+      stripAnalysis ( path+"/"+ana )
+
 if len(sys.argv)<2:
   usage()
 
@@ -44,16 +82,4 @@ if useScp:
     print cmd
     os.system ( cmd )
 else:
-  cmd="cp -r"
-  for Dir in Dirs:
-    Target="%s/%s" % (dest, Dir)
-    print "Dir",Target
-    if os.path.exists ( Target ):
-      print "Warning:",Target,"exists already."
-      if force:
-        print "Requested removal of",Target
-        os.system ( "rm -rf %s" % Target )
-    cmd+=" %s/%s " % ( DB, Dir )
-  cmd+= dest
-  print cmd
-  os.system ( cmd )
+  localCopy ( dest, Dirs, force )
