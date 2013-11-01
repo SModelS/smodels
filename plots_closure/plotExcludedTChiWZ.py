9c31059315a5d123d72d69f299e212abdb5aa27b
lessa.at.XPS12 <lessa.a.p@gmail.com>
2013-11-01 20:23:01 +0100

    added new validation plots

--- plots_closure/plotExcludedTChiWZ.py
+++ plots_closure/plotExcludedTChiWZ.py
@@ -15,7 +15,7 @@ metadata = AuxPlot.getMetadata(inputOn,tags)
 Rmax = 1.
 if metadata['Kfactor']:
   Rmax = Rmax/eval(metadata['Kfactor'][0])
-  metadata['title'][0] += '*'+metadata['Kfactor'][0]
+  metadata['title'][0] = metadata['title'][0].replace('LO','LO*'+str(metadata['Kfactor'][0]))
 
 #Get data for on and off-shell and combine:
 resultsOn = AuxPlot.getData(inputOn,Rmax)
@@ -45,7 +45,9 @@ xmax,ymax,zmax = max(xvS),max(yvS),max(zvS)
 xmin,ymin,zmin = min(xvS),min(yvS),min(zvS)
 dxmin = min([abs(xvS[i]-xvS[i+1]) for i in range(len(xvS)-1)])*3.
 dymin = min([abs(yvS[i]-yvS[i+1]) for i in range(len(yvS)-1)])*3.
-exp_limit = TH2F("","",int((xmax-xmin)/dxmin),xmin,xmax,int((ymax-ymin)/dymin),ymin,ymax)
+#exp_limit = TH2F("","",int((xmax-xmin)/dxmin),xmin,xmax,int((ymax-ymin)/dymin),ymin,ymax)
+exp_limit = TH2F("","",12,100.,400.,10,0.,250.)
+print int((xmax-xmin)/dxmin),xmin,xmax,int((ymax-ymin)/dymin),ymin,ymax
 for ilim,lim in enumerate(zv):
   if exp_limit.GetBinContent(exp_limit.FindBin(xv[ilim],yv[ilim])) < lim:
     exp_limit.SetBinContent(exp_limit.FindBin(xv[ilim],yv[ilim]),lim)
@@ -53,38 +55,41 @@ for ilim,lim in enumerate(zv):
 
 #Canvas
 AuxPlot.set_palette(gStyle)
-plane = TCanvas("c1", "c1",0,0,800,500)
+plane = TCanvas("c1", "c1",0,0,800,700)
 AuxPlot.Default(plane,"TCanvas")
-plane.SetRightMargin(0.14700422)
+plane.SetLeftMargin(0.15700422)
+plane.SetRightMargin(0.15700422)
 plane.SetTopMargin(0.05296053)
 plane.SetBottomMargin(0.16796053)
 
 exp_limit.Draw('COLZ')
-for plot in rootPlots.keys(): rootPlots[plot].Draw("SAMEP")
+for plot in rootPlots.keys():
+  rootPlots[plot].Sort()
+  rootPlots[plot].Draw("SAMEL")
 exc_curve.Draw("SAMEL")
 #Set options
 exp_limit.SetStats(kFALSE)
 AuxPlot.Default(exp_limit,'TH2')
 plane.SetLogz()
-exp_limit.GetXaxis().SetTitle("M (GeV)")
+exp_limit.GetXaxis().SetTitle("m_{#tilde{#chi}_{1}^{#pm}} (GeV)")
 exp_limit.GetYaxis().SetTitle("m_{LSP} (GeV)")
-exp_limit.GetYaxis().SetTitleOffset(0.75)
+exp_limit.GetYaxis().SetTitleOffset(0.98)
 exp_limit.GetZaxis().SetTitle("Upper Limit (fb)")
-exp_limit.GetZaxis().SetTitleOffset(0.75)
+exp_limit.GetZaxis().SetTitleOffset(0.95)
 exp_limit.GetZaxis().SetRangeUser(zmin,zmax)
 exc_curve.SetLineColor(kRed)
 exc_curve.SetLineStyle(9)
-exc_curve.SetLineWidth(3)
+exc_curve.SetLineWidth(5)
 for iplot,plot in enumerate(rootPlots.keys()):
   rootPlots[plot].SetLineStyle(iplot+1)
-  rootPlots[plot].SetLineWidth(3)
+  rootPlots[plot].SetLineWidth(5)
   rootPlots[plot].SetLineColor(kBlack)  
   rootPlots[plot].SetMarkerStyle(20)
   rootPlots[plot].SetMarkerSize(0.9)  
 gPad.RedrawAxis()
 
 #Legend
-leg = TLegend(0.6934673,0.8051392,0.9886935,0.99)
+leg = TLegend(0.571608,0.7645306,0.9824121,0.9850969)
 AuxPlot.Default(leg,"Legend")  
 leg.AddEntry(exc_curve,'SModelS',"L")
 for plot in rootPlots.keys(): leg.AddEntry(rootPlots[plot],plot,"L")
@@ -93,15 +98,15 @@ leg.Draw()
 #Title
 if metadata['title']:
   title = metadata['title'][0]
-  tit = TPaveLabel(0.01005025,0.8672377,0.5150754,0.9807281,title,"brNDC")
+  tit = TPaveLabel(0.01005025,0.8301043,0.5226131,0.9821162,title,"brNDC")
   tit.SetBorderSize(4)
   tit.SetFillColor(0)
   tit.SetTextFont(42)
-  tit.SetTextSize(0.3127273)
+  tit.SetTextSize(0.2327273)
   tit.Draw()
 
 gPad.Update()
-c1.Print('2D-'+'TChiWZonoff.png')
+c1.Print('2D-'+'TChiWZonoff.eps')
 ans = raw_input("Hit any key to close\n")
 
   
