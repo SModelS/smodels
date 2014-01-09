c1ed9c04601f295f29ff44a3b936a93dea3a3e55
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-01-09 10:34:43 +0100

    add version retrieval with using import

--- bin/checkTools.py
+++ bin/checkTools.py
@@ -5,6 +5,8 @@ from Tools import ROOTTools
 
 print "ROOTTools.getRootVersion():",ROOTTools.getRootVersion()
 print "ROOTTools.getRootVersion(true):",ROOTTools.getRootVersion(True)
+print "ROOTTools.getRootVersionFromImport_(true):",ROOTTools.getRootVersion(True,True)
 print "ROOTTools.getRootPath():",ROOTTools.getRootPath()
 print "ROOTTools.getRootLibraryPath():",ROOTTools.getRootLibraryPath()
+print "ROOTTools.getRootPythonPath():",ROOTTools.getRootPythonPath()
 print "ROOTTools.getRootPythonPath():",ROOTTools.getRootPythonPath()
