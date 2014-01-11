fa210b2dce75b09634360525870229f32c3df932
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-01-11 18:44:49 +0100

    smodels version introduced

---
+++ SModelS.py
@@ -0,0 +1,24 @@
+"""
+.. module:: SModelS
+    :synopsis: Intended as a potential main entry point, currently just for
+               returning the SModelS version number.
+
+.. moduleauthor:: Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
+
+"""
+
+def installdir():
+  """ return the software installation directory, by looking at location of this method """
+  import os, inspect
+  return os.path.dirname ( inspect.getfile(installdir) )
+
+def version( astuple=False ):
+  """ prints out version number of SModelS framework """
+  f=open("%s/version" % installdir() )
+  l=f.readline()
+  f.close()
+  if not astuple: return l
+  T,C=l.split("/")
+  A,B=T.split(".")
+  return (int(A),int(B),C.strip())
+  
--- bin/checkTools.py
+++ bin/checkTools.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 
 import set_path
+import SModelS
 from Tools import ROOTTools
 
 print "ROOTTools.getRootVersion():",ROOTTools.getRootVersion()
 print "ROOTTools.getRootVersion(true):",ROOTTools.getRootVersion(True)
-print "ROOTTools.getRootVersionFromImport_(true):",ROOTTools.getRootVersion(True,True)
 print "ROOTTools.getRootPath():",ROOTTools.getRootPath()
 print "ROOTTools.getRootLibraryPath():",ROOTTools.getRootLibraryPath()
 print "ROOTTools.getRootPythonPath():",ROOTTools.getRootPythonPath()
-print "ROOTTools.getRootPythonPath():",ROOTTools.getRootPythonPath()
+print "SModelS.version()",SModelS.version()
+print "SModelS.version(true)",SModelS.version(True)
