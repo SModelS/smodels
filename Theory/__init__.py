0b5cc8049d03cd0449c00b599a5450591fc8dec0
Wolfgang Magerl <wolfgang.magerl@gmail.com>
2014-03-09 23:01:05 +0100

    Simplified logging facility (put initial settings to packages).

--- Theory/__init__.py
+++ Theory/__init__.py
@@ -10,3 +10,10 @@ import lheDecomposer
 import slhaDecomposer
 import clusterTools
 import crossSection
+import os
+import logging.config
+
+basepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+logging.config.fileConfig('%s/logging.conf' % basepath, disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
