4b393daa937b17bd5d2201dac842983fb57abb4b
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-03-13 10:32:45 +0100

    introduced a smodels-config to find where SModelS is installed. a few comment bug fixes.

--- SModelS.py
+++ SModelS.py
@@ -8,19 +8,21 @@
 """
 
 def installdir():
-  """ return the software installation directory, by looking at location of this method """
-  import os, inspect
-  return os.path.dirname ( inspect.getabsfile(installdir) )
+    """ return the software installation directory, by looking at location of this method """
+    import os, inspect
+    ret=os.path.realpath ( inspect.getabsfile(installdir) )
+    ret=ret.replace("SModelS.py","")
+    return ret
 
 def version( astuple=False ):
-  """ prints out version number of SModelS framework """
-  f=open("%s/version" % installdir() )
-  l=f.readline()
-  f.close()
-  l=l.replace("\n","")
-  l.strip()
-  if not astuple: return l
-  T,C=l.split("/")
-  A,B=T.split(".")
-  return (int(A),int(B),C.strip())
-  
+    """ prints out version number of SModelS framework """
+    f=open("%s/version" % installdir() )
+    l=f.readline()
+    f.close()
+    l=l.replace("\n","")
+    l.strip()
+    if not astuple: return l
+    T,C=l.split("/")
+    A,B=T.split(".")
+    return (int(A),int(B),C.strip())
+
--- bin/set_path.py
+++ bin/set_path.py
@@ -1,4 +1,4 @@
-""" this sets the path so we can write e.g. "from Theory import blahblah """
+""" this sets the path so we can write e.g. "from theory import blahblah """
 
 import sys
 sys.path.append ( "../" )
