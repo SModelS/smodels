dc899a59de773cc2ea61b2a935a7b7451812045c
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-03-14 11:13:19 +0100

    commented out the print statements

--- tools/loggingConfiguration.py
+++ tools/loggingConfiguration.py
@@ -5,12 +5,12 @@ def configure():
     basepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     logfile= '%s/etc/logging.conf' % basepath
     if not os.path.exists ( logfile ):
-        print "[tools/__init__.py] %s does not exist." % logfile
+        # print "[tools/__init__.py] %s does not exist." % logfile
         import inspect, set_path
         base=os.path.realpath ( inspect.getabsfile(configure) )
         base=base.replace("tools/loggingConfiguration.py","")
         logfile=base+"etc/logging.conf"
-        print "base=",base
+        # print "base=",base
     logging.config.fileConfig( logfile, disable_existing_loggers=False)
 
 def createLogger ( name ):
