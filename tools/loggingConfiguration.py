55a372f6183c11d65bebbe06e41eb3e95f6b2d70
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-03-18 11:15:57 +0100

    turned loggingCOnfiguration also into a script

--- tools/loggingConfiguration.py
+++ tools/loggingConfiguration.py
@@ -1,3 +1,16 @@
+#!/usr/bin/python
+
+"""
+.. module:: loggingConfiguration
+    :synopsis: Creates a logger object that is configured according to
+        <current-working-directory>/etc/logging.conf.
+        If that file is not found, revert to
+        <smodels-installation>/etc/logging.conf.
+
+.. moduleauthor:: Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
+
+"""
+
 def configure():
     import os
     import logging.config
@@ -12,8 +25,13 @@ def configure():
         logfile=base+"etc/logging.conf"
         # print "base=",base
     logging.config.fileConfig( logfile, disable_existing_loggers=False)
+    return logfile
 
 def createLogger ( name ):
     configure()
     import logging
     return logging.getLogger(name)
+
+if __name__ == "__main__": 
+    """ called as a script, we just show the path to the log file """
+    print "Using log file",configure()
