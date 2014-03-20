8561bd606f277dc0b6def2d509879805928420b8
Wolfgang Magerl <wolfgang.magerl@gmail.com>
2014-03-20 21:54:26 +0100

    Removed unused imports and improved docstrings.

--- experiment/__init__.py
+++ experiment/__init__.py
@@ -3,6 +3,5 @@ This package is intended to contain everything that has to do with the
 experimental results.
 
 """
-import os
 from tools import loggingConfiguration
-logger = loggingConfiguration.createLogger  (__name__ )
+logger = loggingConfiguration.createLogger(__name__)
--- theory/__init__.py
+++ theory/__init__.py
@@ -1,10 +1,10 @@
-"""This Package is intended to contain everything related to theory:
+"""
+This Package is intended to contain everything related to theory:
 
    * cross section calculation code
    * sms decomposition code (LHE-based, SLHA-based)
    * some more tools, e.g. for reading/writing slha files, or particle names
    
 """
-import os
 from tools import loggingConfiguration
-logger = loggingConfiguration.createLogger  (__name__ )
+logger = loggingConfiguration.createLogger(__name__)
