9d2278afe0eed0ef0bbbe27387d391eb76204ffc
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-03-21 12:05:36 +0100

    ported to develop API

--- bin/timeDBAccess.py
+++ bin/timeDBAccess.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import set_path, argparse, types
-from experiment import SMSResults, SMSHelpers
+from experiment import smsResults, smsHelpers
 from tools import rcFile
 from timeit import timeit
 
@@ -11,21 +11,21 @@ argparser.add_argument ( '-n', '--nqueries', nargs='?', help='number of queries'
 args=argparser.parse_args()
 
 if args.database!="":
-  SMSHelpers.Base=args.database
+  smsHelpers.Base=args.database
 n=args.nqueries
 
 results={ "n": 0 }
 
 def query():
-  res=SMSResults.getAllResults()
+  res=smsResults.getAllResults()
 
   for  (ana,topos) in res.items():
     for topo in topos:
-      constraint=SMSResults.getConstraints ( ana, topo )
-      condition=SMSResults.getConditions ( ana, topo )
+      constraint=smsResults.getConstraints ( ana, topo )
+      condition=smsResults.getConditions ( ana, topo )
       results["n"]+=1
       # print ana,topo,constraint,condition
 
 T=timeit(query, number=n )
-print "n=%d base=%s" % ( n, SMSHelpers.Base )
+print "n=%d base=%s" % ( n, smsHelpers.Base )
 print "%d results in %f seconds" % ( results["n"],T )
