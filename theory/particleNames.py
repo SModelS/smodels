48736c89a8924574566198444c01a4d1f617a9dd
Wolfgang Magerl <wolfgang.magerl@gmail.com>
2014-03-24 15:50:22 +0100

    Renamed ParticleNames to particleNames and improved code structure.

--- theory/particleNames.py
+++ theory/particleNames.py
@@ -2,13 +2,13 @@
 
 """
 .. module:: ParticleNames
-        :synopsis: methods for getting particle names from pdg ids, and other helpers
+   :synopsis: Methods for getting particle names from pdg ids, and other
+   helpers.
 
 .. moduleauthor:: Andre Lessa <lessa.a.p@gmail.com>
 
 """
 
-import auxiliaryFunctions
 import logging
 
 logger = logging.getLogger(__name__)
@@ -183,87 +183,86 @@ def vertInStr(instring):
 
 
 def simParticles(ptype1,ptype2,useDict=True):
-     """ Compares 2 particle names or 2 nested name arrays. \
-             Allows for dictionary labels
-             (Ex: L = l, l+ = l, l = l-,...) 
-             For the last nested level ignore particle ordering 
-             FIXME nesting? 
+    """
+    Compares 2 particle names or 2 nested name arrays. Allows for dictionary
+    labels (Ex: L = l, l+ = l, l = l-,...). For the last nested level ignore
+    particle ordering. FIXME nesting? 
  
-         :param ptype1: first (nested) list of particle names, e.g. ['l','jet']
-         :param ptype2: second (nested) list of particle names
- 
-         :param useDict: use the translation dictionary, i.e. allow e to stand for e+ or e-, l+ to stand for e+ or mu+, etc
- 
-         :returns: boolean
-     """
-     import copy
+    :param ptype1: first (nested) list of particle names, e.g. ['l','jet']
+    :param ptype2: second (nested) list of particle names 
+    :param useDict: use the translation dictionary, i.e. allow e to stand for
+    e+ or e-, l+ to stand for e+ or mu+, etc 
+    :returns: boolean
+    
+    """
+    import copy
      
-     wrongFormat = False
-     if type(ptype1) != type(ptype2): return False
- #Check for nested arrays (should be in the standard notation [[[]],[[]]]):    
-     if type(ptype1) == type([]):
-         if len(ptype1) != len(ptype2): return False
-         for ib,br in enumerate(ptype1):
-             if type(br) != type(ptype2[ib]) or type(br) != type([]): wrongFormat = True
-             if len(ptype1[ib]) != len(ptype2[ib]): return False    #Check number of vertices in branch
-             for iv,vt in enumerate(br):
-                 if type(vt) != type(ptype2[ib][iv]) or type(vt) != type([]): wrongFormat = True
-                 if len(ptype1[ib][iv]) != len(ptype2[ib][iv]): return False #Check number of particles in vertex
-                 for ptc in ptype1[ib][iv]+ptype2[ib][iv]:
-                     if not ptc in PtcDic.keys()+Reven.values(): wrongFormat = True
-     if wrongFormat:     
-         print "[ParticleNames.simParticles]: Wrong input format!",ptype1,ptype2
-         return False
+    wrongFormat = False
+    if type(ptype1) != type(ptype2): return False
+#Check for nested arrays (should be in the standard notation [[[]],[[]]]):    
+    if type(ptype1) == type([]):
+        if len(ptype1) != len(ptype2): return False
+        for ib,br in enumerate(ptype1):
+            if type(br) != type(ptype2[ib]) or type(br) != type([]): wrongFormat = True
+            if len(ptype1[ib]) != len(ptype2[ib]): return False    #Check number of vertices in branch
+            for iv,vt in enumerate(br):
+                if type(vt) != type(ptype2[ib][iv]) or type(vt) != type([]): wrongFormat = True
+                if len(ptype1[ib][iv]) != len(ptype2[ib][iv]): return False #Check number of particles in vertex
+                for ptc in ptype1[ib][iv]+ptype2[ib][iv]:
+                    if not ptc in PtcDic.keys()+Reven.values(): wrongFormat = True
+    if wrongFormat:     
+        logger.error("Wrong input format!" + str(ptype1) + " " + str(ptype2))
+        return False
      
- #Put input in standard notation
-     if type(ptype1) == type("str"):
-         ptype1v = [[[ptype1]],[[ptype1]]]
-         ptype2v = [[[ptype2]],[[ptype2]]]
-     else:
-         ptype1v = ptype1[:]
-         ptype2v = ptype2[:]
- 
-     for ibr,br in enumerate(ptype1v): #Loop through branches
-         for iv,vt in enumerate(br): #Loop over vertices    
- #Check    if lists match, ignoring possible dictionary entries
-             pmatch = True
-             for ptc in ptype1v[ibr][iv]:
-                 if ptype1v[ibr][iv].count(ptc) != ptype2v[ibr][iv].count(ptc): pmatch = False
-             if pmatch: continue
-             elif not useDict: return False        
- #If they do not match and useDict=True, generate all possible lists from dictionary entries:
-             allptcs = [[ptype1v[ibr][iv]],[ptype2v[ibr][iv]]]
-             for allpt in allptcs:
-                 ptc0 = copy.deepcopy(allpt[0])
-                 for ipt in range(len(ptc0)):
-                     if PtcDic.has_key(ptc0[ipt]):
-                         for jpt in range(len(allpt)):
-                             if allpt[jpt] == []: continue
-                             newptc = copy.deepcopy(allpt[jpt])
-                             for ptc in PtcDic[ptc0[ipt]]:
-                                 newptc[ipt] = ptc
-                                 allpt.append(copy.deepcopy(newptc))
-                             allpt[jpt] = []
-                 while allpt.count([]) > 0: allpt.remove([])
+#Put input in standard notation
+    if type(ptype1) == type("str"):
+        ptype1v = [[[ptype1]],[[ptype1]]]
+        ptype2v = [[[ptype2]],[[ptype2]]]
+    else:
+        ptype1v = ptype1[:]
+        ptype2v = ptype2[:]
+
+    for ibr,br in enumerate(ptype1v): #Loop through branches
+        for iv,vt in enumerate(br): #Loop over vertices    
+#Check    if lists match, ignoring possible dictionary entries
+            pmatch = True
+            for ptc in ptype1v[ibr][iv]:
+                if ptype1v[ibr][iv].count(ptc) != ptype2v[ibr][iv].count(ptc): pmatch = False
+            if pmatch: continue
+            elif not useDict: return False        
+#If they do not match and useDict=True, generate all possible lists from dictionary entries:
+            allptcs = [[ptype1v[ibr][iv]],[ptype2v[ibr][iv]]]
+            for allpt in allptcs:
+                ptc0 = copy.deepcopy(allpt[0])
+                for ipt in range(len(ptc0)):
+                    if PtcDic.has_key(ptc0[ipt]):
+                        for jpt in range(len(allpt)):
+                            if allpt[jpt] == []: continue
+                            newptc = copy.deepcopy(allpt[jpt])
+                            for ptc in PtcDic[ptc0[ipt]]:
+                                newptc[ipt] = ptc
+                                allpt.append(copy.deepcopy(newptc))
+                            allpt[jpt] = []
+                while allpt.count([]) > 0: allpt.remove([])
                  
- #Now compare all possibilities:
-             match = False
-             iA = 0
-             while not match and iA < len(allptcs[0]):
-                 ptcA = allptcs[0][iA]
-                 for ptcB in allptcs[1]:
-                     if len(ptcA) != len(ptcB): return False
-                     pmatch = True
-                     for ptc in ptcA:
-                         if ptcA.count(ptc) != ptcB.count(ptc): pmatch = False
-                     if pmatch:
-                         match = True
-                         break
-                 iA += 1
-             if not match: return False
+#Now compare all possibilities:
+            match = False
+            iA = 0
+            while not match and iA < len(allptcs[0]):
+                ptcA = allptcs[0][iA]
+                for ptcB in allptcs[1]:
+                    if len(ptcA) != len(ptcB): return False
+                    pmatch = True
+                    for ptc in ptcA:
+                        if ptcA.count(ptc) != ptcB.count(ptc): pmatch = False
+                    if pmatch:
+                        match = True
+                        break
+                iA += 1
+            if not match: return False
  
- #if it reached here, entries are similar:
-     return True
+#if it reached here, entries are similar:
+    return True
 
 
 Rodd={
