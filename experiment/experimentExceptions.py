fec2e24a66ce4b8a2f7ca94c2d83e9b275fb4a9d
Wolfgang Magerl <wolfgang.magerl@gmail.com>
2014-03-24 19:40:45 +0100

    Improved code structure.

--- experiment/experimentExceptions.py
+++ experiment/experimentExceptions.py
@@ -2,13 +2,16 @@
 
 """
 .. module:: experiment.experimentExceptions
-   :synopsis: This module contains exception classes for experiment-specific use.
+   :synopsis: This module contains exception classes for experiment-specific
+   use.
     
 .. moduleauthor:: Wolfgang magerl <wolfgang.magerl@gmail.com>
     
 """
 
 class MetaInfoError(Exception):
-    """Exception class that is raised when a meta info field cannot be found
+    """
+    Exception class that is raised when a meta info field cannot be found.
+    
     """
     pass
