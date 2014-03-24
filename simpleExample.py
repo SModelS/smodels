8da97155848124c93dc9358a3fff7348de886969
Wolfgang Magerl <wolfgang.magerl@gmail.com>
2014-03-24 15:57:16 +0100

    Added support for Python 3 and improved code structure.

--- simpleExample.py
+++ simpleExample.py
@@ -6,15 +6,15 @@
 
 """
 
-from tools import set_path
-import tools.loggingConfiguration
-tools.loggingConfiguration.configure()
-from theory import slhaDecomposer, lheDecomposer
+from __future__ import print_function
+from theory import slhaDecomposer
+from theory import lheDecomposer
 from tools.physicsUnits import addunit
 from experiment import smsAnalysisFactory
 from theory.theoryPrediction import theoryPredictionFor
-import logging
-#logger = logging.getLogger(__name__)
+import tools.loggingConfiguration
+
+tools.loggingConfiguration.configure()
 
 def main():
     """
@@ -27,30 +27,32 @@ def main():
     lhefile = "inputFiles/lhe/ued_1.lhe"
     mingap = addunit(5.,'GeV')
     sigmacut = addunit(0.1,'fb')
-    smsTopList = slhaDecomposer.decompose(slhafile, sigmacut, doCompress=True
-                                          , doInvisible=True, minmassgap=mingap)
-#     smsTopList = lheDecomposer.decompose(lhefile, doCompress=True, doInvisible=True, minmassgap=mingap)
-#Print decomposition summary
+    smsTopList = slhaDecomposer.decompose(slhafile, sigmacut, doCompress=True,
+                                          doInvisible=True, minmassgap=mingap)
+    # smsTopList = lheDecomposer.decompose(lhefile, doCompress=True,
+    #                                      doInvisible=True, minmassgap=mingap)
+    # Print decomposition summary
     smsTopList.printout()
     
-#Load analyses
+    # Load analyses
     listOfAnalyses = smsAnalysisFactory.load()
-#Get theory prediction for each analysis and print basic output       
+    # Get theory prediction for each analysis and print basic output       
     for ana in listOfAnalyses:
         preds = theoryPredictionFor(ana, smsTopList)
-        if not preds: continue
-        print ana.label
+        if not preds:
+            continue
+        print(ana.label)
         for pred in preds:
-            print 'mass=', pred.mass
-            print 'theory prediction=', pred.value
-            print 'theory conditions:'
+            print('mass=', pred.mass)
+            print('theory prediction=', pred.value)
+            print('theory conditions:')
             if not pred.conditions:
-                print pred.conditions
+                print(pred.conditions)
             else:
                 for cond in pred.conditions:
-                    print pred.conditions[cond]
-            print 'experimental limit=',ana.getUpperLimitFor(pred.mass)
-            print '\n'
+                    print(pred.conditions[cond])
+            print('experimental limit=',ana.getUpperLimitFor(pred.mass))
+            print('\n')
     
 
 if __name__ == '__main__':
