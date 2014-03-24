e1acd5a7241d078879e3551b498d402db8b58805
Wolfgang Magerl <wolfgang.magerl@gmail.com>
2014-03-24 20:31:30 +0100

    Added support for Python 3 and improved code structure.

--- tools/physicsUnits.py
+++ tools/physicsUnits.py
@@ -2,15 +2,13 @@
 
 """
 .. module:: physicsUnits
-    :synopsis: This introduces physical units (GeV,fb) to the framework
+   :synopsis: This introduces physical units (GeV, fb) to the framework.
 
 .. moduleauthor:: Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
 
 """
 
-
-from unum import Unum
-import set_path
+import unum
 from tools import logger
 
 
@@ -24,84 +22,90 @@ from tools import logger
 # or sth similar
 
 # can be switched off with a single switch
-useUnits=True
+useUnits = True
 
 #if len(Unum.getUnitTable())==0: # make sure we define only once
-Unum.reset()
-Unum.VALUE_FORMAT = "%0.2E"
-Unum.UNIT_HIDE_EMPTY = True
+unum.Unum.reset()
+unum.VALUE_FORMAT = "%0.2E"
+unum.UNIT_HIDE_EMPTY = True
 
-fb=Unum.unit('fb')
-pb=Unum.unit('pb', 1000 * fb)
+fb = unum.Unum.unit('fb')
+pb = unum.Unum.unit('pb', 1000 * fb)
 
-eV=Unum.unit('eV')
-keV=Unum.unit('keV',10**3*eV)
-MeV=Unum.unit('MeV',10**6*eV)
-GeV=Unum.unit('GeV',10**9*eV)
-TeV=Unum.unit('TeV',10**12*eV)
+eV = unum.Unum.unit('eV')
+keV = unum.Unum.unit('keV',10**3*eV)
+MeV = unum.Unum.unit('MeV',10**6*eV)
+GeV = unum.Unum.unit('GeV',10**9*eV)
+TeV = unum.Unum.unit('TeV',10**12*eV)
 
-def addunit ( value, unitstring ):
-  """ a function that can add units to values, but also
-      makes it easy to turn this functionality off, in case "units" isnt installed
-      """
-  if value==None: return value
-  if not useUnits: return value
-  if useUnits: 
-    import types as t
-    ## for convenience, we add units also to tuples, lists, and dictionaries
-    if type(value)==t.ListType:
-      return [ addunit(x,unitstring) for x in value ]
-    if type(value)==t.TupleType:
-      return tuple ( [ addunit(x,unitstring) for x in value ] )
-    if type(value)==t.DictType:
-      ret={}
-      for (k,v) in value.items():
-        ret[k]=addunit(v,unitstring)
-      return ret
-    if type(value)!=t.FloatType and type(value)!=t.IntType:
-      return value
-    if unitstring=="GeV":
-      return value * GeV
-    if unitstring=="TeV":
-      return value * TeV
-    if unitstring=="fb":
-      return value * fb
-    if unitstring=="pb":
-      return value * pb
-    if unitstring=="fb-1":
-      return value / fb
-    logger.warning ( "dont know what to do with unit "+unitstring )
-  return value
 
-def rmvunit ( value, unitstring ):
-  """ a function that can remove units from values, but also
-      makes it easy to turn this functionality off, in case "units" isnt installed
-      """
-  if not useUnits: return value
-  if useUnits: 
-    import types as t
-    if type(value) != type(1.*GeV):
-      return value
-    if unitstring=="GeV":
-      return value.asNumber(GeV)
-    if unitstring=="TeV":
-      return value.asNumber(TeV)
-    if unitstring=="fb":
-      return value.asNumber(fb)
-    if unitstring=="pb":
-      return value.asNumber(pb)
-    if unitstring=="fb-1":
-      return value.asNumber(1/fb)
-
-    logger.warning ( "dont know what to do with unit "+unitstring )
+def addunit(value, unitstring):
+    """
+    Adds units to values, but also makes it easy to turn this functionality
+    off, in case "units" isn't installed.
+    
+    """
+    if value == None:
+        return value
+    if not useUnits:
+        return value
+    if useUnits:
+        # for convenience, we add units also to tuples, lists, and dictionaries
+        if isinstance(value, list):
+            return [addunit(x,unitstring) for x in value]
+        if isinstance(value, tuple):
+            return tuple([addunit(x,unitstring) for x in value])
+        if isinstance(value, dict):
+            ret = {}
+            for (k, v) in value.items():
+                ret[k] = addunit(v,unitstring)
+            return ret
+        if not isinstance(value, float) and not isinstance(value, int):
+            return value
+        if unitstring == "GeV":
+            return value * GeV
+        if unitstring == "TeV":
+            return value * TeV
+        if unitstring == "fb":
+            return value * fb
+        if unitstring == "pb":
+            return value * pb
+        if unitstring == "fb-1":
+            return value / fb
+        logger.warning("Unknown unit: " + unitstring)
     return value
 
 
+def rmvunit ( value, unitstring ):
+    """
+    Removes units from values, but also makes it easy to turn this
+    functionality off, in case "units" isn't installed.
+    
+    """
+    if not useUnits: return value
+    if useUnits: 
+        if type(value) != type(1.*GeV):
+            return value
+        if unitstring == "GeV":
+            return value.asNumber(GeV)
+        if unitstring == "TeV":
+            return value.asNumber(TeV)
+        if unitstring == "fb":
+            return value.asNumber(fb)
+        if unitstring == "pb":
+            return value.asNumber(pb)
+        if unitstring == "fb-1":
+            return value.asNumber(1/fb)
+        logger.warning("Unknown unit: " + unitstring)
+        return value
+
+
 if __name__ == "__main__":
-    """ called as script, will print some physicsUnits """
-    three=addunit(3.0,"fb")
-    print three,"=",three.asUnit(pb)
-    seven=addunit(7.,"TeV")
-    print seven,"=",seven.asUnit(GeV)
-
-
+    """
+    Called as script, will print some physicsUnits.
+    
+    """
+    three = addunit(3.0,"fb")
+    print(three, "=", three.asUnit(pb))
+    seven = addunit(7.,"TeV")
+    print(seven, "=", seven.asUnit(GeV))
