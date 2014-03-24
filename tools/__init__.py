371e4a372c9b70e27ab4539899ebb2016b6dc4b2
Wolfgang Magerl <wolfgang.magerl@gmail.com>
2014-03-24 15:59:32 +0100

    Reverted logging configuration.

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,5 +3,9 @@ This package contains all code that cannot be classified as being part of
 *experiment* or *theory*.
 
 """
-import loggingConfiguration 
-logger = loggingConfiguration.createLogger(__name__)
+import os
+import logging.config
+
+basepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+logging.config.fileConfig('%s/etc/logging.conf' % basepath, disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
