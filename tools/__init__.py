e2fee4407a616cd5e4663bb8c029a39f3d3278bf
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-04-09 11:35:30 +0200

    added version check

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,9 +3,12 @@ This package contains all code that cannot be classified as being part of
 *experiment* or *theory*.
 
 """
-import os
+import os, sys
 import logging.config
 
 basepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-logging.config.fileConfig('%s/etc/logging.conf' % basepath, disable_existing_loggers=False)
+if sys.version_info < (2 , 5 ):
+    logging.config.fileConfig('%s/etc/logging.conf' % basepath)
+else:
+    logging.config.fileConfig('%s/etc/logging.conf' % basepath, disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
