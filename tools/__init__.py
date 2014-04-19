d9d416d5df7eed64d14d3b2898fd9af57da35517
Wolfgang Magerl <wolfgang.magerl@gmail.com>
2014-04-19 19:33:55 +0200

    Improved code structure

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,12 +3,15 @@ This package contains all code that cannot be classified as being part of
 *experiment* or *theory*.
 
 """
-import os, sys
+import os
+import sys
 import logging.config
 
 basepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-if sys.version_info < (2 , 5 ):
+
+if sys.version_info < (2, 5):
     logging.config.fileConfig('%s/etc/logging.conf' % basepath)
 else:
     logging.config.fileConfig('%s/etc/logging.conf' % basepath, disable_existing_loggers=False)
+
 logger = logging.getLogger(__name__)
