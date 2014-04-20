10c6eeeb2c5250c0ec3b44e789c4ff29a79661ab
Wolfgang Magerl <wolfgang.magerl@gmail.com>
2014-04-20 18:57:44 +0200

    Improved docstrings and removed redundant function.

--- experiment/smsHelpers.py
+++ experiment/smsHelpers.py
@@ -14,46 +14,40 @@
 from os.path import os
 import logging
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__) # pylint: disable-msg=C0103
 
-base = "/afs/hephy.at/user/w/walten/public/sms/"
-runs = ["8TeV", "2012", "ATLAS8TeV", "2011", "RPV8", "RPV7"]
-runs_ = {}
-mlines = {}
-infoFields = {}
-pMI_ = {}
-upperLimitDict = {}
-expupperLimitDict = {}
+base = "/afs/hephy.at/user/w/walten/public/sms/" # pylint: disable-msg=C0103
+runs = ["8TeV", "2012", "ATLAS8TeV", "2011", "RPV8", "RPV7"] # pylint: disable-msg=C0103
+runs_ = {} # pylint: disable-msg=C0103
+mlines = {} # pylint: disable-msg=C0103
+infoFields = {} # pylint: disable-msg=C0103
+pMI_ = {} # pylint: disable-msg=C0103
+upperLimitDict = {} # pylint: disable-msg=C0103
+expupperLimitDict = {} # pylint: disable-msg=C0103
 
 
 def getRun(analysis, run=None):
     """
-    Search for an analysis. If a specific run is given, then just check if
-    results are present
+    Search for an analysis.
+    
+    If a specific run is given, check if results are present.
 
-    :returns: The run or None, if not found.
+    :returns: run; None, if not found;
     
     """
-    key=analysis+str(run)
-    if key in runs_: return runs_[key]
+    key = analysis + str(run)
+    if key in runs_:
+        return runs_[key]
     if run:
-        if os.path.exists ( "%s/%s/%s" % ( base, run, analysis ) ):
-            runs_[key]=run
+        if os.path.exists("%s/%s/%s" % (base, run, analysis)):
+            runs_[key] = run
             return run
     for trun in runs:
-        if os.path.exists ( "%s/%s/%s" % ( base, trun, analysis ) ):
-            runs_[key]=trun
+        if os.path.exists("%s/%s/%s" % (base, trun, analysis)):
+            runs_[key] = trun
             return trun
-    runs_[key]=None
+    runs_[key] = None
     return None
-
-
-def constraints(analysis, run):
-    """
-    Get all the constraints for an analysis-run pair.
-    
-    """
-    return getLines(analysis, run, "constraint")
 
 
 def getLines(analysis, run, label="condition"):
@@ -78,7 +72,7 @@ def getLines(analysis, run, label="condition"):
             line = line[:line.find("#")].strip()
         if line == "":
             continue
-        tokens = line.split(":",1)
+        tokens = line.split(":", 1)
         if not len(tokens) == 2:
             logger.warn("Cannot parse line: %s in %s" % (line, info))
             continue
@@ -109,15 +103,14 @@ def getMetaInfoField(analysis, field, run=None):
     run = getRun(analysis, run)
     metainfo = parseMetaInfo(analysis, run)
     if not field in metainfo:
-        infoFields[key]=None
+        infoFields[key] = None
         return infoFields[key]
-        # raise MetaInfoError("field ``%s'' not found for ``%s''" % (field,analysis) )
-    f=metainfo[field]
+    f = metainfo[field]
     if len(f) == 0: 
         infoFields[key] = f
         return f
     while f[0] == ' ':
-        f=f[1:]
+        f = f[1:]
     if f[-1] == '\n':
         f = f[:-1]
     infoFields[key] = f
@@ -144,13 +137,13 @@ def parseMetaInfo ( analysis, run ):
     for line in lines:
         if line.find("#") > -1:
             line = line[:line.find("#")].strip()
-        line = line.replace("\n","")
+        line = line.replace("\n", "")
         line = line.strip()
         if line == "":
             continue
-        tokens = line.split(":",1)
+        tokens = line.split(":", 1)
         if not len(tokens) == 2:
-            logger.error("Cannot parse line: ``%s'' in ``%s''" % (line, info))
+            logger.error("Cannot parse line: '%s' in '%s'" % (line, info))
             continue
         if tokens[0] == "exclusions":
             # Ignore exclusions
@@ -160,49 +153,51 @@ def parseMetaInfo ( analysis, run ):
     return ret
 
 
-def getUpperLimitDictionary(analysis, topo, run, expected=False):
+def getUpperLimitDictionary(analysis, topology, run, expected=False):
     """
     TODO: write docstring
     
     """
-    key = analysis + topo + str(run)
+    key = analysis + topology + str(run)
     if expected:
         if key in expupperLimitDict:
             return expupperLimitDict[key]
     else:
         if key in upperLimitDict:
             return upperLimitDict[key]
-    dictfile="%s/%s/%s/sms.py" % (base, run, analysis)
+    dictfile = "%s/%s/%s/sms.py" % (base, run, analysis)
     if not os.path.exists(dictfile):
         logger.warn("Dictionary file %s does not exist." % dictfile)
         upperLimitDict[key] = None
         return None
-    Locals = {}
-    execfile(dictfile, Locals)
+    localsDictionary = {}
+    execfile(dictfile, localsDictionary)
     if expected:
-        if not Locals.has_key("ExpectedDict"):
-            logger.warn("Expected dictionary is missing for analysis " + analysis)
+        if not localsDictionary.has_key("ExpectedDict"):
+            logger.warn("Expected dictionary is missing for analysis", \
+                        analysis)
             return None
-        Dict = Locals["ExpectedDict"]
-        if not Dict.has_key(topo):
-            logger.warn("Dictionary does not have topology " + topo )
+        dictionary = localsDictionary["ExpectedDict"]
+        if not dictionary.has_key(topology):
+            logger.warn("Dictionary does not have topology", topology)
             expupperLimitDict[key] = None
             return None
-        expupperLimitDict[key]=Dict[topo]
+        expupperLimitDict[key] = dictionary[topology]
     else:
-        if not Locals.has_key("Dict"):
-            logger.warn("Observed dictionary is missing for analysis " + analysis)
+        if not localsDictionary.has_key("Dict"):
+            logger.warn("Observed dictionary is missing for analysis", \
+                        analysis)
             return None
-        Dict=Locals["Dict"]
-        if not Dict.has_key(topo):
-            logger.warn("Dictionary does not have topology " + topo )
+        dictionary = localsDictionary["Dict"]
+        if not dictionary.has_key(topology):
+            logger.warn("Dictionary does not have topology", topology)
             upperLimitDict[key] = None
             return None
-        upperLimitDict[key] = Dict[topo]
-    return Dict[topo]
+        upperLimitDict[key] = dictionary[topology]
+    return dictionary[topology]
 
 
-def hasDictionary(analysis, run=None, topo=None):
+def hasDictionary(analysis, run=None, topology=None):
     """
     Check if available upper limits are in dictionary format.
     
@@ -211,14 +206,14 @@ def hasDictionary(analysis, run=None, topo=None):
         run = getRun(analysis)
     dictfile = "%s/%s/%s/sms.py" % (base, run, analysis)
     if os.path.exists(dictfile):
-        if topo == None:
+        if topology == None:
             return True
-        Locals = {}
-        execfile(dictfile, Locals)
-        Dict = Locals["Dict"]
-        if Dict.has_key(topo):
+        localsDictionary = {}
+        execfile(dictfile, localsDictionary)
+        dictionary = locals["Dict"]
+        if dictionary.has_key(topology):
             return True
         return False
 
-    logger.warn("Dictionary file %s does not exist." % dictfile )
+    logger.warn("Dictionary file %s does not exist." % dictfile)
     return False
