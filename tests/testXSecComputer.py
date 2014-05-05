ee9ff5fb1f3ca84ba8fb368f7c02f1705563fe25
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-05-05 11:57:16 +0200

    nll test started

--- tests/testXSecComputer.py
+++ tests/testXSecComputer.py
@@ -9,16 +9,19 @@
 """
 import unittest
 import setPath
-from tools import xsecComputer
-
+from smodels.tools import xsecComputer
+import logging.config
 
 class Test(unittest.TestCase):
-    def testXSecComputer(self):
+    def TestXSecComputer(self):
         w = xsecComputer.computeXSec(8,0,1000, "../inputFiles/slha/andrePT4.slha").getDictionary()
         w8lo= 1000 * w[(1000023, 1000024)]['8 TeV (LO)'].asNumber() 
         self.assertAlmostEqual(w8lo, 35.014621117 )  ## 35.01 fb
 
     def testSecond (self):
+        logging.config.fileConfig('./logging.conf' )
+        w = xsecComputer.computeXSec(8,2,1000, "../inputFiles/slha/andrePT4.slha").getDictionary()
+        print "w=",w
         self.assertEqual(3*4,12)
 
 if __name__ == "__main__":
