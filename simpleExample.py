b2d66d497db24b953f7579fb580ff5b28b317e5d
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-05-05 11:15:13 +0200

    corrected a few pylint issues

--- simpleExample.py
+++ simpleExample.py
@@ -10,46 +10,43 @@
 
 from __future__ import print_function
 from theory import slhaDecomposer
-from theory import lheDecomposer
+## from theory import lheDecomposer
 from tools.physicsUnits import addunit
 from experiment import smsAnalysisFactory
 from theory.theoryPrediction import theoryPredictionFor
-import logging
-
-logger = logging.getLogger(__name__) # pylint: disable-msg=C0103
-
-slhafile = 'inputFiles/slha/andrePT4.slha'
-lhefile = 'inputFiles/lhe/ued_1.lhe'
-
+## import logging
 
 def main():
     """
     Main program. Displays basic use case.
-    
+
     """
+    slhafile = 'inputFiles/slha/andrePT4.slha'
+    # lhefile = 'inputFiles/lhe/ued_1.lhe'
+
     # Decompose model (SLHA or LHE input):
-    sigmacut = addunit(0.1,'fb')
-    mingap = addunit(5.,'GeV')
-    
-    smsTopList = slhaDecomposer.decompose(slhafile, sigmacut, doCompress=True,
+    sigmacut = addunit(0.1, 'fb')
+    mingap = addunit(5., 'GeV')
+
+    smstoplist = slhaDecomposer.decompose(slhafile, sigmacut, doCompress=True,
                                           doInvisible=True, minmassgap=mingap)
-    # smsTopList = lheDecomposer.decompose(lhefile, doCompress=True,
+    # smstoplist = lheDecomposer.decompose(lhefile, doCompress=True,
     #                                      doInvisible=True, minmassgap=mingap)
-    
+
     # Print decomposition summary
-    smsTopList.printout()
-    
+    smstoplist.printout()
+
     # Load analyses
-    listOfAnalyses = smsAnalysisFactory.load()
-    
-    # Get theory prediction for each analysis and print basic output       
-    for analysis in listOfAnalyses:
-        theoryPredictions = theoryPredictionFor(analysis, smsTopList)
-        if not theoryPredictions:
+    listofanalyses = smsAnalysisFactory.load()
+
+    # Get theory prediction for each analysis and print basic output
+    for analysis in listofanalyses:
+        theorypredictions = theoryPredictionFor(analysis, smstoplist)
+        if not theorypredictions:
             continue
         print(analysis.label)
-        theoryPredictions.printout()
-    
+        theorypredictions.printout()
+
 
 if __name__ == '__main__':
     main()
