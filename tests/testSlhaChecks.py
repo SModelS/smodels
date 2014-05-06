80d7ed12b8ba1dd2e8a5a1f61b77998c43d4b6dd
Ursula Laa <Ursula.Laa@assoc.oeaw.ac.at>
2014-05-06 16:51:01 +0200

    added slha checker test for bad slha file

--- tests/testSlhaChecks.py
+++ tests/testSlhaChecks.py
@@ -18,7 +18,10 @@ class SlhaTest(unittest.TestCase):
         filename = "%sinputFiles/slha/andrePT4.slha" % (installDirectory() )
         st=slhaChecks.SlhaStatus(filename)
         self.assertEquals ( st.status, (1, 'Input file ok') )
-
+        
+        filename = "%sinputFiles/slha/nobdecay.slha" % (installDirectory() )
+        st=slhaChecks.SlhaStatus(filename)
+        self.assertEquals (st.status, (-1, '#ERROR: charged lsp or long-lived NLSP.\n#Warnings:\n#Empty decay block for PIDs, 1000005.\n#XSECTION table missing.\n#Charged NLSP is stable.\n'))
 
 if __name__ == "__main__":
     unittest.main()
