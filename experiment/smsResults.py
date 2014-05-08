104493eaa8a6224e9ce1ffd76a282db6946e62c1
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-05-08 12:50:46 +0200

    unique log filter

--- experiment/smsResults.py
+++ experiment/smsResults.py
@@ -10,14 +10,15 @@
 """
 
 from smodels.tools.physicsUnits import addunit, rmvunit
-from smodels.tools import rcFile
+## from smodels.tools import rcFile
 from smodels.experiment import smsHelpers
 from smodels.experiment.experimentExceptions import MetaInfoError
 from smodels.experiment.smsHelpers import getRun
 import logging
 
-
 logger = logging.getLogger(__name__)
+from smodels.tools.uniqueLogFilter import UniqueFilter
+logger.addFilter ( UniqueFilter() )
 
 allresults = {}
 constraints = {}
@@ -174,13 +175,12 @@ def setBase(base):
     """
     smsHelpers.base = base
 
-
 def getURL(analysis, run=None):
     """
     Get the URL of an analysis.
     
     """
-    logger.warning("getURL is deprecated")
+    logger.warning ( "getURL is deprecated" )
     return smsHelpers.getMetaInfoField(analysis, "url", run)
 
 
@@ -196,7 +196,7 @@ def getPAS(analysis, run=None):
     Get the PAS of an analysis.
     
     """
-    logger.warning("getPAS is deprecated")
+    logger.warning("getPAS is deprecated" )
     return smsHelpers.getMetaInfoField(analysis, "pas", run)
 
 
@@ -205,7 +205,7 @@ def getJournal(analysis, run=None):
     Get the journal of an analysis.
     
     """
-    logger.warning("getJournal is deprecated")
+    logger.warning ("getJournal is deprecated") 
     return smsHelpers.getMetaInfoField(analysis, "journal", run)
 
 
@@ -223,7 +223,7 @@ def getExperiment(analysis, run=None):
     Check if run is ATLAS8TeV, else return CMS.
     
     """
-    logger.warning("getExperiment is deprecated")
+    logger.warning("getExperiment is deprecated" )
     run1 = getRun(analysis, run)
     if run1.find("ATLAS") > -1:
         return "ATLAS"
@@ -235,7 +235,7 @@ def getComment(analysis, run=None):
     Get the comment of an analysis.
     
     """
-    logger.warning("getComment is deprecated")
+    logger.warning ( "getComment is deprecated" )
     return smsHelpers.getMetaInfoField(analysis, "comment", run)
 
 
