338093f7a646b786351e4f22ddfef8d753acbea5
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-06-03 14:40:27 +0200

    import absolute path added

--- simpleExample.py
+++ simpleExample.py
@@ -12,7 +12,7 @@ from __future__ import print_function
 import setPath ## set to python path for smodels
 ## from smodels import SModelS
 from smodels.theory import slhaDecomposer
-## from theory import lheDecomposer
+## from smodels.theory import lheDecomposer
 from smodels.tools.physicsUnits import fb, GeV
 from smodels.experiment import smsAnalysisFactory
 from smodels.theory.theoryPrediction import theoryPredictionFor
