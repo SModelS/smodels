a427f7a3843ccf9f52a8e8fc61ea00d7637fe750
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-07-02 10:38:39 +0200

    examples -> recipes

--- recipes/convert.sh
+++ recipes/convert.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in `ls recipce*.ipynb`; do
+for i in `ls recipe*.ipynb`; do
 	echo $i;
 	ipython nbconvert --template full --to python $i;
 	ipython nbconvert --template full --to html $i;
