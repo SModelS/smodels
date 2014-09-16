43e7aa1e0a764b9a1c5e09fceaf954dc8f2434db
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-09-16 15:06:10 +0200

    first version of setup.py

--- theory/__init__.py
+++ theory/__init__.py
@@ -11,6 +11,9 @@ import os
 import logging.config
 
 basepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if basepath.find(".egg")>0:
+    basepath=basepath[:-7] ## remove /smodels, if we're in an egg
+## print "basepath=",basepath
 logging.config.fileConfig('%s/etc/logging.conf' % basepath,
                           disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
