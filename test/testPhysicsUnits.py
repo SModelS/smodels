f88b7454a3ebef3528b997f3aa29cf1241514425
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-10-08 13:43:18 +0200

    starting with deprecation

--- test/testPhysicsUnits.py
+++ test/testPhysicsUnits.py
@@ -8,12 +8,12 @@
 
 """
 import unittest
-from smodels.tools.physicsUnits import addunit, rmvunit, GeV, TeV, eV, fb
+from smodels.tools.physicsUnits import GeV, TeV, fb, pb
 
 class PhysicsUnitsTest(unittest.TestCase):
     def testXSecs(self):
-        x1=addunit ( 3000.0, "fb" )
-        x2=addunit ( 3.0, "pb" )
+        x1= 3000.0 * fb
+        x2= 3.0 * pb
         self.assertAlmostEqual(x1,x2)
     def testEnergies(self):
         d1= 7000.0 * GeV
@@ -21,7 +21,7 @@ class PhysicsUnitsTest(unittest.TestCase):
         self.assertAlmostEqual(d1.asNumber(GeV),d2.asNumber(GeV))
 
     def testLumi(self):
-        d1=addunit ( 2.0, "fb-1" )
+        d1= 2.0 / fb 
         self.assertAlmostEqual ( (d1*.5*fb).asNumber(), 1.0 )
 
 
