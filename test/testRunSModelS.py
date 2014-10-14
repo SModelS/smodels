cb7a5d00b0b54622a90facd3399be15f2a036fef
Ursula Laa <Ursula.Laa@assoc.oeaw.ac.at>
2014-10-14 09:53:42 +0200

    fixed testRunSModelS, added necessary information to database, inputFiles

--- test/testRunSModelS.py
+++ test/testRunSModelS.py
@@ -10,17 +10,18 @@
 import unittest
 import os
 from smodels.installation import installDirectory
+from smodels.tools import summaryReader
 from runSModelS import main
 
 
 class RunSModelSTest(unittest.TestCase):
     def testGoodFile(self):
 
-        filename = "%sinputFiles/slha/andrePT4.slha" % (installDirectory() )
-        main(filename, outputfile="%s/test/unitTestOutput.txt" %installDirectory())
-        sample = open("%s/test/summary_default.txt" %installDirectory(),"r")
-        outputfile = open("%s/test/unitTestOutput.txt" %installDirectory(), "r")
-        self.assertEquals(outputfile.readlines(), sample.readlines())
+        filename = "%sinputFiles/slha/T1_7TeV.slha" % (installDirectory() )
+        main(filename, parameterfile="%s/test/testParameters.ini" %installDirectory(), outputfile="%s/test/unitTestOutput.txt" %installDirectory())
+        sample = summaryReader.Summary("%s/test/summary_default.txt" %installDirectory())
+        outputfile = summaryReader.Summary("%s/test/unitTestOutput.txt" %installDirectory())
+        self.assertEquals(sample, outputfile)
 
 if __name__ == "__main__":
     unittest.main()
