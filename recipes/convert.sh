70dfde07ff4ebbe3d46e74b9fd407b300c286cc0
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-10-30 16:02:28 +0100

    minor fixes

--- recipes/convert.sh
+++ recipes/convert.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 for i in `ls recipe*.ipynb`; do
+	echo 
+	echo
 	echo $i;
-	ipython nbconvert --template full --to python $i;
-	ipython nbconvert --template full --to html $i;
+	cmd="ipython nbconvert --to python $i"
+	echo $cmd;
+  `$cmd`;
+	cmd="ipython nbconvert --to html $i"
+	echo $cmd;
+  `$cmd`;
 done
