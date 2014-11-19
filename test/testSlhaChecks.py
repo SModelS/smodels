1adbb4c46cef5a5581d083364f0823a05d543798
Ursula Laa <Ursula.Laa@assoc.oeaw.ac.at>
2014-11-19 16:16:06 +0100

    updated tests for runSModelS, slhaChecks

--- test/testSlhaChecks.py
+++ test/testSlhaChecks.py
@@ -15,14 +15,14 @@ from smodels.tools import slhaChecks
 class SlhaTest(unittest.TestCase):
     def testGoodFile(self):
 
-        filename = "%sinputFiles/slha/andrePT4.slha" % (installDirectory() )
+        filename = "%sinputFiles/slha/strong.slha" % (installDirectory() )
         st=slhaChecks.SlhaStatus(filename)
         self.assertEquals ( st.status, (1, 'Input file ok') )
         
     def testBadFile(self):
         filename = "%sinputFiles/slha/nobdecay.slha" % (installDirectory() )
         st=slhaChecks.SlhaStatus(filename)
-        self.assertEquals (st.status, (-1, '#ERROR: special signatures in this point.\n#Warnings:\n#Empty decay block for PIDs, 1000005.\n#XSECTION table missing, will be computed by SModelS.\n#Charged NLSP is stable.\n'))
+        self.assertEquals (st.status, (-1, '#ERROR: special signatures in this point.\n#Warnings:\n#Empty decay block for PIDs, 1000005.\n#XSECTION table missing, will be computed by SModelS.\n#Charged stable particle 1000005\n\n'))
 
 if __name__ == "__main__":
     unittest.main()
