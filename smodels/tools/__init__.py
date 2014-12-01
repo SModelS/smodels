a8a90d5d65cdba493b5e3236b3727f738adecddb
Wolfgang Magerl <wolfgang.magerl@gmail.com>
2014-12-01 17:08:44 +0100

    Removed comments.
    Former-commit-id: ebd91aa0b7adfc5da808df00ffb04ff2170bea8b

--- smodels/tools/__init__.py
+++ smodels/tools/__init__.py
@@ -7,9 +7,7 @@
 import os
 import logging.config
 
-# print "tools.__init__: basepath=",os.path.abspath(os.path.join(os.path.dirname(__file__)))
 basepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
-# print "tools.__init__: basepath=",basepath
 logging.config.fileConfig('%s/etc/logging.conf' % basepath,
                           disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
