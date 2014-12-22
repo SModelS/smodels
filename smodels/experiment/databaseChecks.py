6fc1b71486d50c883d959ff6113bfc5b8da2ba5b
traub <michael.traub@gmx.at>
2014-12-22 13:26:33 +0100

    Created Class TxNameInfoChecker

--- smodels/experiment/databaseChecks.py
+++ smodels/experiment/databaseChecks.py
@@ -7,7 +7,7 @@
 
 """
 
-import logging, os, sys
+import logging, os, sys, re
 from smodels.experiment import infoObjects, dataObjects, analysisObjects
 
 FORMAT = '%(levelname)s in %(module)s.%(funcName)s() in %(lineno)s: %(message)s'
@@ -17,18 +17,225 @@ logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.INFO)
 
 
-def checkTxNameInfo(txnameInfoObj):
+class TxNameInfoChecker(object):
     """
     Checks if the info file object contains all the necessary fields
     and the field values are correctly set.
     """
+    def __init__(self,path,txnameInfoObj):
+        
+        self.testObj = txnameInfoObj
+        self.path = path
+        self.mandatoryFields = ['constraint','fuzzycondition','branchcondition']
+        
+        for field in self.mandatoryFields:
+            if not hasattr(self.testObj ,field):
+                logger.warning('in file %s\nmissing field %s for txName %s'\
+                %(self.path,field, self.testObj.txName))
+                
+        for attripute in self.testObj.__dict__:
+            if not attripute in TxNameInfoChecker.__dict__:
+                continue
+            value = getattr(self.testObj,attripute)
+            if not getattr(self,attripute)(value):
+                logger.warning('in file %s\ninvalid entry %s in field %s for txName %s'\
+                %(self.path, getattr(self.testObj,attripute), attripute, self.testObj.txName))
+                
+    def _constrints(self, value):
+        """find the constraint strings in a given string, 
+        and check the strings
+        :param:value: string
+        :return: list with constraints as string
+        """
+        startString = '[[['
+        endString = ']]]'
+  
+        constraints = []
+        for i in range(len(value)):
+            if value[i:i + len(startString)] == startString:
+                start = i
+            if value[i:i + len(endString)] == endString:
+                end = i + len(endString)
+                constraints.append(value[start:end])
 
+        try:
+            checkConstraints = [eval(constraint) for constraint in constraints]
+            for constraint in checkConstraints:
+                for branch in constraint:
+                    for vertex in branch:
+                        for particle in vertex:
+                            if not isinstance(particle,str): return False
+        except:
+            return False
+  
+        return constraints
 
-def checkGlobalInfo(globalInfoObj):
+    
+    def constraint(self,value):
+        
+        constraints = self._constrints(value)
+        if not constraints: return False
+  
+        for constraint in constraints:
+            value = value.replace(constraint,'1.0')
+        try:
+            value = eval(value)
+            if not isinstance(value,float): return False
+        except:
+            return False
+
+        return True
+        
+    def condition(self, value):
+  
+        if value == 'None': return True
+        constraints = self._constrints(value)
+        if not constraints: return False
+
+        value = value.replace('~','==')
+        for constraint in constraints:
+            value = value.replace(constraint,'1.0')
+        try:
+            value = eval(value)
+            if isinstance(value,tuple):
+                for supValue in value: 
+                    if not isinstance(supValue,bool): return False
+            elif not isinstance(value,bool): return False
+        except:
+            return False
+  
+        return True
+    
+    def fuzzycondition(self,value):
+        
+        if value == 'None': return True
+        constraints = self._constrints(value)
+        if not constraints: return False
+
+        for constraint in constraints:
+            value = value.replace(constraint,'1.0')
+        functions = value.split(';')
+        for function in functions:
+            function = function.strip()
+            if not function[:4] == 'Csim' and not function[:4] == 'Cgtr':
+                return False
+            if not function[4:5] == '(' or not function[-1:] == ')':
+                return False
+            arguments = function[5:-1].split(',')
+            try:
+                arguments = [eval(argument) for argument in arguments]
+                for argument in arguments:
+                    if not isinstance(argument,float): return False
+            except: 
+                return False
+   
+        return True
+    
+    def branchcondition(self,value):
+
+        if 'equal branches' == value: return True
+        return False
+    
+    def axes(self,value):
+        
+        values = value.split('_')
+        values = [value.strip() for value in values]
+        if not value[:2] == 'Eq': return False
+        if not value[2:3] == '(' or not value[-1:] == ')':
+            return  False
+        if not ',' in value[3:-1]: return False
+        return True
+    
+    def txName(self, value):
+        
+        if not value[:1] == 'T': return False
+        return True
+
+    
+
+def globalInfoChecker(object):
+    
+    
     """
     Checks if the info file object contains all the necessary fields
     and the field values are correctly set.
     """
+    
+    def __init__(self,path,globalInfoObj):
+        
+        self.testObj = txnameInfoObj
+        self.path = path
+        self.mandatoryFields = ['sqrts', 'lumi', 'id', 'url', 'digitaldata']
+        
+        for field in self.mandatoryFields:
+            if not hasattr(self.testObj ,field):
+                logger.warning('in file: %s\nmissing field %s'\
+                %(self.path,field))
+                
+        for attripute in self.testObj.__dict__:
+            if not attripute in globalInfoChecker.__dict__:
+                continue
+            value = getattr(self.testObj,attripute)
+            if not getattr(self,attripute)(value):
+                logger.warning('in file %s\ninvalid entry %s in field %s'\
+                %(self.path, getattr(self.testObj,attripute), attripute))
+        
+    
+    def sqrts(self,value):
+        
+        pass
+    
+    def lumi(self,lumi):
+        
+        pass
+    
+    def id(self,value):
+        
+        pass
+    
+    def url(self,value):
+        
+        pass
+    
+    def digitaldata(self,value):
+        
+        pass
+  
+    def publication(self,value):
+        
+        pass
+    
+    def arxiv(self,value):
+        
+        pass
+    
+    def superseded_by(self,value):
+        
+        pass
+    
+    def supersedes(self, value):
+        
+        pass
+    
+    def checked(self, value):
+        
+        pass
+    
+    def private(self, value):
+        
+        pass
+    
+    def prettyname(self, value):
+    
+        pass
+    
+    def contact(self, value):
+        
+        pass
+    
+    def validated(self, value):
+        
+        pass
 
 
 def checkInfoFile(infoFileObj):
@@ -37,7 +244,7 @@ def checkInfoFile(infoFileObj):
     and the field values are correctly set.
     """
     
-    globalFields = infoFileObj.globalInfo.getattr()
+    pass
     
     
     
@@ -56,8 +263,22 @@ def checkExpResult(expResultObj):
     and the field values are correctly set.
     """  
     
+    pass
+    
 def checkDataBase(dataBaseObj):
     """
     Checks if the info file object contains all the necessary fields
     and the field values are correctly set.
     """       
+    
+    pass
+
+class Test(object):
+    
+    def __init__(self):
+        self.constraint = "71.*([[['mu+','mu-']],[['l','nu']]] + [[['e+','e-']],[['l','nu']]])"
+        self.fuzzycondition = "Cgtr([[['b'],['L','nu']],[['b'],['jet','jet']]],3.*[[['b'],['ta','nu']],[['b'],['jet','jet']]]);Cgtr([[['b'],['L','nu']],[['b'],['jet','jet']]],3.*[[['b'],['e','nu']],[['b'],['jet','jet']]])"
+        self.axes = 'Eq(y,lsp)_Eq(x,mother)'
+        self.branchcondition = 'equal branches'
+        self.condition = "[[['mu+']],[['mu-']]] > [[['e+']],[['e-']]]"
+        self.txName = 'T6WW'
