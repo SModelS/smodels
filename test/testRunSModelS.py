9307985c9fae20fa1cd7cba5a2f1a7ca8b56819c
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2015-01-07 17:18:28 +0100

    fixed last unit test

--- test/testRunSModelS.py
+++ test/testRunSModelS.py
@@ -16,7 +16,7 @@ from runSModelS import main
 class RunSModelSTest(unittest.TestCase):
     def testGoodFile(self):
 
-        filename = "%s/inputFiles/slha/lightSquarks.slha" % (installDirectory() )
+        filename = "%s/inputFiles/slha/gluino_squarks.slha" % (installDirectory() )
 #        print "filename=",filename
         main(filename, 
              parameterFile="%s/test/testParameters.ini" %installDirectory(), 
