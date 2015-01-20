e9274feaff2d97005763f30b1d9de22cd9a5487e
walten <wolfgang.waltenberger@gmail.com>
2015-01-20 10:50:53 -0200

    adding distribution tool
    Former-commit-id: 42d69db10add2d9b044b042675780b6c536612c2

---
+++ distribution/createTarball.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+"""
+.. module:: createTarball
+   :synopsis: script that is meant to create the distribution tarball
+
+.. moduleauthor:: Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
+   
+"""
+
+
+import sys
+import commands
+import os
+
+def produceTarball():
+    """ finally, create the tarball itself """
+    commands.getoutput( "cd ../../; tar czvf smodels.tar.gz smodels/")
+
+def getVersion():
+    """ obtain the smodels version """
+    sys.path.append("../")
+    from smodels import installation
+    return installation.version()
+
+def mkdir( dirname ):
+    """ create the directory which we will create the tarball with """
+    print "Creating temporary directory",dirname
+    o=commands.getoutput ( "mkdir -p %s" % dirname )
+    print o
+
+def rmdir( dirname ):
+    """ remove the temporary directory """
+    if os.path.exists(dirname):
+        print "Removing temporary directory",dirname
+        o=commands.getoutput ( "rm -r %s" % dirname )
+        print o
+
+def cp( dirname ):
+    """ copy files to temp directory """
+    print "Copying the files to",dirname
+    for i in os.listdir("../"):
+        if i not in [ ".git", ".gitignore", "distribution", "test" ]:
+      #      print i
+            o=commands.getoutput ( "cp -r ../%s %s/" % (i,dirname) )
+    #print o
+
+def rmpyc ( dirname ):
+    """ remove all pyc files """
+    print "Removing all pyc files ... "
+    o=commands.getoutput("cd %s; rm -f *.pyc */*.pyc */*/*.pyc" % dirname )
+    print o
+
+def makeClean ():
+    """ perform 'make clean' in host directory """
+    print "Make clean ...."
+    o=commands.getoutput("cd ../lib/ ; make clean" )
+    print o
+
+def fetchDatabase(dirname):
+    """ git-pull the database """
+    print "git pull the database """
+    o=commands.getoutput( "cd %s; git clone git@smodels.hephy.at:smodels-database " % dirname )
+    print o
+
+def createTarball(version,dirname):
+    """ finally create the tarball """
+    print "Create tarball smodels-v%s.tar.gz" % version
+    o=commands.getoutput("tar czvf smodels-v%s.tar.gz %s" % (version,dirname) )
+    print o
+
+
+def create():
+    """ create a tarball for distribution """
+    version=getVersion()
+    dirname="smodels-v%s" % version
+    print "Creating tarball for distribution, version",version
+    makeClean()
+    rmdir(dirname)
+    mkdir(dirname)
+    cp(dirname)
+    rmpyc(dirname)
+    ## fetchDatabase(dirname)
+    createTarball(version,dirname)
+    rmdir(dirname)
+
+
+if __name__ == "__main__":
+    create()
