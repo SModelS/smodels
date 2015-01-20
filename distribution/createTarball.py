49d32252d651ae8cfce3ffdb7accafe5d8f3d8b6
walten <wolfgang.waltenberger@gmail.com>
2015-01-20 14:47:19 -0200

    continued working on tarball creation
    Former-commit-id: bd8702a707bbf664a03ecfe15e12cf96024ccafe

--- distribution/createTarball.py
+++ distribution/createTarball.py
@@ -12,9 +12,8 @@ import sys
 import commands
 import os
 
-def produceTarball():
-    """ finally, create the tarball itself """
-    commands.getoutput( "cd ../../; tar czvf smodels.tar.gz smodels/")
+RED="[31;11m"
+RESET="[37;0m" 
 
 def getVersion():
     """ obtain the smodels version """
@@ -22,78 +21,104 @@ def getVersion():
     from smodels import installation
     return installation.version()
 
-def mkdir( dirname ):
+version=getVersion()
+dirname="smodels-v%s" % version
+
+def mkdir( ):
     """ create the directory which we will create the tarball with """
-    print "Creating temporary directory",dirname
+    print RED,"Creating temporary directory",dirname,RESET
     o=commands.getoutput ( "mkdir -p %s" % dirname )
     print o
 
-def rmdir( dirname ):
+def rmdir( ):
     """ remove the temporary directory """
     if os.path.exists(dirname):
-        print "Removing temporary directory",dirname
+        print RED,"Removing temporary directory",dirname,RESET
         o=commands.getoutput ( "rm -rf %s" % dirname )
         print o
 
-def cp( dirname ):
+def cp( ):
     """ copy files to temp directory """
-    print "Copying the files to",dirname
+    print RED,"Copying the files to",dirname,RESET
     for i in os.listdir("../"):
         if i not in [ ".git", ".gitignore", "distribution", "test" ]:
       #      print i
             o=commands.getoutput ( "cp -r ../%s %s/" % (i,dirname) )
     #print o
 
-def rmpyc ( dirname ):
+def rmpyc ( ):
     """ remove all pyc files """
-    print "Removing all pyc files ... "
+    print RED,"Removing all pyc files ... ",RESET
     o=commands.getoutput("cd %s; rm -f *.pyc */*.pyc */*/*.pyc" % dirname )
     print o
 
 def makeClean ():
     """ perform 'make clean' in host directory """
-    print "Make clean ...."
+    print RED,"Make clean ....",RESET
     o=commands.getoutput("cd ../lib/ ; make clean" )
     print o
 
-def fetchDatabase(version,dirname):
+def fetchDatabase():
     """ git-pull the database """
-    print "git clone the database """
+    print RED,"git clone the database ",RESET
     cmd="cd %s; git clone -b v%s git@smodels.hephy.at:smodels-database " % \
             (dirname, version)
     o=commands.getoutput( cmd )
     print o
 
-def createTarball(version,dirname):
+def createTarball():
     """ finally create the tarball """
-    print "Create tarball smodels-v%s.tar.gz" % version
+    print RED,"Create tarball smodels-v%s.tar.gz" % version, RESET
     o=commands.getoutput("tar czvf smodels-v%s.tar.gz %s" % (version, dirname) )
     print o
 
-def rmExtraFiles(dirname):
+def rmExtraFiles():
     """ remove a few more files """
 
-def convertRecipes(dirname):
+def convertRecipes():
     """ convert the recipes from .ipynb to .py and .html """
-    print "Converting the recipes"
+    print RED,"Converting the recipes", RESET
     cmd="cd %s/docs/Manual/recipes/; make convert remove_ipynbs" % dirname
     o=commands.getoutput ( cmd )
     print o
 
+def explode ( ):
+    """ explode the tarball """
+    cmd="tar xzvf smodels-v%s.tar.gz" % version
+    o=commands.getoutput ( cmd )
+
+def make ( ):
+    """ run make in dirname/lib """
+    print RED,"Now run make in dirname/lib", RESET
+    cmd="cd %s/lib; make" % dirname
+    o=commands.getoutput ( cmd )
+    print o
+
+def runExample ( ):
+    """ run Example.py """
+
+
+def test ( ):
+    """ test the tarball, explode it, run make, and runSModelS.py """
+    print RED,"Now test the setup",RESET
+    rmdir ( )
+    explode ( )
+    make ( )
+
+
 def create():
     """ create a tarball for distribution """
-    version=getVersion()
-    dirname="smodels-v%s" % version
-    print "Creating tarball for distribution, version",version
+    print RED,"Creating tarball for distribution, version",version,RESET
     makeClean()
-    rmdir(dirname)
-    mkdir(dirname)
-    cp(dirname)
-    rmpyc(dirname)
-    rmExtraFiles(dirname)
-    fetchDatabase(version,dirname)
-    convertRecipes(dirname)
-    createTarball(version,dirname)
+    rmdir()
+    mkdir()
+    cp()
+    rmpyc()
+    rmExtraFiles()
+    fetchDatabase()
+    convertRecipes()
+    createTarball()
+    test ( )
 ##     rmdir(dirname)
 
 
