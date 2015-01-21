5edde0216b1eeb82efa32bef269c81aeacbc87b0
Wolfgang Magerl <wolfgang.magerl@gmail.com>
2015-01-21 18:28:18 +0100

    Fixed code style.
    Former-commit-id: 678ef9aa7120ea12589b386594a593ee27957ed5

--- distribution/createTarball.py
+++ distribution/createTarball.py
@@ -2,7 +2,7 @@
 
 """
 .. module:: createTarball
-   :synopsis: script that is meant to create the distribution tarball
+   :synopsis: Script that is meant to create the distribution tarball
 
 .. moduleauthor:: Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
    
@@ -12,8 +12,8 @@ import sys
 import commands
 import os
 
-RED="\[31;11m"
-RESET="\[37;0m" 
+RED = "\[31;11m"
+RESET = "\[37;0m"
 
 def getVersion():
     """ obtain the smodels version """
@@ -21,56 +21,56 @@ def getVersion():
     from smodels import installation
     return installation.version()
 
-version=getVersion()
-dirname="smodels-v%s" % version
+version = getVersion()
+dirname = "smodels-v%s" % version
 
-def mkdir( ):
+def mkdir():
     """ create the directory which we will create the tarball with """
-    print RED,"Creating temporary directory",dirname,RESET
-    o=commands.getoutput ( "mkdir -p %s" % dirname )
+    print RED, "Creating temporary directory", dirname, RESET
+    o = commands.getoutput("mkdir -p %s" % dirname)
     print o
 
-def rmdir( ):
+def rmdir():
     """ remove the temporary directory """
     if os.path.exists(dirname):
-        print RED,"Removing temporary directory",dirname,RESET
-        o=commands.getoutput ( "rm -rf %s" % dirname )
+        print RED, "Removing temporary directory", dirname, RESET
+        o = commands.getoutput("rm -rf %s" % dirname)
         print o
 
-def cp( ):
+def cp():
     """ copy files to temp directory """
-    print RED,"Copying the files to",dirname,RESET
+    print RED, "Copying the files to", dirname, RESET
     for i in os.listdir("../"):
-        if i not in [ ".git", ".gitignore", "distribution", "test" ]:
-      #      print i
-            o=commands.getoutput ( "cp -r ../%s %s/" % (i,dirname) )
-    #print o
+        if i not in [".git", ".gitignore", "distribution", "test"]:
+            # print i
+            o = commands.getoutput("cp -r ../%s %s/" % (i, dirname))
+    # print o
 
-def rmpyc ( ):
+def rmpyc ():
     """ remove all pyc files """
-    print RED,"Removing all pyc files ... ",RESET
-    o=commands.getoutput("cd %s; rm -f *.pyc */*.pyc */*/*.pyc" % dirname )
+    print RED, "Removing all pyc files ... ", RESET
+    o = commands.getoutput("cd %s; rm -f *.pyc */*.pyc */*/*.pyc" % dirname)
     print o
 
 def makeClean ():
     """ perform 'make clean' in host directory """
-    print RED,"Make clean ....",RESET
-    o=commands.getoutput("cd ../lib/ ; make clean" )
+    print RED, "Make clean ....", RESET
+    o = commands.getoutput("cd ../lib/ ; make clean")
     print o
 
 def fetchDatabase():
     """ git-pull the database """
-    print RED,"git clone the database ... ",RESET
-    cmd="cd %s; git clone -b v%s git@smodels.hephy.at:smodels-database ;" \
+    print RED, "git clone the database ... ", RESET
+    cmd = "cd %s; git clone -b v%s git@smodels.hephy.at:smodels-database ;" \
         " rm -rf smodels-database/.git smodels-database/.gitignore " % \
             (dirname, version)
-    o=commands.getoutput( cmd )
+    o = commands.getoutput(cmd)
     print o
 
 def createTarball():
     """ finally create the tarball """
-    print RED,"Create tarball smodels-v%s.tar.gz" % version, RESET
-    o=commands.getoutput("tar czvf smodels-v%s.tar.gz %s" % (version, dirname) )
+    print RED, "Create tarball smodels-v%s.tar.gz" % version, RESET
+    o = commands.getoutput("tar czvf smodels-v%s.tar.gz %s" % (version, dirname))
     print o
 
 def rmExtraFiles():
@@ -78,45 +78,45 @@ def rmExtraFiles():
 
 def convertRecipes():
     """ convert the recipes from .ipynb to .py and .html """
-    print RED,"Converting the recipes", RESET
-    cmd="cd %s/docs/Manual/recipes/; make convert remove_ipynbs" % dirname
-    o=commands.getoutput ( cmd )
+    print RED, "Converting the recipes", RESET
+    cmd = "cd %s/docs/Manual/recipes/; make convert remove_ipynbs" % dirname
+    o = commands.getoutput (cmd)
     print o
 
-def explode ( ):
+def explode ():
     """ explode the tarball """
-    print RED,"Explode the tarball ...", RESET
-    cmd="tar xzvf smodels-v%s.tar.gz" % version
-    o=commands.getoutput ( cmd )
+    print RED, "Explode the tarball ...", RESET
+    cmd = "tar xzvf smodels-v%s.tar.gz" % version
+    o = commands.getoutput (cmd)
 
-def make ( ):
+def make ():
     """ run make in dirname/lib """
-    print RED,"Now run make in dirname/lib ...", RESET
-    cmd="cd %s/lib; make" % dirname
-    o=commands.getoutput ( cmd )
+    print RED, "Now run make in dirname/lib ...", RESET
+    cmd = "cd %s/lib; make" % dirname
+    o = commands.getoutput (cmd)
     print o
 
-def runExample ( ):
+def runExample ():
     """ run Example.py """
-    print RED,"Now run Example.py ...",RESET
-    cmd="cd %s/; ./Example.py" % dirname
-    o=commands.getoutput ( cmd )
+    print RED, "Now run Example.py ...", RESET
+    cmd = "cd %s/; ./Example.py" % dirname
+    o = commands.getoutput (cmd)
     print o
 
-def test ( ):
+def test ():
     """ test the tarball, explode it, run make, and runSModelS.py """
-    print RED,"--------------------------",RESET
-    print RED,"    Test the setup ...    ",RESET
-    print RED,"--------------------------",RESET
-    rmdir ( )
-    explode ( )
-    make ( )
+    print RED, "--------------------------", RESET
+    print RED, "    Test the setup ...    ", RESET
+    print RED, "--------------------------", RESET
+    rmdir ()
+    explode ()
+    make ()
     runExample()
 
 
 def create():
     """ create a tarball for distribution """
-    print RED,"Creating tarball for distribution, version",version,RESET
+    print RED, "Creating tarball for distribution, version", version, RESET
     makeClean()
     rmdir()
     mkdir()
@@ -126,8 +126,8 @@ def create():
     fetchDatabase()
     convertRecipes()
     createTarball()
-    test ( )
-##     rmdir(dirname)
+    test ()
+    # rmdir(dirname)
 
 
 if __name__ == "__main__":
