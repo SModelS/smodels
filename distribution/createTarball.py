7e6b61be6880bb4492811f4bae61169773ebec80
Wolfgang Magerl <wolfgang.magerl@gmail.com>
2015-01-21 18:35:26 +0100

    Fixed docstring style.
    Former-commit-id: b9dcd6dd8929f83bd65d87db92630c101e649d86

--- distribution/createTarball.py
+++ distribution/createTarball.py
@@ -12,11 +12,14 @@ import sys
 import commands
 import os
 
+
 RED = "\[31;11m"
 RESET = "\[37;0m"
 
+
 def getVersion():
-    """ obtain the smodels version """
+    """
+    Obtain the smodels version """
     sys.path.append("../")
     from smodels import installation
     return installation.version()
@@ -24,21 +27,30 @@ def getVersion():
 version = getVersion()
 dirname = "smodels-v%s" % version
 
+
 def mkdir():
-    """ create the directory which we will create the tarball with """
+    """
+    Create a temporary directory for creating the tarball.
+    """
     print RED, "Creating temporary directory", dirname, RESET
     o = commands.getoutput("mkdir -p %s" % dirname)
     print o
 
+
 def rmdir():
-    """ remove the temporary directory """
+    """
+    Remove the temporary directory.
+    """
     if os.path.exists(dirname):
         print RED, "Removing temporary directory", dirname, RESET
         o = commands.getoutput("rm -rf %s" % dirname)
         print o
 
+
 def cp():
-    """ copy files to temp directory """
+    """
+    Copy files to temporary directory.
+    """
     print RED, "Copying the files to", dirname, RESET
     for i in os.listdir("../"):
         if i not in [".git", ".gitignore", "distribution", "test"]:
@@ -46,20 +58,29 @@ def cp():
             o = commands.getoutput("cp -r ../%s %s/" % (i, dirname))
     # print o
 
+
 def rmpyc ():
-    """ remove all pyc files """
+    """
+    Remove .pyc files.
+    """
     print RED, "Removing all pyc files ... ", RESET
     o = commands.getoutput("cd %s; rm -f *.pyc */*.pyc */*/*.pyc" % dirname)
     print o
 
+
 def makeClean ():
-    """ perform 'make clean' in host directory """
+    """
+    Execute 'make clean' in host directory.
+    """
     print RED, "Make clean ....", RESET
     o = commands.getoutput("cd ../lib/ ; make clean")
     print o
 
+
 def fetchDatabase():
-    """ git-pull the database """
+    """
+    Execute 'git clone' to retrieve the database.
+    """
     print RED, "git clone the database ... ", RESET
     cmd = "cd %s; git clone -b v%s git@smodels.hephy.at:smodels-database ;" \
         " rm -rf smodels-database/.git smodels-database/.gitignore " % \
@@ -67,44 +88,66 @@ def fetchDatabase():
     o = commands.getoutput(cmd)
     print o
 
+
 def createTarball():
-    """ finally create the tarball """
+    """
+    Create the tarball.
+    """
     print RED, "Create tarball smodels-v%s.tar.gz" % version, RESET
     o = commands.getoutput("tar czvf smodels-v%s.tar.gz %s" % (version, dirname))
     print o
 
+
 def rmExtraFiles():
-    """ remove a few more files """
+    """
+    Remove additional files.
+    """
+    pass
+
 
 def convertRecipes():
-    """ convert the recipes from .ipynb to .py and .html """
+    """
+    Compile recipes from .ipynb to .py and .html.
+    """
     print RED, "Converting the recipes", RESET
     cmd = "cd %s/docs/Manual/recipes/; make convert remove_ipynbs" % dirname
     o = commands.getoutput (cmd)
     print o
 
+
 def explode ():
-    """ explode the tarball """
+    """
+    Explode the tarball.
+    """
     print RED, "Explode the tarball ...", RESET
     cmd = "tar xzvf smodels-v%s.tar.gz" % version
     o = commands.getoutput (cmd)
 
+
 def make ():
-    """ run make in dirname/lib """
+    """
+    Execute 'make' in dirname/lib.
+    """
     print RED, "Now run make in dirname/lib ...", RESET
     cmd = "cd %s/lib; make" % dirname
     o = commands.getoutput (cmd)
     print o
 
+
 def runExample ():
-    """ run Example.py """
+    """
+    Execute Example.py.
+    """
     print RED, "Now run Example.py ...", RESET
     cmd = "cd %s/; ./Example.py" % dirname
     o = commands.getoutput (cmd)
     print o
 
+
 def test ():
-    """ test the tarball, explode it, run make, and runSModelS.py """
+    """
+    Test the tarball, explode it, execute 'make', and 'runSModelS.py'.
+    """
     print RED, "--------------------------", RESET
     print RED, "    Test the setup ...    ", RESET
     print RED, "--------------------------", RESET
@@ -115,7 +158,9 @@ def test ():
 
 
 def create():
-    """ create a tarball for distribution """
+    """
+    Create a tarball for distribution.
+    """
     print RED, "Creating tarball for distribution, version", version, RESET
     makeClean()
     rmdir()
