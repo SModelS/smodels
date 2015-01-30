12687f4a648e167f336dc267be4fc3fb73ac4d68
walten <wolfgang.waltenberger@gmail.com>
2015-01-30 16:52:41 -0200

    smaller ammendments

--- test/testSVD.py
+++ test/testSVD.py
@@ -9,6 +9,7 @@
 
 """
 import unittest
+import math
 from smodels.experiment.sVDTrafo import SVDTrafo
 from smodels.tools.physicsUnits import GeV, fb
 
@@ -26,11 +27,15 @@ class SVDTest(unittest.TestCase):
                  [ [[ 400.*GeV,350.*GeV], [ 400.*GeV,350.*GeV] ], 19.*fb ], 
                  ]
         trafo=SVDTrafo ( data )
-        print "upper limit for 300,100=", trafo.getUpperLimit ( [[ 300.*GeV,100.*GeV], [ 300.*GeV,100.*GeV] ] )
-        print "upper limit for 400,300=", trafo.getUpperLimit ( [[ 400.*GeV,300.*GeV], [ 400.*GeV,300.*GeV] ] )
-        print "upper limit for 300,125=", trafo.getUpperLimit ( [[ 300.*GeV,125.*GeV], [ 300.*GeV,125.*GeV] ] )
-        print "upper limit for 300,120=", trafo.getUpperLimit ( [[ 300.*GeV,120.*GeV], [ 300.*GeV,120.*GeV] ] )
-        print "upper limit for 300,120,300,125=", trafo.getUpperLimit ( [[ 300.*GeV,120.*GeV], [ 300.*GeV,125.*GeV] ] )
+        self.assertAlmostEquals ( trafo.getUpperLimit ( [[ 300.*GeV,100.*GeV], [ 300.*GeV,100.*GeV] ] ), 10.*fb )
+        self.assertAlmostEquals ( trafo.getUpperLimit ( [[ 400.*GeV,300.*GeV], [ 400.*GeV,300.*GeV] ] ), 17.*fb )
+        self.assertAlmostEquals ( trafo.getUpperLimit ( [[ 300.*GeV,125.*GeV], [ 300.*GeV,125.*GeV] ] ), 11.5*fb )
+        self.assertAlmostEquals ( trafo.getUpperLimit ( [[ 300.*GeV,120.*GeV], [ 300.*GeV,120.*GeV] ] ), 11.2*fb )
+        self.assertTrue ( math.isnan ( trafo.getUpperLimit ( [[ 300.*GeV,120.*GeV], [ 300.*GeV,125.*GeV] ] ).asNumber(fb) ) )
+        self.assertTrue ( math.isnan ( trafo.getUpperLimit ( [[ 500.*GeV,200.*GeV], [ 500.*GeV,200.*GeV] ] ).asNumber(fb) ) )
+    #    print "upper limit for 300,120=", trafo.getUpperLimit ( [[ 300.*GeV,120.*GeV], [ 300.*GeV,120.*GeV] ] )
+        #print "upper limit for 300,120,300,125=", trafo.getUpperLimit ( [[ 300.*GeV,120.*GeV], [ 300.*GeV,125.*GeV] ] )
+        #print "upper limit for 500,200,500,200=", trafo.getUpperLimit ( [[ 500.*GeV,200.*GeV], [ 500.*GeV,200.*GeV] ] )
 
 if __name__ == "__main__":
     unittest.main()
