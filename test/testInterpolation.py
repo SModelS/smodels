372da6b3939d928650491a8fd2b6c1d7aaf263a5
lessa.at.XPS12 <lessa.a.p@gmail.com>
2015-02-06 17:00:18 +0100

    added path insert to testInterpolation

--- test/testInterpolation.py
+++ test/testInterpolation.py
@@ -8,6 +8,8 @@
 .. moduleauthor:: Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
 
 """
+import sys
+sys.path.insert(0,'../')
 import unittest
 from smodels.experiment.databaseObjects import DataBase
 from smodels.experiment.txnameObject import TxNameData
