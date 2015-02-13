766187ad313fd8e40d693ae320ec6e59d8ed90c7
walten <wolfgang.waltenberger@gmail.com>
2015-02-13 17:00:21 -0200

    upper limit test added, logger msg extended

--- test/testStatistics.py
+++ test/testStatistics.py
@@ -13,7 +13,15 @@ from smodels.tools import statistics
 class StatisticsTest(unittest.TestCase):
     def testCLInterval(self):
         re = statistics.computeCLInterval ( 100., 100., 1. )
-        self.assertEqual ( re, 18.0792727821 )
+        self.assertAlmostEqual ( re, 18.0792727821 )
+
+    def testBayesianLimit(self):
+        re = statistics.bayesianUpperLimit ( 100, 0., 100., 0. )
+        self.assertAlmostEqual ( re, 21.4256127382 )
+
+    def testUL(self):
+        re = statistics.getUL ( 100, 100., 10. )
+        print re
 
 if __name__ == "__main__":
     unittest.main()
