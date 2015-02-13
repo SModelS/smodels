24c91126cc96e47a90c094476cb58bd6aa21145d
Andre Lessa <lessa.a.p@gmail.com>
2015-02-13 17:54:28 -0200

    fixes in statistics

--- smodels/tools/statistics.py
+++ smodels/tools/statistics.py
@@ -34,30 +34,31 @@ def bayesianUpperLimit ( nev, sac, xbg, sbg, cl=.95, prec=-1., smax=0. ):
     
     return BayesianUpperLimit.upperLimit ( nev, sac, xbg, sbg, cl, prec, smax )
 
-def getPValue(Nobs,Nbg,NbgErr,Nsig):
-    """
-    Computes the p-value using the signal cross-section (Nsig) and the systematic
-    error in the BG (bgsysError = systematic error/expected BG).
-    Assumes a Gaussian distribution for the BG systematical error.
-    
-    :param Nsig: number of signal events (float)
-    :param Nobs: number of observed events (integer)
-    :param Nbg: number of expected BG events (float)
-    :param NbgErr:  systematical error on the background (float)
-    :returns: the p-value (float)
-    """
-    #Signal + BG prediction:
-    Ntot = Nbg+Nsig
-    #Normalization        
-    n = (1./2.)*(1. + special.erf(Ntot/(sqrt(2.)*NbgErr)))
-    #P-value integrand
-    def pint(x):            
-        pInt = stats.poisson.cdf(Nobs,x)   #poisson.cdf with mean x (=total number of predicted events distributed according to gaussian)
-        pInt *= stats.norm.pdf(x,loc=Ntot,scale=NbgErr)  #systematical error weight
-        return pInt
-    #P-value integral
-    p = n*integrate.quad(pint,0,inf)[0]        
-    return p
+def getPValue(Nsig,Nobs,Nbg,NbgErr):
+        """
+        Computes the p-value using the signal cross-section (signalxsec) and the systematic
+        error in the BG (bgsysError = systematic error/expected BG).
+        Assumes a Gaussian distribution for the BG systematical error.
+        """
+       
+        #Signal + BG prediction:
+        Ntot = Nbg+Nsig
+        #Total systematical error in BG+signal:
+        NErr = NbgErr
+                
+        #Normalization        
+        n = (1./2.)*(1. + special.erf(Ntot/(sqrt(2.)*NErr)))
+                
+        #P-value integrand
+        def pint(x):            
+            pInt = stats.poisson.cdf(Nobs,x)   #poisson.cdf with mean x (=total number of predicted events distributed according to gaussian)
+            pInt *= stats.norm.pdf(x,loc=Ntot,scale=NErr)  #systematical error weight
+            return pInt
+        
+        #P-value integral
+        p = n*integrate.quad(pint,Ntot-10.*NbgErr,Ntot+10.*NbgErr)[0]
+                
+        return p
 
 def getUL(Nobs,Nbg,NbgErr):
     """
@@ -71,9 +72,12 @@ def getUL(Nobs,Nbg,NbgErr):
     :returns: the 95% confidence level on signal*efficiency*luminosity (float)
     """
     
+    n0 = max((Nobs - Nbg) - 4*sqrt(NbgErr**2 + Nbg),0.)
+    n1 = (Nobs - Nbg) + 4*sqrt(NbgErr**2 + Nbg)
+
     def pValm(x):
-        return 0.0227 - getPValue(x,Nobs,Nbg,NbgErr) 
-    
-    nmax = optimize.brentq(pValm,0.,float(Nobs))
+        return 0.0227 - getPValue(x,Nobs,Nbg,NbgErr)
+
+    nmax = optimize.brentq(pValm,n0,n1)
     return nmax
     
