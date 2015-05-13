bbf2bdca1c78f157ff28e717e14aaf8d63048fef
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2015-05-13 19:16:41 -0300

    loggers at folder level removed
    Former-commit-id: e601aef3c32e92e6e9506f69808c28da01827822

--- smodels/tools/__init__.py
+++ smodels/tools/__init__.py
@@ -4,10 +4,3 @@
        main SModelS code.
 """
 
-#import os
-#import logging.config
-
-#basepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
-#logging.config.fileConfig('%s/etc/logging.conf' % basepath,
-#                          disable_existing_loggers=False)
-#logger = logging.getLogger(__name__)
