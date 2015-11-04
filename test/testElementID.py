96c02feffa03c3dbc7bd355313a3265ff2f150e7
Ursula Laa <ursula.laa@lpsc.in2p3.fr>
2015-11-04 18:45:05 +0100

    added unit test for element ID
    Former-commit-id: f1594abe5e23493003ecfe36f65d4095cea8078c

---
+++ test/testElementID.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+.. module:: testElementID
+   :synopsis: Tests the slha checker
+
+.. moduleauthor:: Ursula Laa <Ursula.Laa@assoc.oeaw.ac.at>
+
+"""
+import unittest, sys
+sys.path.insert(1, "../")
+from smodels.installation import installDirectory
+from smodels.theory import slhaDecomposer
+from smodels.tools.physicsUnits import GeV
+from smodels.experiment.databaseObjects import Database
+from smodels.theory.theoryPrediction import theoryPredictionsFor
+import smodels.tools.printer as prt
+
+class ElementIdTest(unittest.TestCase):
+    def testGoodFile(self):
+
+        listOfIDs = [[945],[943, 944, 946, 947, 948, 949, 950, 951]]
+        stdoutPrinter = prt.TxTPrinter(output = 'stdout')
+        filename = "%sinputFiles/slha/compressedSpec.slha" % (installDirectory() )
+        topoList = slhaDecomposer.decompose(filename,doCompress = True, doInvisible=True, minmassgap = 5*GeV)
+        stdoutPrinter.addObj(topoList,2)
+        database = Database("database/")
+        resultlist = database.getExpResults()
+        for res in resultlist:
+            theorypredictions = theoryPredictionsFor(res, topoList)
+            if not theorypredictions: continue
+            for tp in theorypredictions:
+                self.assertEquals(tp.IDs,listOfIDs.pop())
+            
+
+if __name__ == "__main__":
+    unittest.main()
