c3b312b45be227f9a0d06814da43185638d582e5
lessa.at.homeDesktop <lessa.a.p@gmail.com>
2016-03-09 01:53:46 -0300

    Added timeout in runSModelS
    Former-commit-id: 59ce16578ff8b1f19fd94dc1ec3e6c1593476f37

---
+++ smodels/tools/timeOut.py
@@ -0,0 +1,38 @@
+"""
+.. module:: tools.timeOut
+   :synopsis: Facility to implement a time out option when running smodels
+
+.. moduleauthor:: Andre Lessa <lessa.a.p@gmail.com>
+
+"""
+
+import signal
+
+class NoTime(Exception):
+    """
+    The time out exception. Raised when the running time exceeds timeout
+    """
+    def __init__(self, value=None):
+        self.value = value
+        Exception.__init__(self, value)
+        
+    def __str__(self):
+        return '%.2es time out exceeded' %float(self.value)
+
+
+class Timeout():
+    """Timeout class using ALARM signal."""
+    
+    def __init__(self, sec):
+        self.sec = sec
+ 
+    def __enter__(self):
+        if self.sec:            
+            signal.signal(signal.SIGALRM, self.raise_timeout)
+            signal.alarm(self.sec)            
+ 
+    def __exit__(self, *args):
+        signal.alarm(0)    # disable alarm
+ 
+    def raise_timeout(self, *args):
+        raise NoTime(self.sec)
