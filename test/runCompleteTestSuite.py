86e7dfc5b5c4c75fb7a4e848532aab296d369dd9
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2016-04-27 12:16:29 +0200

    fixed missing tempdir removal bug
    Former-commit-id: 2ec2821ae8ee83a2a818295a3633609de32014a8

--- test/runCompleteTestSuite.py
+++ test/runCompleteTestSuite.py
@@ -13,8 +13,23 @@ import sys
 sys.path.insert(0,"../")
 import unittest
 
-def main():
+def run():
     unittest.TextTestRunner().run( unittest.TestLoader().discover("./") )
 
+def verbose_run():
+    alltests = unittest.TestLoader().discover("./") 
+    for series in alltests:
+        for test in series:
+            for t in test:
+                print "Running ",t.id()
+                t.run()
+
 if __name__ == "__main__":
-    main()
+    import argparse
+    ap = argparse.ArgumentParser('runs the complete test suite')
+    ap.add_argument('-v','--verbose', help='run verbosely',action='store_true')
+    args = ap.parse_args()
+    if args.verbose:
+        verbose_run()
+    else:
+        run()
