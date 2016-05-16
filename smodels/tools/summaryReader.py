9e32a162a78b1bb2116392f9b89157a50c81bb36
lessa.at.XPS12 <lessa.a.p@gmail.com>
2016-05-16 06:49:51 -0300

    Added missing topos check in unit test
    Former-commit-id: 9e6d822f6d0a4a35ed69f6c707228cd5d36468d5

--- smodels/tools/summaryReader.py
+++ smodels/tools/summaryReader.py
@@ -22,6 +22,18 @@ class Output():
             return self.__dict__ == other.__dict__
         else:
             return False
+        
+class MissOutput():        
+    def __init__(self, l):
+        self.sqrts = eval(l[0])
+        self.weight = eval(l[1])
+        self.element = l[3]
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
 
 
 class Summary():
@@ -34,6 +46,7 @@ class Summary():
         self.signalRegions = []
         self.txnames = []
         self.filename = filename
+        self.missedTopos = []
         self.read(filename)
 
     def read(self, infile):
@@ -41,6 +54,7 @@ class Summary():
         lines = f.readlines()
         f.close()
         resultLines = None
+        missingLines = None
         signalRegion = ""
         txnames = []
         for il,l in enumerate(lines):
@@ -49,27 +63,40 @@ class Summary():
             if "#Analysis" in l:
                 resultLines = True
                 continue
+            if "Missing topologies" in l:
+                missingLines = True
+                continue            
             if l.startswith("#"):
                 continue
             if "----" in l:
                 continue
             if "====" in l:
                 resultLines = None
+                missingLines = None
             if not l.strip():
                 continue
             if 'Signal Region' in l or 'Txnames' in l:
                 continue
+            if 'Missing Topologies' in l or 'Sqrts (TeV)' in l:
+                continue            
             if resultLines:
                 if 'Signal Region' in lines[il+1]:                    
                     signalRegion = lines[il+1].split(':')[1].strip()
                 if 'Txnames' in lines[il+2]:
                     txnames = sorted(lines[il+2].split(':')[1].strip().split(','))
                 self.results.append(Output(l.split(),signalRegion,txnames))
+            if missingLines:
+                self.missedTopos.append(MissOutput(l.split()))
 
     def __eq__(self, other):
         if not len(self.results) == len(other.results):
             return False
+        if not len(self.missedTopos) == len(other.missedTopos):
+            return False        
         for res in self.results:
             if not res in other.results:
                 return False
+        for miss in self.missedTopos:
+            if not miss in other.missedTopos:
+                return False            
         return True
