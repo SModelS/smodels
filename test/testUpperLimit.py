13c9f7d99e8be4582dcaa7bc5ba1fc3a29c41682
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2016-05-17 19:49:43 +0200

    revert rounding
    Former-commit-id: 8021416ef658d95ec14b627f073963ad17a619f7

--- test/testUpperLimit.py
+++ test/testUpperLimit.py
@@ -13,7 +13,7 @@ import unittest
 from smodels.tools.physicsUnits import GeV, pb
 from smodels.installation import installDirectory
 from smodels.experiment.databaseObj import Database
-database=Database ( "./database", verbosity='error' )
+database=Database ( "./database", verbosity='warning' )
 
 class UpperLimitTest(unittest.TestCase):
 
@@ -35,7 +35,7 @@ class UpperLimitTest(unittest.TestCase):
         expRes=database.getExpResults ( analysisIDs = [ "ATLAS-SUSY-2013-05" ], 
                     datasetIDs= [ None ] , txnames= [ "T6bbWW" ] )
         ul = expRes[0].getUpperLimitFor (txname= "T6bbWW",  
-         mass=[[150*GeV,140*GeV,135*GeV],[150*GeV,140*GeV,135*GeV]] ).asNumber ( pb )
+         mass=[[150.*GeV,140.*GeV,135.*GeV],[150.*GeV,140.*GeV,135.*GeV]] ).asNumber ( pb )
         self.assertAlmostEquals ( ul, 324.682 )
 
 if __name__ == "__main__":
