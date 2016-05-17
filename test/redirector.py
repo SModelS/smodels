573a999d1db6294b317bcdd0073ddb5a90f83484
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2016-05-17 12:24:21 +0200

    warning suppressed
    Former-commit-id: 4b77bdb44205e473d07c58465422cc45d57442aa

---
+++ test/redirector.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+"""
+.. module: testLikelihood
+   :synopsis: Test likelihood functions module.
+
+.. moduleauthor:: Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
+
+"""
+
+import contextlib
+import os
+import sys
+
+def fileno(file_or_fd):
+    fd = getattr(file_or_fd, 'fileno', lambda: file_or_fd)()
+    if not isinstance(fd, int):
+        raise ValueError("Expected a file (`.fileno()`) or a file descriptor")
+    return fd
+
+@contextlib.contextmanager
+def stdout_redirected(to=os.devnull, stdout=None):
+    """
+    http://stackoverflow.com/a/22434262/190597 (J.F. Sebastian)
+    """
+    if stdout is None:
+       stdout = sys.stdout
+
+    stdout_fd = fileno(stdout)
+    # copy stdout_fd before it is overwritten
+    #NOTE: `copied` is inheritable on Windows when duplicating a standard stream
+    with os.fdopen(os.dup(stdout_fd), 'wb') as copied: 
+        stdout.flush()  # flush library buffers that dup2 knows nothing about
+        try:
+            os.dup2(fileno(to), stdout_fd)  # $ exec >&to
+        except ValueError:  # filename
+            with open(to, 'wb') as to_file:
+	              os.dup2(to_file.fileno(), stdout_fd)  # $ exec > to
+        try:
+            yield stdout # allow code to be run with the redirected stdout
+        finally:
+            # restore stdout to its previous value
+            #NOTE: dup2 makes stdout_fd inheritable unconditionally
+            stdout.flush()
+            os.dup2(copied.fileno(), stdout_fd)  # $ exec >&copied
+
+@contextlib.contextmanager
+def stderr_redirected(to=os.devnull, stderr=None):
+    """
+    http://stackoverflow.com/a/22434262/190597 (J.F. Sebastian)
+    """
+    if stderr is None:
+       stderr = sys.stderr
+
+    stderr_fd = fileno(stderr)
+    # copy stderr_fd before it is overwritten
+    #NOTE: `copied` is inheritable on Windows when duplicating a standard stream
+    with os.fdopen(os.dup(stderr_fd), 'wb') as copied: 
+        stderr.flush()  # flush library buffers that dup2 knows nothing about
+        try:
+            os.dup2(fileno(to), stderr_fd)  # $ exec >&to
+        except ValueError:  # filename
+            with open(to, 'wb') as to_file:
+	              os.dup2(to_file.fileno(), stderr_fd)  # $ exec > to
+        try:
+            yield stderr # allow code to be run with the redirected stderr
+        finally:
+            # restore stderr to its previous value
+            #NOTE: dup2 makes stderr_fd inheritable unconditionally
+            stderr.flush()
+            os.dup2(copied.fileno(), stderr_fd)  # $ exec >&copied
+
+if __name__ == '__main__':
+    print ( "test" )
+    with stdout_redirected():
+			print ( "redirected. this must not appear." )
+    print ( "test" )
