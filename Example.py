1428a2a5bd74b648ea9af3722be3de7973c164c6
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2016-05-25 15:13:44 +0200

    fixing stuff for distribution
    Former-commit-id: 845b72b796c268fc6d212f8abe2375c780707ba7

--- Example.py
+++ Example.py
@@ -18,7 +18,7 @@ from smodels.theory import lheDecomposer
 from smodels.tools.physicsUnits import fb, GeV
 from smodels.tools.printer import printout
 from smodels.theory.theoryPrediction import theoryPredictionsFor
-from smodels.experiment.databaseObjects import Database
+from smodels.experiment.databaseObjs import Database
 
 #Set the address of the database folder
 database = Database("../smodels-database/")
