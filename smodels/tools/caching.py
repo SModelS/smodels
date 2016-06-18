2dbe220677e505c9f7e732fe64dae4c6cf702ba0
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2016-06-18 21:23:54 +0200

    caching with cleaning
    Former-commit-id: 097287fbf36e1ff9461338c5775886b60074b261

--- smodels/tools/caching.py
+++ smodels/tools/caching.py
@@ -12,39 +12,69 @@ from smodels.tools.physicsUnits import pb, GeV, fb
 
 def _toString ( arg ):
     try:
-        return "%.2f " % arg.asUnit(fb)
+        return "%.2f" % arg.asUnit(fb)
     except Exception,e:
         pass
     try:
-        return "%.3f " % arg.asNumber(GeV)
+        return "%.3f" % arg.asNumber(GeV)
     except Exception,e:
         pass
     try:
-        return "%.2f " % arg.asUnit(1/fb)
+        return "%.2f" % arg.asUnit(1/fb)
     except Exception,e:
         pass
     if type(arg) == float:
-        return "%.2f " % arg
+        return "%.2f" % arg
     if type(arg) == int:
-        return "%d " % arg
+        return "%d" % arg
     if type(arg) == str:
-        return "%s " % arg
+        return "%s" % arg
     if type(arg) in [ list, tuple ]:
         argstring=""
         for newarg in arg:
-            argstring += _toString ( newarg )
+            argstring += _toString ( newarg ) + " "
+        argstring=argstring[:-1]
         return argstring
-    return "%s " % ( str(arg) )
+    return "%s" % ( str(arg) )
     
-    
-_cache = {}
+class Cache:
+    """ a class for storing results from interpolation """
+    _cache = {}
+    _cache_order=[]
+    n_stored = 10000 ## number of interpolations we keep
 
-def clearCache():
-    """
-    Clear the cache
-    """
-    _cache={}
-            
+    @staticmethod
+    def size():
+        return len(Cache._cache_order)
+
+    @staticmethod
+    def _clear_garbage ():
+        """
+        every once in a while we clear the garbage, i.e. 
+        discard half of the cached interpolations
+        """
+        if len(Cache._cache_order)<Cache.n_stored:
+            return
+        ns2 = Cache.n_stored / 2
+        for i in range ( ns2 ):
+            Cache._cache.pop ( Cache._cache_order[i] ) ## remove 
+        Cache._cache_order=Cache._cache_order[ ns2 : ]
+
+    @staticmethod
+    def reset ():
+        """ completely reset the cache """
+        Cache._cache = {}
+        Cache._cache_order = []
+
+    @staticmethod
+    def add ( key, value ):
+        if key in Cache._cache_order:
+            return value
+        Cache._cache_order.append ( key )
+        Cache._cache[key] = value
+        Cache._clear_garbage()
+        return value
+
 def _memoize(func):
     """
     Serves as a wrapper to cache the results of func, since this is a
@@ -57,7 +87,5 @@ def _memoize(func):
         Wrapper for the function to be memoized
         """ 
         argstring = _toString ( args )
-        if argstring not in _cache:
-            _cache[argstring] = func(*args)
-        return _cache[argstring]
+        return Cache.add ( argstring, func(*args) )
     return _wrap
