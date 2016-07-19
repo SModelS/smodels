d66151fe8bb45677daa1b803ad5d4aef7d3a1a4d
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2016-07-19 19:03:36 +0200

    parallelization test added
    Former-commit-id: 2ddd15d7bcc691519d1a0c3972b16e80005b6686

--- test/testRunSModelS.py
+++ test/testRunSModelS.py
@@ -12,6 +12,7 @@ import sys
 sys.path.insert(0,"../")
 import unittest
 import os
+import glob
 from os.path import join, basename
 from smodels.installation import installDirectory as iDir
 from smodels.tools import summaryReader
@@ -32,6 +33,32 @@ class RunSModelSTest(unittest.TestCase):
         if suppressStdout:
             sys.stdout = a
         return outputfile
+
+    def testMultipleFiles ( self ):
+        filename = join ( iDir(), "inputFiles/slha/" )
+        suppressStdout = True
+        if suppressStdout:
+            a=sys.stdout
+            sys.stdout = open ( "stdout.log", "w" )
+        out = join ( iDir(), "test/unitTestOutput" )
+        for i in os.listdir ( out ):
+            if i[-8:]==".smodels":
+                os.unlink ( os.path.join ( out, i ) )
+        mypid = os.getpid()
+        try:
+            main(filename, parameterFile=join ( iDir(), "test/testParameters.ini" ),
+                 outputDir= out, verbosity = 'error' )
+        except SystemExit,e:
+            # the daughters!
+            return True
+            # pass
+        if mypid != os.getpid():
+            return True
+        if suppressStdout:
+            sys.stdout = a
+        nout = len( list ( glob.iglob ( "unitTestOutput/*smodels" )) )
+        nin = len( list ( glob.iglob ( "%s/*slha" % filename )) )
+        self.assertTrue ( nout == nin )
 
     def testGoodFile(self):
         filename = join ( iDir(), "inputFiles/slha/gluino_squarks.slha" )
