9044c2b541345fbd50ead94c50f9507540848965
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2016-08-11 09:15:07 +0200

    __str__ added
    Former-commit-id: f242a3603ec2c6e3446890e3e1520af6c7b05fd5

--- smodels/experiment/datasetObj.py
+++ smodels/experiment/datasetObj.py
@@ -53,6 +53,9 @@ class DataSet(object):
     def __ne__ ( self, other ):
         return not self.__eq__ ( other )
 
+    def __str__ ( self ):
+        ret = "Dataset: %s" % ( ", ".join ( self.txnameList ) )
+
     def __eq__ ( self, other ):
         if self.dataInfo != other.dataInfo:
             return False
