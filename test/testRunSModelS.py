49c85c5d641fcc19bf6c861242aafa1d424693f6
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2016-08-16 10:59:09 +0200

    allow for different ini file
    Former-commit-id: c5b30474b68e219921e3e98d94a7c7f08f42e0cb

--- test/testRunSModelS.py
+++ test/testRunSModelS.py
@@ -26,31 +26,31 @@ def equalObjs(obj1,obj2,allowedDiff,ignore=[]):
     """
     Compare two objects.
     The numerical values are compared up to the precision defined by allowedDiff.
-    
-    :param obj1: First python object to be compared 
+
+    :param obj1: First python object to be compared
     :param obj2: Second python object to be compared
-    :param allowedDiff: Allowed % difference between two numerical values 
+    :param allowedDiff: Allowed % difference between two numerical values
     :param ignore: List of keys to be ignored
-    :return: True/False    
-    """      
-    
+    :return: True/False
+    """
+
     if type(obj1) != type(obj2):
         logger.info("Data types differ (%s,%s)" %(type(obj1),type(obj2)))
         return False
-    
+
     if isinstance(obj1,unum.Unum):
         if obj1 == obj2:
             return True
         diff = 2.*abs(obj1-obj2)/abs(obj1+obj2)
         return diff.asNumber() < allowedDiff
-    elif isinstance(obj1,float):        
+    elif isinstance(obj1,float):
         if obj1 == obj2:
             return True
         diff = 2.*abs(obj1-obj2)/abs(obj1+obj2)
         return diff < allowedDiff
     elif isinstance(obj1,str):
         return obj1 == obj2
-    elif isinstance(obj1,dict):    
+    elif isinstance(obj1,dict):
         for key in obj1:
             if key in ignore: continue
             if not key in obj2:
@@ -66,23 +66,26 @@ def equalObjs(obj1,obj2,allowedDiff,ignore=[]):
                 return False
     else:
         return obj1 == obj2
-            
+
     return True
 
 
 class RunSModelSTest(unittest.TestCase):
-    def runMain(self, filename, timeout = 0, suppressStdout=True, development=False):
+    def runMain(self, filename, timeout = 0, suppressStdout=True, development=False,
+                 inifile = "testParameters.ini" ):
         to = None
+        verbosity='debug'
         if suppressStdout:
+            verbosity='error'
             to = os.devnull
         with redirector.stdout_redirected ( to = to ):
             out = join( iDir(), "test/unitTestOutput" )
-            main(filename, parameterFile=join ( iDir(), "test/testParameters.ini" ),
-                 outputDir= out, verbosity = 'error', db= None, timeout = timeout,
+            main(filename, parameterFile=join ( iDir(), "test/%s" % inifile ),
+                 outputDir= out, verbosity = verbosity, db= None, timeout = timeout,
                  development = development)
             sfile = join(iDir(),"test/unitTestOutput/%s.py" % basename(filename))
             return sfile
-    
+
     def testMultipleFiles( self ):
         out = join( iDir(), "test/unitTestOutput")
         for i in os.listdir( out ):
@@ -93,34 +96,34 @@ class RunSModelSTest(unittest.TestCase):
         nout = len( list ( glob.iglob ( "unitTestOutput/*smodels" )) )
         nin = len( list ( glob.iglob ( "%s/*slha" % dirname )) )
         self.assertTrue( nout == nin )
-           
+
     def timeoutRun(self):
-        filename = join ( iDir(), "inputFiles/slha/complicated.slha" )
-        outputfile = self.runMain(filename, timeout=1, suppressStdout=True,
-                     development=True)
-    
+        filename = join ( iDir(), "inputFiles/slha/gluino_squarks.slha" )
+        outputfile = self.runMain(filename, timeout=1, suppressStdout=False,
+                     development=True, inifile = "low_cut.ini" )
+
     def testTimeout(self):
-        self.assertRaises(NoTime, self.timeoutRun)               
-    
-    def testGoodFile(self):        
+        self.assertRaises(NoTime, self.timeoutRun)
+
+    def testGoodFile(self):
         filename = join ( iDir(), "inputFiles/slha/gluino_squarks.slha" )
         outputfile = self.runMain(filename)
         shutil.copyfile(outputfile,'./output.py')
         from gluino_squarks_default import smodelsOutputDefault
         from output import smodelsOutput
         ignoreFields = ['input file','smodels version', 'ncpus']
-        smodelsOutputDefault['ExptRes'] = sorted(smodelsOutputDefault['ExptRes'], 
+        smodelsOutputDefault['ExptRes'] = sorted(smodelsOutputDefault['ExptRes'],
                                                  key=lambda res: [res['theory prediction (fb)'],res['TxNames'],
-                                                   res['AnalysisID'],res['DataSetID']])        
+                                                   res['AnalysisID'],res['DataSetID']])
         equals = equalObjs(smodelsOutput,smodelsOutputDefault,allowedDiff=0.01,ignore=ignoreFields)
-        try:        
+        try:
             os.remove('./output.py')
             os.remove('./output.pyc')
         except: pass
         self.assertTrue(equals)
-   
-   
-    def testBadFile(self):   
+
+
+    def testBadFile(self):
         filename = join (iDir(), "inputFiles/slha/I_dont_exist.slha" )
         outputfile = self.runMain(filename)
         shutil.copyfile(outputfile,'./bad_output.py')
@@ -132,7 +135,7 @@ class RunSModelSTest(unittest.TestCase):
         os.remove('./bad_output.py')
         os.remove('./bad_output.pyc')
         self.assertTrue(equals )
-          
+
     def testCrash(self):
         for f in os.listdir("."):
             if ".crash" in f: os.remove(f)
@@ -144,7 +147,7 @@ class RunSModelSTest(unittest.TestCase):
             if ".crash" in f: break
         inp, par = crashReport.readCrashReportFile(f)
         self.assertEquals(open(filename).readlines(), open(inp).readlines())
-        self.assertEquals(open("testParameters.ini").readlines(), open(par).readlines())        
+        self.assertEquals(open("testParameters.ini").readlines(), open(par).readlines())
 
 if __name__ == "__main__":
     unittest.main()
