21858b0a3ec35d70c3e3227b9eafb707c6e91f53
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2016-08-16 11:59:20 +0200

    force loading of pcl file
    Former-commit-id: 9253f1750e2d88eab86612b707759fa03e9098c4

--- test/databaseLoader.py
+++ test/databaseLoader.py
@@ -12,5 +12,5 @@
 import sys
 sys.path.insert(0,"../")
 from smodels.experiment.databaseObj import Database
-filename="./database/"
-database = Database ( filename, verbosity="warning" )
+filename="./database/database.pcl"
+database = Database ( filename, force_load="pcl", verbosity="warning" )
