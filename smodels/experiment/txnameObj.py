69f33fa59160bdde920f092cd5a99c2460c5fd36
lessa.at.XPS12 <lessa.a.p@gmail.com>
2016-10-07 15:41:43 -0300

    1D data now extend to band around data
    Former-commit-id: 62e4ed0fde8e9a28f63da2a624ab3f39d681ce79

--- smodels/experiment/txnameObj.py
+++ smodels/experiment/txnameObj.py
@@ -472,8 +472,8 @@ class TxNameData(object):
                 MpCut.append([i[0].tolist(),0.])
         if self.dimensionality == 1:
             logger.debug("1-D data found. Extending to a small 2-D band around the line.")
-            MpCut += [[pt[0],pt[1]+0.0001] for pt in MpCut]
-            self.xsec += self.xsec
+            MpCut += [[pt[0],pt[1]+0.0001] for pt in MpCut] + [[pt[0],pt[1]-0.0001] for pt in MpCut]
+            self.xsec += self.xsec + self.xsec
             self.dimensionality = 2
              
         self.Mp=MpCut ## also keep the rotated points, with truncated zeros
