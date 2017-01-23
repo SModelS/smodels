0d5cff2a733a6abbd8219326264a807f36b6bba9
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-01-23 12:38:20 +0100

    cpp v1.1
    Former-commit-id: 8d2fb72bbdf93da754966efa1f7886bfa0a3bd50

---
+++ cpp/SModelS.cc
@@ -0,0 +1,52 @@
+#include <SModelS.h>
+#include <python2.7/Python.h>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+// https://docs.python.org/2/extending/embedding.html
+// http://stackoverflow.com/questions/3286448/calling-a-python-method-from-c-c-and-extracting-its-return-value
+
+SModelS::SModelS( const std::string & parameterfile )
+{
+  cout << "[smodels.cpp] initialising!" << endl;
+  /// initialise python, import modules
+  Py_SetProgramName( (char *) (const char *) "SModelS" ); // huh??
+  Py_Initialize();
+  PyRun_SimpleString("import sys");
+  PyRun_SimpleString("sys.path.insert(0,'../smodels/tools')");
+  PyRun_SimpleString("import time");
+  PyRun_SimpleString("t0=time.time()");
+  PyRun_SimpleString("import modelTester");
+	// PyObject* myModuleString = PyString_FromString((char*)"modelTester");
+	// PyObject* myModule = PyImport_Import( myModuleString );
+  loadDatabase ( parameterfile );
+}
+
+SModelS::~SModelS ()
+{
+  Py_Finalize();
+  cout << "[smodels.cpp] ends!" << endl;
+}
+
+void SModelS::loadDatabase ( const string & parameterfile )
+{
+	ostringstream buffer;
+	buffer << "parameterFile='" << parameterfile << "'";
+  PyRun_SimpleString( buffer.str().c_str() );
+  PyRun_SimpleString( "parser = modelTester.getParameters( parameterFile )" );
+  PyRun_SimpleString( "database, databaseVersion = modelTester.loadDatabase(parser, None, 'info' )" );
+  PyRun_SimpleString( "listOfExpRes = modelTester.loadDatabaseResults(parser, database)" );
+  PyRun_SimpleString( "print '[smodels.cpp] %d experimental results found.' % len(listOfExpRes) " );
+}
+
+void SModelS::run ( const string & inFile )
+{
+  cout << "[smodels.cpp] now running over " << inFile << endl;
+	ostringstream buffer;
+	buffer << "inFile='" << inFile << "'";
+  PyRun_SimpleString( buffer.str().c_str() );
+  PyRun_SimpleString( "fileList = modelTester.getAllInputFiles( inFile )" );
+  PyRun_SimpleString( "modelTester.testPoints( fileList, inFile, 'results', parser, databaseVersion, listOfExpRes, 900, False, parameterFile )" );
+}
---
+++ cpp/SModelS.h
@@ -0,0 +1,21 @@
+#ifndef SModelS_H
+#define SModelS_H
+
+#include <string>
+
+class SModelS {
+  /** 
+   * \class SModelS
+   * a simple class that wraps python-based SModelS for use with a C++ environment
+   */
+
+  public:
+    SModelS( const std::string & parameterfile );
+    ~SModelS();
+    void run ( const std::string & file );
+
+  private:
+    void loadDatabase( const std::string & parameterfile );
+};
+
+#endif /* SModelS_H */
