524c150578956ad4fc51188f678cb3b7758156e8
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-01-24 19:12:13 +0100

    colors added
    Former-commit-id: a7912b5eae833169dd9e447612279b613d1c08fc

--- smodels/tools/smodelsLogging.py
+++ smodels/tools/smodelsLogging.py
@@ -7,10 +7,29 @@
 """
 
 import logging
+from smodels.tools.colors import colors
 
 FORMAT = '%(levelname)s in %(module)s.%(funcName)s() in %(lineno)s: %(message)s'
 logging.basicConfig(format=FORMAT)
-logger = logging.getLogger("smodels")
+_logger = logging.getLogger("smodels")
+
+class ColoredLogger:
+    def __init__ ( self ):
+        pass
+    def error ( self, msg, *args, **kwargs):
+        msg = "%s%s%s" % ( colors.error, msg, colors.reset )
+        return _logger.error ( msg, *args, **kwargs )
+    def warn ( self, msg, *args, **kwargs):
+        msg = "%s%s%s" % ( colors.warn, msg, colors.reset )
+        return _logger.error ( msg, *args, **kwargs )
+    def info ( self, msg, *args, **kwargs):
+        msg = "%s%s%s" % ( colors.info, msg, colors.reset )
+        return _logger.error ( msg, *args, **kwargs )
+    def debug ( self, msg, *args, **kwargs):
+        msg = "%s%s%s" % ( colors.debug, msg, colors.reset )
+        return _logger.error ( msg, *args, **kwargs )
+
+logger = ColoredLogger()
 
 def setLogLevel ( level ):
     """ set the log level of the central logger. 
