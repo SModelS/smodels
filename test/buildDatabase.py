83c531617c87612acd87e761ec1244654137b2e6
Wolfgang Waltenberger <walten@cern.ch>
2017-01-25 22:23:54 +0100

    36% speedup
    Former-commit-id: 17947ea4da20b9f86cd3495745f12ff208db15e7

--- test/buildDatabase.py
+++ test/buildDatabase.py
@@ -1,8 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import sys
+import sys, os, time
 sys.path.insert(0,"../")
 from smodels.experiment.databaseObj import Database
 
-d=Database("./database/")
+# dir = "./database/"
+dir = "../../smodels-database/"
+
+pcl = "%sdatabase.pcl" % dir
+
+if os.path.exists ( pcl ):
+    os.unlink ( pcl )
+
+t0=time.time()
+d=Database( dir )
 print(d)
+t1=time.time()
+print ( "Building the database took %.2f seconds." % ( t1 - t0 ) )
+s = os.stat ( pcl )
+print ( "Database is %.1f MB." % ( s.st_size / 1000. / 1000. ) )
+d=Database( dir )
+t2=time.time()
+print ( "Reading the database took %.2f seconds." % ( t2 - t1 ) )
