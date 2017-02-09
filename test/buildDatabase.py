f5a522126f201102c30b305331559f458b0b2060
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-02-09 16:16:26 +0100

    slha decomposer, shortened info msg, databaseObj allow for pcl file defined by user
    Former-commit-id: 8c0547fe40c60160a94b60bec5e3674f8c46a32c

--- test/buildDatabase.py
+++ test/buildDatabase.py
@@ -3,6 +3,9 @@
 import sys
 sys.path.insert(0,"../")
 from smodels.experiment.databaseObj import Database
+from smodels.tools.smodelsLogging import setLogLevel
+setLogLevel ( "debug" )
 
-d=Database("./database/")
+# d=Database("./database/database%d.pcl" % int ( sys.version[0] ) )
+d=Database("./database/" )
 print(d)
