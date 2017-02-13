ea1beee30cd4a26a566c15e73825c3df09ce2532
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-02-13 17:16:04 +0100

    cleaned refactored nllfast
    Former-commit-id: 6eaf7823f0281791f72560ef7d470208e2ba2464

--- smodels/tools/externalNllFast.py
+++ smodels/tools/externalNllFast.py
@@ -2,8 +2,12 @@
 
 """
 .. module:: externalNllFast
-   :synopsis: Wrapper for all nllfast versions.
-
+   :synopsis: This module provides methods to access the nllfast grid and             
+              compute k-factors (when available) to SUSY pair                         
+              production cross sections.                                              
+                                                                                      
+.. moduleauthor:: Suchita Kulkarni <suchita.kulkarni@gmail.com>                       
+.. moduleauthor:: Andre Lessa <lessa.a.p@gmail.com>
 .. moduleauthor:: Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
 
 """
@@ -25,7 +29,6 @@ third = [1000005,
          1000006,
          2000006]
 gluinos = [1000021]
-
 
 try:
     import commands as executor
@@ -77,40 +80,7 @@ class ExternalNllFast(ExternalTool):
         logger.info(out)
         return True
 
-
-    def fetch(self):
-        """
-        Fetch and unpack tarball.
-
-        """
-        import urllib, tarfile
-        tempfile = "/tmp/nllfast7.tar.gz"
-        f = open(tempfile, "w")
-        url = "http://smodels.hephy.at/externaltools/nllfast%d.tar.gz" \
-                % self.sqrts
-        logger.info("fetching tarball from " + url)
-        R = urllib.urlopen(url)
-        l = R.readlines()
-        for line in l:
-            f.write(line)
-        R.close()
-        f.close()
-        tar = tarfile.open(tempfile)
-        for item in tar:
-            tar.extract(item, self.srcPath + "/")
-
-
-    def unlink(self, inputFile):
-        """
-        Remove inputFile.out
-
-        """
-        return
-        # fname = "%s/%s.out" % (self.cdPath, inputFile)
-        # if os.path.exists(fname):
-        #     os.unlink(fname)
-
-    def interpolateKfactors( self, kFacsVector, xval):
+    def _interpolateKfactors( self, kFacsVector, xval):
         """
         Interpolate a list of k-factor  values from
         kFacsVector = [[x0,[k1,k2,..]], [x1,[k1,k2,..],...].
@@ -134,7 +104,7 @@ class ExternalNllFast(ExternalTool):
 
         return kFacs
 
-    def getKfactorsFrom( self, output ):
+    def _getKfactorsFrom( self, output ):
         """
         Read NLLfast output and return the k-factors.
 
@@ -167,21 +137,7 @@ class ExternalNllFast(ExternalTool):
 
         return kFacs
 
-    def run_(self, params):
-        """
-        Execute nllfast7.
-
-        :params params: parameters used (e.g. gg cteq5 .... )
-        :returns: stdout and stderr, or error message
-
-        """
-        cmd = "cd %s; %s %s" % (self.cdPath, self.executablePath, params)
-        out = executor.getoutput(cmd)
-        # out = subprocess.check_output ( cmd, shell=True, universal_newlines=True )
-        out = out.split("\n")
-        return out
-
-    def run ( self, this ):
+    def _run ( self, this ):
         """
         Run. Code taken from nllFast.runNLLfast
         Return the process name (in NLLfast notation) for the pair production of
@@ -196,17 +152,17 @@ class ExternalNllFast(ExternalTool):
         os.chdir(current_dir)
         return nll_output
 
-    def compute ( self, energy, pIDs, pdf, squarkmass, gluinomass ):
-        process = self.getProcessName(pIDs)
+    def _compute ( self, energy, pIDs, pdf, squarkmass, gluinomass ):
+        process = self._getProcessName(pIDs)
         if process == "st":
             nll_run = "./nllfast_" + energy + " %s %s %s" % \
                   (process, pdf, squarkmass)
         else:
             nll_run = "./nllfast_" + energy + " %s %s %s %s" % \
                   (process, pdf, squarkmass, gluinomass)
-        return self.run( nll_run )
+        return self._run( nll_run )
 
-    def getProcessName(self, pIDs):
+    def _getProcessName(self, pIDs):
         """
         Return the process name (in NLLfast notation) for the pair production of
         pIDs.
@@ -236,7 +192,7 @@ class ExternalNllFast(ExternalTool):
             process = 'st'
         return process
 
-    def getDecoupledKfactors( self, process, energy, pdf, mass ):
+    def _getDecoupledKfactors( self, process, energy, pdf, mass ):
         """
         Compute k-factors in the decoupled (squark or gluino) regime for the process.
         If a decoupled grid does not exist for the process, return None
@@ -249,37 +205,14 @@ class ExternalNllFast(ExternalTool):
                           (process_dcpl, pdf, mass)
         e = energy.replace ( "TeV", "" ).replace ( "*", "" )
         # tool = toolBox.ToolBox().get ( "nllfast%d" % int ( e ) )
-        nll_output = self.run(nll_run )
+        nll_output = self._run(nll_run )
         if "K_NLO" in nll_output:
-            return self.getKfactorsFrom(nll_output)
+            return self._getKfactorsFrom(nll_output)
         else: return None
 
-    def oldrun(self, process, pdf, squarkmass, gluinomass):
-        """
-        Execute nllfast.
-
-        :params process: which process: st, sb, gg, gdcpl, sdcpl, ss, sg, tot
-        :params pdf: cteq=cteq6, mstw2008
-        :params squarkmass: squarkmass, None if squark decoupled
-        :params gluinomass: gluinomass, None if gluino decoupled
-        :returns: stdout and stderr, or error message
-
-        """
-        processes = ["st", "sb", "gg", "gdcpl", "sdcpl", "ss", "sg", "tot"]
-        if not process in processes:
-            return None
-        if not pdf in ["cteq", "cteq6", "mstw", "mstw2008"]:
-            return None
-        if not squarkmass:
-            return self.run_("%s %s %s") % (process, pdf, gluinomass)
-        if not gluinomass:
-            return self.run_("%s %s %s") % (process, pdf, squarkmass)
-        return self.run_("%s %s %s %s") % \
-                (process, pdf, squarkmass, gluinomass)
-
-    def runForDecoupled ( self, energy, nllinput ):
+    def _runForDecoupled ( self, energy, nllinput ):
         nll_run = "./nllfast_" + energy + " %s %s %s %s" % nllinput
-        return self.run ( nll_run )
+        return self._run ( nll_run )
 
     def checkInstallation(self):
         """
@@ -294,11 +227,12 @@ class ExternalNllFast(ExternalTool):
         if not os.access(self.executablePath, os.X_OK):
             logger.error("%s is not executable", self.executable)
             return False
-        out = self.run_(self.testParams)
+        cmd = "cd %s; %s %s" % (self.cdPath, self.executablePath, self.testParams)
+        out = executor.getoutput(cmd)
+        out = out.split("\n")
         if out[-1].find(self.testCondition) == -1:
             logger.error("Setup invalid: " + str(out))
             return False
-        self.unlink("gg")
         return True
 
     def getKfactorsFor( self, pIDs, slhafile, pdf='cteq' ):
@@ -314,14 +248,9 @@ class ExternalNllFast(ExternalTool):
             logger.error("SLHA file %s not found", slhafile)
             return False
 
-        # box = toolBox.ToolBox()
-        # Set up NLLfast run, the old way
-        # sqrtS = float(sqrts/TeV)
         energy = str(int(self.sqrts)) + 'TeV'
-        # toolname = "nllfast%d" % int(sqrtS)
-        # tool = box.get(toolname)
         # Get process name (in NLLfast notation)
-        process = self.getProcessName(pIDs)
+        process = self._getProcessName(pIDs)
         if not process:
             # Return k-factors = None, if NLLfast does not have the process
             return (None, None)
@@ -352,14 +281,14 @@ class ExternalNllFast(ExternalTool):
         #    logger.warning("No NLLfast data for sqrts = " + str(sqrts))
         #    return (None, None)
         nllpath = self.installDirectory()
-        self.pathOfExecutable()
+        # self.pathOfExecutable()
         self.checkInstallation()
-        nll_output = self.compute ( energy, pIDs, pdf, squarkmass, gluinomass )
+        nll_output = self._compute ( energy, pIDs, pdf, squarkmass, gluinomass )
 
         # If run was successful, return k-factors:
         if "K_NLO" in nll_output:
             # NLLfast ran ok, try to get the k-factors
-            kFacs = self.getKfactorsFrom(nll_output)
+            kFacs = self._getKfactorsFrom(nll_output)
             if not kFacs or min(kFacs) <= 0.:
                 logger.warning("Error obtaining k-factors")
                 return (None, None)
@@ -387,7 +316,7 @@ class ExternalNllFast(ExternalTool):
             return (None, None)
 
         # Obtain k-factors from the NLLfast decoupled grid
-        kfacs = self.getDecoupledKfactors(process,energy,pdf,min(gluinomass,squarkmass))
+        kfacs = self._getDecoupledKfactors(process,energy,pdf,min(gluinomass,squarkmass))
         # Decoupling limit is satisfied, do not interpolate
         if not kfacs:
             logger.warning("Error obtaining k-factors from the NLLfast decoupled grid for " + process)
@@ -402,10 +331,8 @@ class ExternalNllFast(ExternalTool):
                 dcpl_mass -= 100.  # Reduce decoupled mass, until NLLfast produces results
                 if process == 'sb': nllinput = (process, pdf, squarkmass, dcpl_mass)
                 else:  nllinput = (process, pdf, dcpl_mass, gluinomass)
-                nll_output = self.runForDecoupled ( energy, nllinput )
-                #nll_run = "./nllfast_" + energy + " %s %s %s %s" % nllinput
-                #nll_output = tool.run(nll_run )
-                kfacs = self.getKfactorsFrom(nll_output)        
+                nll_output = self._runForDecoupled ( energy, nllinput )
+                kfacs = self._getKfactorsFrom(nll_output)        
             kFacsVector.append([dcpl_mass, kfacs]) #Second point for interpolation (non-decoupled grid)
 
         if len(kFacsVector) < 2:
@@ -414,7 +341,7 @@ class ExternalNllFast(ExternalTool):
             return (None, None)
         else:
             # Interpolate k-factors
-            kFacs = self.interpolateKfactors(kFacsVector,
+            kFacs = self._interpolateKfactors(kFacsVector,
                             max(squarkmass, gluinomass))
         return kFacs
 
