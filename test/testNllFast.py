89f4daacd48703be5fdc99563dbdf7b826f14e9f
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-02-13 16:49:05 +0100

    nllfast refactored, v1
    Former-commit-id: a2c0f334fbcebf767517d6a8a909d789c15dcc05

--- test/testNllFast.py
+++ test/testNllFast.py
@@ -11,24 +11,30 @@ import sys
 sys.path.insert(0,"../")
 import unittest
 import math
-from smodels.tools import nllFast, externalNllFast
+from smodels.tools import toolBox
 from smodels.tools.physicsUnits import TeV
+
+path = "../inputFiles/slha/"
 
 class NllFastTest(unittest.TestCase):
     def testGluino7 (self):
-        o = nllFast.getKfactorsFor ( (1000021, 1000021 ), 7*TeV, "../inputFiles/slha/simplyGluino.slha" )
+        tool = toolBox.ToolBox().get("nllfast7")
+        o = tool.getKfactorsFor ( (1000021, 1000021 ), "%ssimplyGluino.slha" % path )
         self.assertAlmostEqual ( o[0], 2.1 )
         self.assertAlmostEqual ( o[1], 1.08 )
     def testSquark8 (self):
-        o = nllFast.getKfactorsFor ( (1000001, 1000001 ), 8*TeV, "../inputFiles/slha/gluino_squarks.slha" )
+        tool = toolBox.ToolBox().get("nllfast8")
+        o = tool.getKfactorsFor ( (1000001, 1000001 ), "%sgluino_squarks.slha"%path )
         self.assertAlmostEqual ( o[0], 1.22 )
         self.assertAlmostEqual ( o[1], 1.02 )
     def testSquark13 (self):
-        o = nllFast.getKfactorsFor ( (1000001, 1000001 ), 13*TeV, "../inputFiles/slha/gluino_squarks.slha" )
+        tool = toolBox.ToolBox().get("nllfast13")
+        o = tool.getKfactorsFor ( (1000001, 1000001 ), "%sgluino_squarks.slha" % path)
         self.assertAlmostEqual ( o[0], 1.24 )
         self.assertAlmostEqual ( o[1], 1.01 )
     def testWeakino8 (self):
-        o = nllFast.getKfactorsFor ( (1000022, 1000022 ), 13*TeV, "../inputFiles/slha/complicated.slha" )
+        tool = toolBox.ToolBox().get("nllfast13")
+        o = tool.getKfactorsFor ( (1000022, 1000022 ), "../inputFiles/slha/complicated.slha" )
         self.assertEqual ( o[0], None )
         self.assertEqual ( o[1], None )
 
