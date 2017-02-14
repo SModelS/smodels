2ffef0a86c6296e03f231384df903882fe8ca073
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-02-14 11:05:25 +0100

    curl added
    Former-commit-id: 00afebc8df5a01edaab1f586f44569a3b3cfc7d8

--- lib/pythia8/installer.sh
+++ lib/pythia8/installer.sh
@@ -2,8 +2,9 @@
 
 VER='8223'
 TARBALL="pythia$VER.tgz"
+URL=http://home.thep.lu.se/~torbjorn/pythia8/$TARBALL
 
-test -e $TARBALL && { echo "[installer] tarball $TARBALL exists"; } || { echo "[installer] getting $TARBALL"; wget http://home.thep.lu.se/~torbjorn/pythia8/$TARBALL; };
+test -e $TARBALL && { echo "[installer] tarball $TARBALL exists"; } || { echo "[installer] getting $TARBALL"; wget $URL 2>/dev/null || curl -O $URL; };
 
 test -e pythia$VER && { echo "[installer] pythia$VER directory exists"; } || { echo "[installer] $TARBALL"; tar xvf $TARBALL; };
 
