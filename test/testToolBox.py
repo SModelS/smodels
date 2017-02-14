47a4a6ee92b5b0772a667af15476792978ff1e3f
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-02-14 17:29:21 +0100

    toolbox fix, c++ interface
    Former-commit-id: d63fafd0f8bc01d252080f46116124ad0f53de24

--- test/testToolBox.py
+++ test/testToolBox.py
@@ -19,7 +19,7 @@ class TestToolBox(unittest.TestCase):
     def testToolBox(self):
         self.logger.info ( "ToolBox" )
         box = toolBox.ToolBox()
-        ok=box.checkInstallation ( colors=False, make=True, printit=False )
+        ok=box.checkInstallation ( make=True, printit=False )
         self.assertTrue ( ok )
 
 if __name__ == "__main__":
