0a39f3a6afe8b390b7bf3de931428c6f5b5114dd
Andre Lessa <lessa.a.p@gmail.com>
2017-03-07 20:30:52 -0300

    Added LHE output to Pythia8
    Former-commit-id: 8497e6b6fb6be48f1aea40d052b866aab409835e

--- lib/pythia8/pythia8.cc
+++ lib/pythia8/pythia8.cc
@@ -8,7 +8,7 @@ using namespace Pythia8;
 using namespace std;
 
 int run( int nevents, float sqrts /* in TeV */, const string & slhafile,
-         const string & cfgfile, const string & xmldir )
+         const string & cfgfile, const string & lhefile, const string & xmldir )
 {
   cout << "[pythia8.exe] we run with " << nevents << ", sqrts=" << sqrts << " TeV"
        << ", slhafile=" << slhafile << endl;
@@ -21,24 +21,52 @@ int run( int nevents, float sqrts /* in TeV */, const string & slhafile,
   pythia.readString( o2.str() );
   o3 << "SLHA:file=" << slhafile;
   pythia.readString ( o3.str() );
+
+
+  // Create an LHAup object that can access relevant information in pythia.
+  LHAupFromPYTHIA8 myLHA(&pythia.process, &pythia.info);
+
+  // Open a file on which LHEF events should be stored, and write header.
+  myLHA.openLHEF(lhefile);
+
   pythia.init();
+  // Store initialization info in the LHAup object.
+  myLHA.setInit();
+  // Write out this initialization info on the file.
+  myLHA.initLHEF();
 
   // Begin event loop. Generate event. Skip if error.
   for (int iEvent = 0; iEvent < nevents; ++iEvent ) {
     if (!pythia.next()) continue;
+    
+    // Store event info in the LHAup object.
+    myLHA.setEvent();
+
+    // Write out this event info on the file.
+    // With optional argument (verbose =) false the file is smaller.
+    myLHA.eventLHEF();
+
   }
   pythia.stat();
+
+  // Update the cross section info based on Monte Carlo integration during run.
+  myLHA.updateSigma();
+
+  // Write endtag. Overwrite initialization info with new cross sections.
+  myLHA.closeLHEF(true);
+
   return 0;
 }
 
 void help( const char * name )
 {
   cout << "syntax: " << name << " [-h] [-n <nevents>] [-s <sqrts>] [-f <slhafile>] " 
-       << "[-c <cfgfile>]" << endl;
+       << "[-c <cfgfile>] [-l <lhefile>]" << endl;
   cout << "        -n <nevents>:  number of events to simulate [10000]." << endl;
   cout << "        -s <sqrts>:    center-of mass energy, in TeV [13]." << endl;
   cout << "        -f <slhafile>: slhafile [test.slha]." << endl;
   cout << "        -c <cfgfile>: the pythia8 config file [./pythia8.cfg]." << endl;
+  cout << "        -l <lhefile>: the lhe output file [./events.lhe]." << endl;
   cout << "        -x <xmldir>: the pythia8 xmldoc dir [../xmldoc]." << endl;
   exit( 0 );
 };
@@ -48,6 +76,7 @@ int main( int argc, const char * argv[] ) {
   float sqrts = 13;
   string slhafile = "test.slha";
   string cfgfile = "./pythia8.cfg";
+  string lhefile = "./events.lhe";
   string xmlDir = "../xmldoc";
   for ( int i=1; i!=argc ; ++i )
   {
@@ -81,6 +110,14 @@ int main( int argc, const char * argv[] ) {
       continue;
     }
 
+    if ( s== "-l" )
+    {
+      if ( argc < i+2 ) help ( argv[0] );
+      lhefile = argv[i+1];
+      i++;
+      continue;
+    }
+
     if ( s== "-x" )
     {
       if ( argc < i+2 ) help ( argv[0] );
@@ -106,5 +143,5 @@ int main( int argc, const char * argv[] ) {
     help ( argv[0] );
   };
 
-  return run ( nevents, sqrts, slhafile, cfgfile, xmlDir );
+  return run ( nevents, sqrts, slhafile, cfgfile, lhefile, xmlDir );
 }
