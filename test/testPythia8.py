d89a841527699fb2c0ab471c8407994e9f66aa09
Andre Lessa <lessa.a.p@gmail.com>
2017-03-07 19:43:18 -0300

    Pythia8 unit test working
    Former-commit-id: 7c9a5fe5dcca70dba39364116bb1981e592d2feb

--- test/testPythia8.py
+++ test/testPythia8.py
@@ -14,38 +14,47 @@ from smodels.tools import xsecComputer, toolBox
 from smodels.tools.xsecComputer import LO, NLL
 from smodels.tools.physicsUnits import TeV, fb
 from smodels.theory import crossSection
+from unum import Unum
 import tempfile
 import unittest
 import argparse
 import logging.config
 from math import sqrt
 
-Nevents = 10000
+Unum.VALUE_FORMAT = "%.4f"  
+
+Nevents = 50000
 
 def compareXSections(dictA,dictB,nevts):
 
     missingXsecs = set(dictA.keys()).symmetric_difference(set(dictB.keys()))
-    commonXsecs = set(dictA.keys()).union(set(dictB.keys()))
+    commonXsecs = set(dictA.keys()).intersection(set(dictB.keys()))
     totXsecA = sum([x.values()[0].asNumber(fb) for x in dictA.values()])
     totXsecB = sum([x.values()[0].asNumber(fb) for x in dictB.values()])
     totXsec = max(totXsecA,totXsecB)*fb
     mcError = totXsec/sqrt(float(nevts))
 
+    equalXsecs = True
     for xsec in commonXsecs:
         diff = abs(dictA[xsec].values()[0] - dictB[xsec].values()[0])
         if diff > 2.*mcError and (diff/(abs(dictA[xsec].values()[0] + dictB[xsec].values()[0]))).asNumber() > 0.1:
             print 'Cross-section for %s differ by (%s +- %s) fb' %(str(xsec),str(diff.asNumber(fb)),str(mcError.asNumber(fb)))
-            return False
+            print '   %s   %s' %(dictA[xsec].values()[0],dictB[xsec].values()[0])
+            equalXsecs = False
 
     for xsec in missingXsecs:
         if xsec in dictA:
             if dictA[xsec].values()[0] > 2.*mcError:
                 print 'Cross-section for %s missing' %str(xsec)
-                return False
+                print '    %s' %(dictA[xsec].values()[0])
+                equalXsecs = False
         else:
             if dictB[xsec].values()[0] > 2.*mcError:
                 print 'Cross-section for %s missing' %str(xsec)
-                return False
+                print '    %s' %(dictB[xsec].values()[0])
+                equalXsecs =  False
+                
+    return equalXsecs
 
 
 class XSecTest(unittest.TestCase):
@@ -59,13 +68,17 @@ class XSecTest(unittest.TestCase):
     def testLOGlu(self):
         """ test the computation of LO cross section and compare with pythia6 """
         self.logger.info ("test LO xsecs @ 8 TeV")
-        slhafile="../inputFiles/slha/simplyGluino.slha"
+        slhafile  = "../inputFiles/slha/gluino_squarks.slha"
         computer6 = xsecComputer.XSecComputer(LO, Nevents, 6)
         computer8 = xsecComputer.XSecComputer(LO, Nevents, 8)
         w6 = computer6.compute(8*TeV, slhafile).getDictionary()
-        w8 = computer8.compute(8*TeV, slhafile).getDictionary()
-        print w6
-        print w8
+        w8 = computer8.compute(8*TeV, slhafile, pythiacard = './pythia8_to_pythia6.cfg').getDictionary()
+        self.assertEqual(compareXSections(w6, w8,Nevents),True) 
+        slhafile  = "../inputFiles/slha/lightEWinos.slha"
+        computer6 = xsecComputer.XSecComputer(LO, Nevents, 6)
+        computer8 = xsecComputer.XSecComputer(LO, Nevents, 8)
+        w6 = computer6.compute(8*TeV, slhafile).getDictionary()
+        w8 = computer8.compute(8*TeV, slhafile, pythiacard = './pythia8_to_pythia6.cfg').getDictionary()
         self.assertEqual(compareXSections(w6, w8,Nevents),True) 
         
 
