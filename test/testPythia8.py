1825e8b6a19e1a6a021a711cd0c48cab56a8dae3
Andre Lessa <lessa.a.p@gmail.com>
2017-03-08 17:10:32 -0300

    Added extense pythia 8 test
    Former-commit-id: 797586855ac1d1f40c49aab6f8606bd6e8397783

--- test/testPythia8.py
+++ test/testPythia8.py
@@ -25,7 +25,7 @@ Unum.VALUE_FORMAT = "%.4f"
 
 Nevents = 50000
 
-def compareXSections(dictA,dictB,nevts):
+def compareXSections(dictA,dictB,nevts,relError = 0.1):
 
     missingXsecs = set(dictA.keys()).symmetric_difference(set(dictB.keys()))
     commonXsecs = set(dictA.keys()).intersection(set(dictB.keys()))
@@ -33,11 +33,13 @@ def compareXSections(dictA,dictB,nevts):
     totXsecB = sum([x.values()[0].asNumber(fb) for x in dictB.values()])
     totXsec = max(totXsecA,totXsecB)*fb
     mcError = totXsec/sqrt(float(nevts))
+    
+    diffXsecs = []
 
     equalXsecs = True
     for xsec in commonXsecs:
         diff = abs(dictA[xsec].values()[0] - dictB[xsec].values()[0])
-        if diff > 2.*mcError and (diff/(abs(dictA[xsec].values()[0] + dictB[xsec].values()[0]))).asNumber() > 0.1:
+        if diff > 2.*mcError and (diff/(abs(dictA[xsec].values()[0] + dictB[xsec].values()[0]))).asNumber() > relError:
             print 'Cross-section for %s differ by (%s +- %s) fb' %(str(xsec),str(diff.asNumber(fb)),str(mcError.asNumber(fb)))
             print '   %s   %s' %(dictA[xsec].values()[0],dictB[xsec].values()[0])
             equalXsecs = False
@@ -73,13 +75,13 @@ class XSecTest(unittest.TestCase):
         computer8 = xsecComputer.XSecComputer(LO, Nevents, 8)
         w6 = computer6.compute(8*TeV, slhafile).getDictionary()
         w8 = computer8.compute(8*TeV, slhafile, pythiacard = './pythia8_to_pythia6.cfg').getDictionary()
-        self.assertEqual(compareXSections(w6, w8,Nevents),True) 
+        self.assertEqual(compareXSections(w6, w8,Nevents),[]) 
         slhafile  = "../inputFiles/slha/lightEWinos.slha"
         computer6 = xsecComputer.XSecComputer(LO, Nevents, 6)
         computer8 = xsecComputer.XSecComputer(LO, Nevents, 8)
         w6 = computer6.compute(8*TeV, slhafile).getDictionary()
         w8 = computer8.compute(8*TeV, slhafile, pythiacard = './pythia8_to_pythia6.cfg').getDictionary()
-        self.assertEqual(compareXSections(w6, w8,Nevents),True) 
+        self.assertEqual(compareXSections(w6, w8,Nevents),[]) 
         
 
 if __name__ == "__main__":
