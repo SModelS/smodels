4ce010e31219869a58bfebef1c52caed4e1e2a11
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-05-08 14:21:28 +0200

    CMS-SUS-PAS -> CMS-PAS-SUS
    Former-commit-id: 251e070d2ec8669c42e2e452a0f9f635a1ffebb8

--- test/testRunSModelS.py
+++ test/testRunSModelS.py
@@ -153,6 +153,8 @@ class RunSModelSTest(unittest.TestCase):
                     res['AnalysisID'],res['DataSetID']])
         equals = equalObjs(smodelsOutput,smodelsOutputDefault,allowedDiff=0.02,
                            ignore=ignoreFields)
+        if not equals:
+            print "output13.py and simplyGluino_default.py differ!"
         for i in [ './output13.py', './output13.pyc' ]:
             if os.path.exists ( i ):
                 continue
