377651a59fa1d9a9f43612f07a80067249cd8a31
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-05-15 20:49:25 +0200

    updated manual for pythia8, backcontribution from cpc, ...
    Former-commit-id: a9056cc1da82718c5205bb2795265dc5f4af7d41

--- docs/manual/source/recipes/convert.sh
+++ docs/manual/source/recipes/convert.sh
@@ -4,7 +4,7 @@
 CMD=jupyter
 
 for i in `ls *.ipynb`; do
-	$CMD nbconvert --to html $i;
+	$CMD nbconvert --to html $i || echo "\nERROR: cannot execute $CMD nbconvert. Maybe install $CMD-nbconvert?\n\n";
   $CMD nbconvert --to python $i;
 done
 
