d2b421b3aa946302621a3924af2a9f9afbf44a66
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-05-16 17:29:41 +0200

    make tarball hidden
    Former-commit-id: 6dce4ff9618bc25e250c19fcad30ecfe24dabc1e

--- lib/pythia8/installer.sh
+++ lib/pythia8/installer.sh
@@ -9,8 +9,8 @@ VER='8223'
 TARBALL="pythia${VER}_fixed.tgz"
 URL=http://smodels.hephy.at/.hidden/$TARBALL
 
-test -e $TARBALL && { echo "[installer] tarball $TARBALL exists"; } || { echo "[installer] getting $TARBALL"; wget $URL 2>/dev/null || curl -O $URL; };
+test -e .$TARBALL && { echo "[installer] tarball $TARBALL exists"; } || { echo "[installer] getting $TARBALL"; wget $URL 2>/dev/null || curl -O $URL; mv $TARBALL .$TARBALL; };
 
-test -e pythia$VER && { echo "[installer] pythia$VER directory exists"; } || { echo "[installer] $TARBALL"; tar xvf $TARBALL; };
+test -e pythia$VER && { echo "[installer] pythia$VER directory exists"; } || { echo "[installer] $TARBALL"; tar xvf .$TARBALL; };
 
 test -e pythia$VER/lib/libpythia8.a && { echo "[installer] libpythia8.a exists"; } || { echo "[installer] building pythia8"; cd pythia$VER; ./configure; make; };
