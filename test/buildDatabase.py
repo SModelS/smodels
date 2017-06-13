088dee548d49124205c384b75cb045aab7960a3d
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-06-13 17:31:47 +0200

    make it a real builder
    Former-commit-id: ece2f4631405401b03c505bccf0a9e74b0df84ed

--- test/buildDatabase.py
+++ test/buildDatabase.py
@@ -6,19 +6,21 @@ from smodels.experiment.databaseObj import Database
 from smodels.tools.smodelsLogging import setLogLevel
 from smodels.tools.colors import colors
 colors.on = True
-setLogLevel ( "debug" )
+# setLogLevel ( "debug" )
 
-def build ( force=False ):
-    Dir = "tinydb/"
-    Dir = "database/"
+def build ( force=False, dir=None ):
     force_load = None
     if force: force_load = "txt"
-    d=Database( Dir, force_load = force_load, discard_zeroes = True )
+    d=Database( dir, force_load = force_load, discard_zeroes = True, 
+                progressbar=True )
     if force:
         d.createBinaryFile()
-    print ( d.databaseVersion )
+    print ( d )
 
-force=False
-if len(sys.argv)>1 and sys.argv[1]=="-f":
-    force=True
-build(force)
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Simple database builder (i.e. pickelizer)")
+    parser.add_argument('-f', '--force', help='force text mode', action='store_true')
+    parser.add_argument('-d', '--dir', help='database dir', default="./tinydb", type=str )
+    args=parser.parse_args()
+    build( args.force, args.dir )
