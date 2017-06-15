1a6cfdc02cee5e2748f1a59f5fe187c5f9770876
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-06-15 18:19:52 +0200

    pythia8223fixed -> pythia8226
    Former-commit-id: 52f7e4bb79a4182a9d643e713c2e596c31a54056

--- lib/pythia8/installer.sh
+++ lib/pythia8/installer.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-VER='8223'
+VER='8226'
 [ -z "$1" ] && { echo "pythia8 version is not given, use pythia$VER"; } || 
 		{ VER="$1"; echo "set to version pythia$VER"; }
 
-#TARBALL="pythia$VER.tgz"
-#URL=http://home.thep.lu.se/~torbjorn/pythia8/$TARBALL
-TARBALL="pythia${VER}_fixed.tgz"
-URL=http://smodels.hephy.at/.hidden/$TARBALL
+TARBALL="pythia$VER.tgz"
+URL=http://home.thep.lu.se/~torbjorn/pythia8/$TARBALL
+#TARBALL="pythia${VER}_fixed.tgz"
+#URL=http://smodels.hephy.at/.hidden/$TARBALL
 
 test -e .$TARBALL && { echo "[installer] tarball $TARBALL exists"; } || { echo "[installer] getting $TARBALL"; wget $URL 2>/dev/null || curl -O $URL; mv $TARBALL .$TARBALL; };
 
