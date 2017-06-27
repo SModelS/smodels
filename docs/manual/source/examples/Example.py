853fda9107573e016c0b22cd18bbdad85086aaac
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-06-27 11:23:13 +0200

    Example.py changed, ini files harmonized.
    Former-commit-id: 0728db545e83cfaada1fdf57ca0e3a0601680f60

--- docs/manual/source/examples/Example.py
+++ docs/manual/source/examples/Example.py
@@ -17,9 +17,11 @@ from smodels.tools.physicsUnits import fb, GeV, TeV
 from smodels.theory.theoryPrediction import theoryPredictionsFor
 from smodels.experiment.databaseObj import Database
 from smodels.tools import coverage
+from smodels.tools.smodelsLogging import setLogLevel
+setLogLevel("info")
 
 # Set the path to the database folder
-database = Database("../smodels-database/")
+database = Database("./smodels-database/")
 
 def main():
     """
