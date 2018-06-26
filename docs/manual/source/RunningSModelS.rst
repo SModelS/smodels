e4ba8d20e9d6f67c36de772baded63d1f7dd0e7c
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-06-26 18:34:18 +0200

    rename particles:module to particles:model

--- docs/manual/source/RunningSModelS.rst
+++ docs/manual/source/RunningSModelS.rst
@@ -144,7 +144,7 @@ Below we give more detailed information about each entry in the parameters file.
 
 * *particles*: defines the particle content of the BSM model
  
-  * **module**: pathname to the Python file that defines the particle content of the BSM model, given either in Unix file notation ("/path/to/module.py") or as Python module path ("path.to.module"). Defaults to *share.models.default_particles* which is a standard MSSM. See smodels/share/models folder for more examples. Directory name can be omitted; in that case, the current working directory as well as smodels/share/models are searched for.
+  * **model**: pathname to the Python file that defines the particle content of the BSM model, given either in Unix file notation ("/path/to/model.py") or as Python module path ("path.to.model"). Defaults to *share.models.default_particles* which is a standard MSSM. See smodels/share/models folder for more examples. Directory name can be omitted; in that case, the current working directory as well as smodels/share/models are searched for.
 
 * *database*: allows for selection of a subset of :ref:`experimental results <ExpResult>` from the |database|
 
--- smodels/tools/runSModelS.py
+++ smodels/tools/runSModelS.py
@@ -3,7 +3,7 @@
 """
 .. module:: runSModelS
    :synopsis: Main code for running SModelS.
-   
+
 """
 
 from __future__ import print_function
@@ -23,14 +23,14 @@ def main():
     """ Get the name of input SLHA file and parameter file """
     ap = argparse.ArgumentParser( description=
             "Run SModelS over SLHA/LHE input files." )
-    ap.add_argument('-f', '--filename', 
+    ap.add_argument('-f', '--filename',
             help='name of SLHA or LHE input file or a directory path (required argument). '
             'If a directory is given, loop over all files in the directory', required=True)
-    ap.add_argument('-p', '--parameterFile', 
+    ap.add_argument('-p', '--parameterFile',
             help='name of parameter file, where most options are defined (optional argument). If not set, use '
-            'all parameters from smodels/etc/parameters_default.ini', 
+            'all parameters from smodels/etc/parameters_default.ini',
             default=parameterFile)
-    ap.add_argument('-o', '--outputDir', 
+    ap.add_argument('-o', '--outputDir',
             help='name of output directory (optional argument). The default folder is: ' +
             outputDir, default=outputDir)
     ap.add_argument('-d', '--development', help='if set, SModelS will run in development mode and exit if any errors are found.',
@@ -40,43 +40,43 @@ def main():
     ap.add_argument('-C', '--colors', help='colored output',
             action='store_true')
     ap.add_argument('-V', '--version', action='version', version = version() )
-    ap.add_argument('-c', '--run-crashreport', 
+    ap.add_argument('-c', '--run-crashreport',
             help='parse crash report file and use its contents for a SModelS run. '
                  "Supply the crash file simply via '--filename myfile.crash'",
             action='store_true')
     ap.add_argument('-v','--verbose', help='sets the verbosity level (debug, info, warning, error). Default value is info.',
             default = "info", type = str )
-    ap.add_argument('-T', '--timeout', 
+    ap.add_argument('-T', '--timeout',
             help='define a limit on the running time (in secs).'
                  'If not set, run without a time limit. If a directory is given as input, '
                  'the timeout will be  applied for each individual file.',
             default = 0, type = int)
-    
-    
+
+
     args = ap.parse_args()
     if args.colors:
         from smodels.tools.colors import colors
         colors.on = True
-        
-        
+
+
     db=None
     if args.force_txt: db=True
     smodelsLogging.setLogLevel ( args.verbose )
-    
-    if args.run_crashreport: 
+
+    if args.run_crashreport:
         args.filename, args.parameterFile = crashReport.readCrashReportFile(
                 args.filename)
         run(args.filename, args.parameterFile, args.outputDir,
                db, args.timeout, development=True )
-        
+
     else:
-        run(args.filename, args.parameterFile, args.outputDir, 
+        run(args.filename, args.parameterFile, args.outputDir,
               db, args.timeout, args.development)
 
 def run( inFile, parameterFile, outputDir, db, timeout, development ):
     """
     Provides a command line interface to basic SModelS functionalities.
-    
+
     :param inFile: input file name (either a SLHA or LHE file)
                    or directory name (path to directory containing input files)
     :param parameterFile: File containing the input parameters (default =
@@ -89,15 +89,15 @@ def run( inFile, parameterFile, outputDir, db, timeout, development ):
             If True, force loading the text database.
     :param timeout: set a timeout for one model point (0 means no timeout)
     :param development: turn on development mode (e.g. no crash report)
-    
+
     """
 
     """ Read and check parameter file, exit parameterFile does not exist """
     parser = modelTester.getParameters(parameterFile)
 
-    """ Determine particles-Module from ini-file, if necessary"""
-    if parser.has_option("particles","module"):
-        runtime.modelFile = parser.get( "particles", "module" )     
+    """ Determine particles module from ini file, if necessary """
+    if parser.has_option("particles","model"):
+        runtime.modelFile = parser.get( "particles", "model" )
 
     """ Check database location and load database, exit if not found """
     database, databaseVersion = modelTester.loadDatabase(parser, db)
@@ -112,7 +112,7 @@ def run( inFile, parameterFile, outputDir, db, timeout, development ):
     listOfExpRes = modelTester.loadDatabaseResults(parser, database)
 
     """ Test all input points """
-    modelTester.testPoints( fileList, inDir, outputDir, parser, databaseVersion, 
+    modelTester.testPoints( fileList, inDir, outputDir, parser, databaseVersion,
                  listOfExpRes, timeout, development, parameterFile )
 
 if __name__ == "__main__":
