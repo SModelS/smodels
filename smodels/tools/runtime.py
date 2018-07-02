682865f74c7578946942c7f9cbc0d5f694375f69
Andre Lessa <lessa.a.p@gmail.com>
2018-07-02 06:31:27 -0300

    Renamed default_particles.py to mssm.py

--- smodels/tools/runtime.py
+++ smodels/tools/runtime.py
@@ -11,7 +11,7 @@
 """
 
 ## place to keep the pointer to the model file
-modelFile="share.models.default_particles"
+modelFile="share.models.mssm"
 
 def filetype ( filename ):
     """ obtain information about the filetype of an input file,
