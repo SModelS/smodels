21dfc5ef672fa1789a685c57b5e81bc827a7e495
Andre Lessa <lessa.a.p@gmail.com>
2018-07-05 12:34:56 -0300

    Fix in unit tests. All working

--- test/testPrinter.py
+++ test/testPrinter.py
@@ -13,73 +13,18 @@ sys.path.insert(0,"../")
 import unittest
 from smodels.installation import installDirectory as idir
 from smodels.theory import slhaDecomposer
-from smodels.tools.physicsUnits import fb, GeV, TeV
-from smodels.tools.smodelsLogging import logger
+from smodels.tools.physicsUnits import fb, GeV
 from smodels.theory.theoryPrediction import theoryPredictionsFor
 from smodels.tools import printer, ioObjects
 from smodels.tools import coverage
 import summaryReader
 from xml.etree import ElementTree
 from databaseLoader import database
-import unum
 from smodels.tools.smodelsLogging import setLogLevel
+from unitTestHelpers import equalObjs
+
 
 tol = 0.07
-
-def equalObjs(obj1,obj2,allowedDiff,ignore=[], where=None ):
-    """
-    Compare two objects.
-    The numerical values are compared up to the precision defined by allowedDiff.
-    
-    :param obj1: First python object to be compared 
-    :param obj2: Second python object to be compared
-    :param allowedDiff: Allowed % difference between two numerical values 
-    :param ignore: List of keys to be ignored
-    :return: True/False    
-    """      
-
-    if type(obj1) in [ float, int ] and type ( obj2) in [ float, int ]:
-        obj1,obj2=float(obj1),float(obj2)
-    
-    if type(obj1) != type(obj2):
-        logger.info("Data types differ (%s,%s)" %(type(obj1),type(obj2)))
-        return False
-    
-    if isinstance(obj1,unum.Unum):
-        if obj1 == obj2:
-            return True
-        diff = 2.*abs(obj1-obj2)/abs(obj1+obj2)
-        return diff.asNumber() < allowedDiff
-    elif isinstance(obj1,float):        
-        if obj1 == obj2:
-            return True
-        diff = 2.*abs(obj1-obj2)/abs(obj1+obj2)
-        return diff < allowedDiff
-    elif isinstance(obj1,str):
-        return obj1 == obj2
-    elif isinstance(obj1,dict):    
-        for key in obj1:
-            if key in ignore: continue
-            if not key in obj2:
-                logger.warning("Key %s missing" %key)
-                return False
-            if not equalObjs(obj1[key],obj2[key],allowedDiff,ignore=ignore, where=key ):
-                logger.warning('Objects differ in %s:\n   %s\n and\n   %s' %(where, str(obj1[key]),str(obj2[key])))
-                return False
-    elif isinstance(obj1,list):
-        if len(obj1) != len(obj2):
-            logger.warning("Objects differ in length:\n %s (len = %i) \n and\n %s (len = %i)" 
-                            %(str(obj1),len(obj1),str(obj2),len(obj2)))
-            return False
-        for ival,val in enumerate(obj1):
-            if not equalObjs(val,obj2[ival],allowedDiff, where = where ):
-                logger.warning('Objects differ:\n   %s \n and\n   %s' %( str(val),str(obj2[ival])))
-                return False
-    else:
-        return obj1 == obj2
-            
-    return True
-
 
 
 def sortXML(xmltree):
@@ -206,11 +151,11 @@ class RunPrinterTest(unittest.TestCase):
         mprinter.setOutPutFiles('./unitTestOutput/printer_output')
         self.runPrinterMain(slhafile,mprinter)
            
+        from unitTestOutput.printer_output import smodelsOutput
         #Test python output
-        shutil.copyfile('./unitTestOutput/printer_output.py','./output.py')
         from gluino_squarks_default import smodelsOutputDefault 
-        from output import smodelsOutput
-        ignoreFields = ['input file','smodels version', 'ncpus']
+        ignoreFields = ['input file','smodels version', 'ncpus', 'database version' ]
+
         smodelsOutputDefault['ExptRes'] = sorted(smodelsOutputDefault['ExptRes'], 
                       key=lambda res: [res['theory prediction (fb)'],res['TxNames'],
                                        res['AnalysisID'],res['DataSetID']])
--- test/testRunSModelS.py
+++ test/testRunSModelS.py
@@ -9,99 +9,21 @@
 
 """
 
-import sys,shutil,os
+import sys,shutil,os,imp
 sys.path.insert(0,"../")
 import unittest
 import glob
-from os.path import join, basename
+from os.path import join
 from smodels.installation import installDirectory as iDir
 from smodels.tools import crashReport
 from smodels.tools.timeOut import NoTime
-from databaseLoader import database ## to make sure the db exists
-from smodels.tools.runSModelS import run
-import redirector
-import unum
+from unitTestHelpers import equalObjs, runMain
 import time
-
 from smodels.tools.smodelsLogging import logger, setLogLevel
 
-def equalObjs(obj1,obj2,allowedDiff,ignore=[]):
-    """
-    Compare two objects.
-    The numerical values are compared up to the precision defined by allowedDiff.
-
-    :param obj1: First python object to be compared
-    :param obj2: Second python object to be compared
-    :param allowedDiff: Allowed % difference between two numerical values
-    :param ignore: List of keys to be ignored
-    :return: True/False
-    """
-    if type(obj1) in [ float, int ] and type ( obj2) in [ float, int ]:
-        obj1,obj2=float(obj1),float(obj2)
-
-    if type(obj1) != type(obj2):
-        logger.info("Data types differ (%s,%s)" %(type(obj1),type(obj2)))
-        return False
-
-    if isinstance(obj1,unum.Unum):
-        if obj1 == obj2:
-            return True
-        diff = 2.*abs(obj1-obj2)/abs(obj1+obj2)
-        return diff.asNumber() < allowedDiff
-    elif isinstance(obj1,float):
-        if obj1 == obj2:
-            return True
-        diff = 2.*abs(obj1-obj2)/abs(obj1+obj2)
-        return diff < allowedDiff
-    elif isinstance(obj1,str):
-        return obj1 == obj2
-    elif isinstance(obj1,dict):
-        for key in obj1:
-            if key in ignore: continue
-            if not key in obj2:
-                logger.warning("Key %s missing" %key)
-                return False
-            if not equalObjs(obj1[key],obj2[key],allowedDiff, ignore=ignore ):
-                logger.warning( "Dictionaries differ in key ``%s''" % key )
-                s1,s2 = str(obj1[key]),str(obj2[key]) 
-                if len(s1) + len(s2) > 200:
-                    logger.warning ( "The values are too long to print." )
-                else:
-                    logger.warning( 'The values are: %s (this run) versus %s (default)'%\
-                                ( s1,s2 ) )
-                return False
-    elif isinstance(obj1,list):
-        if len(obj1) != len(obj2):
-            logger.warning('Lists differ in length:\n   %i (this run)\n and\n   %i (default)' %\
-                                (len(obj1),len(obj2)))
-            return False
-        for ival,val in enumerate(obj1):
-            if not equalObjs(val,obj2[ival],allowedDiff):
-                logger.warning('Lists differ:\n   %s (this run)\n and\n   %s (default)' %\
-                                (str(val),str(obj2[ival])))
-                return False
-    else:
-        return obj1 == obj2
-
-    return True
 
 
 class RunSModelSTest(unittest.TestCase):
-    def runMain(self, filename, timeout = 0, suppressStdout=True, development=False,
-                 inifile = "testParameters.ini" ):
-        to = None
-        level = 'debug'
-        if suppressStdout:
-            level = 'error'
-            to = os.devnull
-        with redirector.stdout_redirected ( to = to ):
-            out = join( iDir(), "test/unitTestOutput" )
-            setLogLevel ( level )
-            run(filename, parameterFile=join ( iDir(), "test/%s" % inifile ),
-                 outputDir= out, db= database, timeout = timeout,
-                 development = development)
-            sfile = join(iDir(),"test/unitTestOutput/%s.py" % basename(filename))
-            return sfile
 
     def testMultipleFiles( self ):
         out = join( iDir(), "test/unitTestOutput")
@@ -109,27 +31,29 @@ class RunSModelSTest(unittest.TestCase):
             if i[-8:]==".smodels":
                 os.unlink( os.path.join ( out, i ))
         dirname = join( iDir(), "inputFiles/slha/" )
-        self.runMain(dirname)
+        runMain(dirname)
         nout = len([i for i in glob.iglob("unitTestOutput/*smodels") if not "~" in i])
         nin = len([i for i in glob.iglob("%s/*slha" % dirname) if not "~" in i])
         if nout != nin:
             logger.error("Number of output file (%d) differ from number of input files (%d)" % (nout, nin))
         self.assertTrue( nout == nin )
-   
+     
     def timeoutRun(self):
         filename = join ( iDir(), "inputFiles/slha/complicated.slha" )
-        outputfile = self.runMain(filename, timeout=1, suppressStdout=True,
-                     development=True, inifile = "timeout.ini" )
+        outputfile = runMain(filename, timeout=1, suppressStdout=True,
+                             development=True, inifile = "timeout.ini" )
    
     def testTimeout(self):
         self.assertRaises(NoTime, self.timeoutRun)
  
     def testGoodFile(self):
         filename = join ( iDir(), "inputFiles/slha/gluino_squarks.slha" )
-        outputfile = self.runMain(filename)
-        shutil.copyfile(outputfile,'./output.py')
+        outputfile = runMain(filename)
+        with open( outputfile, 'rb') as fp: ## imports file with dots in name
+            output_module = imp.load_module("output",fp,outputfile, ('.py', 'rb', imp.PY_SOURCE) )
+            smodelsOutput = output_module.smodelsOutput
+
         from gluino_squarks_default import smodelsOutputDefault
-        from output import smodelsOutput
         ignoreFields = ['input file','smodels version', 'ncpus']
         smodelsOutputDefault['ExptRes'] = sorted(smodelsOutputDefault['ExptRes'],
                     key=lambda res: [res['theory prediction (fb)'],res['TxNames'],
@@ -143,10 +67,12 @@ class RunSModelSTest(unittest.TestCase):
     def testGoodFile13(self):
           
         filename = join ( iDir(), "inputFiles/slha/simplyGluino.slha" )
-        outputfile = self.runMain(filename)
-        shutil.copyfile(outputfile,'./output13.py')
+        outputfile = runMain(filename)
+        with open( outputfile, 'rb') as fp: ## imports file with dots in name
+            output_module = imp.load_module("output",fp,outputfile, ('.py', 'rb', imp.PY_SOURCE) )
+            smodelsOutput = output_module.smodelsOutput
+        
         from simplyGluino_default import smodelsOutputDefault
-        from output13 import smodelsOutput
         ignoreFields = ['input file','smodels version', 'ncpus', 'Element']
         smodelsOutputDefault['ExptRes'] = sorted(smodelsOutputDefault['ExptRes'],
                     key=lambda res: [res['theory prediction (fb)'],res['TxNames'],
@@ -163,10 +89,12 @@ class RunSModelSTest(unittest.TestCase):
         
     def testGoodFileHSCP(self):
         filename = join ( iDir(), "inputFiles/slha/longLived.slha" )
-        outputfile = self.runMain(filename)
-        shutil.copyfile(outputfile,'./outputHSCP.py')
+        outputfile = runMain(filename)
+        with open( outputfile, 'rb') as fp: ## imports file with dots in name
+            output_module = imp.load_module("output",fp,outputfile, ('.py', 'rb', imp.PY_SOURCE) )
+            smodelsOutput = output_module.smodelsOutput
+
         from longLived_default import smodelsOutputDefault
-        from outputHSCP import smodelsOutput
         ignoreFields = ['input file','smodels version', 'ncpus']
         smodelsOutputDefault['ExptRes'] = sorted(smodelsOutputDefault['ExptRes'],
                     key=lambda res: [res['theory prediction (fb)'],res['TxNames'],
@@ -180,10 +108,12 @@ class RunSModelSTest(unittest.TestCase):
   
     def testBadFile(self):
         filename = join (iDir(), "inputFiles/slha/I_dont_exist.slha" )
-        outputfile = self.runMain(filename)
-        shutil.copyfile(outputfile,'./bad_output.py')
+        outputfile = runMain(filename)
+        with open( outputfile, 'rb') as fp: ## imports file with dots in name
+            output_module = imp.load_module("output",fp,outputfile, ('.py', 'rb', imp.PY_SOURCE) )
+            smodelsOutput = output_module.smodelsOutput        
+        
         from bad_default import smodelsOutputDefault
-        from bad_output import smodelsOutput
         ignoreFields = ['input file','smodels version', 'ncpus']
         equals = equalObjs( smodelsOutput,smodelsOutputDefault,allowedDiff=0.,
                             ignore=ignoreFields)
@@ -203,7 +133,7 @@ class RunSModelSTest(unittest.TestCase):
         ctr=0
         crash_file = None
         self.cleanUp()
-        outputfile = self.runMain(filename, timeout=1, suppressStdout=True,
+        outputfile = runMain(filename, timeout=1, suppressStdout=True,
                                    inifile= "timeout.ini" )
         try:
             ## trying to sync!!
@@ -225,7 +155,7 @@ class RunSModelSTest(unittest.TestCase):
                 self.assertEqual(f.readlines(), g.readlines())
         with open("timeout.ini") as f:
             with open(par) as g:
-               self.assertEqual( f.readlines(), g.readlines())
+                self.assertEqual( f.readlines(), g.readlines())
         self.cleanUp()
 
 if __name__ == "__main__":
