473c48dd95e97985664189792e7ab09d3f20f26c
Andre Lessa <lessa.a.p@gmail.com>
2018-08-02 06:44:41 -0300

    Extended theory prediction tests to HSCPs

--- test/testTheoryPrediction.py
+++ test/testTheoryPrediction.py
@@ -15,6 +15,9 @@ import unittest
 from smodels.tools.physicsUnits import fb, GeV
 from databaseLoader import database
 import inspect
+from smodels.theory.theoryPrediction import theoryPredictionsFor
+from smodels.theory import slhaDecomposer
+from smodels.tools.smodelsLogging import setLogLevel
 
 class IntegrationTest(unittest.TestCase):
     def configureLogger(self):
@@ -34,7 +37,6 @@ class IntegrationTest(unittest.TestCase):
 
     def checkAnalysis(self,expresult,smstoplist):
         expID = expresult.globalInfo.id
-        from smodels.theory.theoryPrediction import theoryPredictionsFor
         theorypredictions = theoryPredictionsFor(expresult, smstoplist)
         defpreds=self.predictions()
         if not theorypredictions:
@@ -50,7 +52,6 @@ class IntegrationTest(unittest.TestCase):
 
 
     def testIntegration(self):
-        from smodels.theory import slhaDecomposer
         slhafile = './testFiles/slha/simplyGluino.slha'
         self.configureLogger()
         smstoplist = slhaDecomposer.decompose(slhafile, .1*fb, doCompress=True,
@@ -62,6 +63,42 @@ class IntegrationTest(unittest.TestCase):
             listofanalyses= [ listofanalyses] 
         for analysis in listofanalyses:
             self.checkAnalysis(analysis,smstoplist)
+    
+    def checkPrediction(self,slhafile,expID,expectedValues):
+        self.configureLogger()
+        smstoplist = slhaDecomposer.decompose(slhafile, 0.*fb, doCompress=True,
+                doInvisible=True, minmassgap=5.*GeV)
+        expresults = database.getExpResults(analysisIDs= expID)
+        for expresult in expresults:
+            theorypredictions = theoryPredictionsFor(expresult, smstoplist)
+            for pred in theorypredictions:
+                predval=pred.xsection.value 
+                expval = expectedValues.pop()
+                delta = expval*0.01
+                self.assertAlmostEqual(predval.asNumber(fb), expval,delta=delta)
+        
+        self.assertTrue(len(expectedValues) == 0)
+            
+    def testHSCPPredictions(self):
+        setLogLevel('error')
+        expID = ["CMS-EXO-13-006"]
+        
+        #Test long-lived case:
+        slhafile = './testFiles/slha/hscpTest_long.slha'
+        expValue = [0.0735]
+        self.checkPrediction(slhafile, expID, expValue)
+
+        #Test displaced case:
+        slhafile = './testFiles/slha/hscpTest_mid.slha'
+        expValue = [7.95e-8]
+        self.checkPrediction(slhafile, expID, expValue)
+
+
+        #Test short-lived case:
+        slhafile = './testFiles/slha/hscpTest_short.slha'
+        expValue = [1.75e-10]
+        self.checkPrediction(slhafile, expID, expValue)
+
 
 if __name__ == "__main__":
     unittest.main()
