9715b3dc6bcfdba62f02a462899c7bbda2b0cbf6
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-10-09 19:20:57 +0200

    tiny fix of C++ unit test

--- test/testCpp.py
+++ test/testCpp.py
@@ -11,7 +11,6 @@
 import sys
 sys.path.insert(0,"../")
 from smodels.tools.smodelsLogging import setLogLevel
-setLogLevel ( "error" )
 
 import unittest
 if sys.version[0]=="2":
@@ -20,6 +19,8 @@ else:
     import subprocess as CMD
 
 class CppTest(unittest.TestCase):
+    skip=8 ## skip first lines
+
     def compile(self):
         """ compile the C++ interface """
         cmd = "cd ../cpp; make"
@@ -29,27 +30,37 @@ class CppTest(unittest.TestCase):
 
     def writeOutput(self):
         """ write the default output. will *define* the unit test. """
+        setLogLevel ( "error" )
         f=open("default_cpp.txt","w" )
         cmd = "cd ../cpp; ./run"
         a = CMD.getoutput ( cmd )
-        f.write ( a )
+        b = a.split("\n")
+        for ctr,i in enumerate(b):
+            if "INFO" in i or "WARNING" in i or "smodels.cpp" in i:
+                continue
+            ctr+=1
+            if ctr < self.skip:
+                continue
+            f.write ( i+ "\n" )
         f.close()
 
     def runExample(self):
         """ now run the example """
+        setLogLevel ( "error" )
         cmd = "cd ../cpp; ./run"
         l = CMD.getoutput ( cmd )
-        skip=8 ## skip first lines
-        la = l.split("\n")[skip:] 
+        la = l.split("\n")[self.skip:] 
         a = [ x.strip() for x in la ]
         with open("default_cpp.txt","r" ) as f:
             l = f.readlines()
-            lb=l[skip:]
+            lb=l ## [self.skip:]
             b = [ x.strip() for x in lb ]
         if len(a) != len(b):
             print ( "test failed. writing output to debug.txt" )
             f=open("debug.txt","w")
             for i in b:
+                if "INFO" in i or "WARNING" in i or "smodels.cpp" in i:
+                    continue
                 f.write ( i+ "\n" )
             f.close()
         self.assertEqual ( len(a), len(b) )
@@ -58,7 +69,7 @@ class CppTest(unittest.TestCase):
 
     def testRun(self):
         self.compile()
-        #self.writeOutput()
+        # self.writeOutput()
         self.runExample()
 
 if __name__ == "__main__":
