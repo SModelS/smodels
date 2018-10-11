deaebfb4f6f34a01e200c7b6759298078c74d0d8
Andre Lessa <lessa.a.p@gmail.com>
2018-10-11 08:47:48 -0300

    Small improvement for C++ unit test

--- test/testCpp.py
+++ test/testCpp.py
@@ -11,6 +11,7 @@
 import sys
 sys.path.insert(0,"../")
 from smodels.tools.smodelsLogging import setLogLevel
+setLogLevel ( "error" )
 
 import unittest
 if sys.version[0]=="2":
@@ -19,8 +20,6 @@ else:
     import subprocess as CMD
 
 class CppTest(unittest.TestCase):
-    skip=8 ## skip first lines
-
     def compile(self):
         """ compile the C++ interface """
         cmd = "cd ../cpp; make"
@@ -30,46 +29,54 @@ class CppTest(unittest.TestCase):
 
     def writeOutput(self):
         """ write the default output. will *define* the unit test. """
-        setLogLevel ( "error" )
         f=open("default_cpp.txt","w" )
         cmd = "cd ../cpp; ./run"
-        a = CMD.getoutput ( cmd )
-        b = a.split("\n")
-        for ctr,i in enumerate(b):
-            if "INFO" in i or "WARNING" in i or "smodels.cpp" in i:
-                continue
-            ctr+=1
-            if ctr < self.skip:
-                continue
-            f.write ( i+ "\n" )
+        a = CMD.getoutput(cmd)
+        f.write ( a )
         f.close()
 
     def runExample(self):
         """ now run the example """
-        setLogLevel ( "error" )
         cmd = "cd ../cpp; ./run"
-        l = CMD.getoutput ( cmd )
-        la = l.split("\n")[self.skip:] 
-        a = [ x.strip() for x in la ]
+        l = CMD.getoutput(cmd)
+        l = l[l.find('Input status'):]
+        la = l.split("\n")
+        a = [ x.strip() for x in la  if x.strip() and x.strip()[0] != '#' 
+             and not 'WARNING' in x and not 'INFO' in x]        
         with open("default_cpp.txt","r" ) as f:
-            l = f.readlines()
-            lb=l ## [self.skip:]
-            b = [ x.strip() for x in lb ]
+            l = f.read()
+            l = l[l.find('Input status'):]            
+            lb = l.split("\n")
+            b = [ x.strip() for x in lb  if x.strip() and x.strip()[0] != '#' 
+                 and not 'WARNING' in x and not 'INFO' in x]
         if len(a) != len(b):
             print ( "test failed. writing output to debug.txt" )
             f=open("debug.txt","w")
             for i in b:
-                if "INFO" in i or "WARNING" in i or "smodels.cpp" in i:
-                    continue
                 f.write ( i+ "\n" )
             f.close()
-        self.assertEqual ( len(a), len(b) )
+        self.assertEqual(len(a), len(b))
         for x,y in zip(a,b):
-            self.assertEqual ( x, y )
+            if x == y:
+                continue
+            xvals = [v.strip() for v in x.split()]
+            yvals = [v.strip() for v in y.split()]
+            self.assertEqual(len(xvals), len(yvals), "Lines:\n %s and \n %s \n differ" %(x,y))
+            for i,xv in enumerate(xvals):
+                yv = yvals[i]
+                try:
+                    yv = eval(yv)
+                    xv = eval(xv)
+                except:
+                    pass
+                if isinstance(yv,float) and isinstance(xv,float):
+                    self.assertAlmostEqual(xv, yv, 5)
+                else:
+                    self.assertEqual(xv, yv)
 
     def testRun(self):
         self.compile()
-        # self.writeOutput()
+        #self.writeOutput()
         self.runExample()
 
 if __name__ == "__main__":
