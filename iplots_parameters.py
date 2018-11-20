96fbf330ffd3b996783710256ba2ea9da25efafa
Humberto Reyes <humberto.reyes-gonzalez@lpsc.in2p3.fr>
2018-11-20 10:56:24 +0100

    Interactive plot changes 20/02/18

--- iplots_parameters.py
+++ iplots_parameters.py
@@ -4,11 +4,11 @@ plot_title = 'Mi interative plot example'
 
 #Label, block and code number for the variables you want to plot, e.g. 'm_gluino': ['MASS', 1000021]. These will be your x and y axes.
 
-variable_x = {'m_gluino': ['MASS', 1000021]}
+variable_x = {'m<sub>gluino</sub>': ['MASS', 1000021]}
 variable_y = {'m_suR': ['MASS', 2000002]}
 
 #SLHA hover information: In a dictionary form, give the name* of your variable, the block and code number to find it in the SLHA file.
-slha_hover_information = {'m(gluino)': ['MASS', 1000021], 'm(suR)': ['MASS', 2000002], 'm_LSP': ['MASS', 1000022]} 
+slha_hover_information = {'m_gluino': ['MASS', 1000021], 'm_suR': ['MASS', 2000002], 'm_LSP': ['MASS', 1000022]} 
 
 #For which particles you want to get the mean decay length.
 ctau_hover_information = {'ctau_gluino': 1000021}
@@ -24,7 +24,7 @@ BR_get_top = 5
 #
 #SModelS_status -> prints whether the point is excluded or not by SModelS.
 #r_max -> shows the highest r-value for each parameter point.
-#chi2 -> shows the chi^2 value, if available (if not, the output is 'False').
+#chi2 -> shows the chi^2 value which corresponds to r_max, if available (if not, the output is 'False').
 #Tx -> shows the topology/ies which give r_max
 #Analysis -> shows the experimental analysis from which the strongest constraint (r_max) comes from.
 #MT_max -> shows the missing topology with the largest cross section (in SModelS bracket notation).
@@ -41,4 +41,4 @@ SModelS_hover_information = ['SModelS_status', 'r_max', 'Tx', 'Analysis','chi2',
 # choice for plot data: all, non-excluded, excluded points.
 plot_data = ['all', 'non-excluded', 'excluded'] 
 # choice for plot list: same options as for SModels hover information.
-plot_list = ['SModelS_status','r_max', 'chi2', 'Tx', 'Analysis', 'MT_max', 'MT_max_xsec', 'MT_total_xsec', 'MT_long_xsec', 'MT_asym_xsec']
+plot_list = ['SModelS_status','r_max', 'chi2', 'Tx', 'Analysis', 'MT_max', 'MT_max_xsec', 'MT_total_xsec', 'MT_long_xsec', 'MT_asym_xsec','MT_outgrid_xsec']
--- test/iplots_parameters.py
+++ test/iplots_parameters.py
@@ -4,8 +4,8 @@ plot_title = 'My interactive plot example'
 
 #Label, block and code number for the variables you want to plot, e.g. 'm_gluino': ['MASS', 1000021]. These will be your x and y axes.
 
-variable_x = {'m_gluino[GeV]': ['MASS', 1000021]}
-variable_y = {'m_suR[GeV]': ['MASS', 2000002]}
+variable_x = {'m_gluino': ['MASS', 1000021]}
+variable_y = {'m_suR': ['MASS', 2000002]}
 
 #SLHA hover information: In a dictionary form, give the name* of your variable, the block and code number to find it in the SLHA file.
 slha_hover_information = {'m_gluino': ['MASS', 1000021], 'm_suR': ['MASS', 2000002], 'm_LSP': ['MASS', 1000022]} 
@@ -21,9 +21,9 @@ BR_get_top = 'all'
 
 #SModelS hover information; options are:
 #
-#SmodelS_excluded -> prints whether the point is excluded or not by SModelS
+#SmodelS_status -> prints whether the point is excluded or not by SModelS
 #r_max -> shows the highest r-value for each parameter point
-#chi2 -> shows the chi^2 value, if available (if not, the output is 'none')
+#chi2 -> shows the chi^2 value which corresponds to r_max, if available (if not, the output is 'none')
 #Tx -> shows the topology/ies which give r_max
 #Analysis -> shows the experimental analysis from which the strongest constraint (r_max) comes from
 #MT_max -> shows the missing topology with the largest cross section (in SModelS bracket notation)
@@ -34,13 +34,13 @@ BR_get_top = 'all'
 #MT_outgrid_xsec -> shows the total missing cross section outside the mass grids of the experimental results
 #file -> shows the name of the input spectrum file 
 
-smodels_hover_information = ['SmodelS_excluded', 'r_max', 'chi2', 'Tx', 'Analysis', 'MT_max', 'MT_max_xsec', 'MT_total_xsec', 'MT_long_xsec', 'MT_asym_xsec', 'MT_outgrid_xsec', 'file']
+SModelS_hover_information = ['SModelS_status', 'r_max', 'chi2', 'Tx', 'Analysis', 'MT_max', 'MT_max_xsec', 'MT_total_xsec', 'MT_long_xsec', 'MT_asym_xsec', 'MT_outgrid_xsec', 'file']
 
 #Set which plots do you want
 # choice for plot data: all, non-excluded, excluded points
 plot_data = ['all', 'non-excluded', 'excluded'] 
 # choice for plot list: same options as for SModels hover information
-plot_list = ['SmodelS_excluded','chi2','r_max','Tx','Analysis','MT_asym_xsec','MT_total_xsec','MT_outgrid_xsec','MT_max']
+plot_list = ['SModelS_status','chi2','r_max','Tx','Analysis','MT_asym_xsec','MT_total_xsec','MT_outgrid_xsec','MT_max']
 
 
 
