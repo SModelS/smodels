230a7a8d90c2fccfc98f8e0ad86c37eaf403d835
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-02-04 14:25:53 +0100

    updated test

--- test/testElementID.py
+++ test/testElementID.py
@@ -2,7 +2,7 @@
 
 """
 .. module:: testElementID
-   :synopsis: Tests the slha checker
+   :synopsis: ??
 
 .. moduleauthor:: Ursula Laa <Ursula.Laa@assoc.oeaw.ac.at>
 
@@ -14,8 +14,8 @@ from smodels.theory import decomposer
 from smodels.tools.physicsUnits import GeV,fb
 from databaseLoader import database
 from smodels.theory.theoryPrediction import theoryPredictionsFor
-from smodels.share.models.mssm import BSMList
 from smodels.share.models.SMparticles import SMList
+from smodels.share.models.mssm import BSMList
 from smodels.theory.model import Model
 
 class ElementIdTest(unittest.TestCase):
@@ -26,16 +26,17 @@ class ElementIdTest(unittest.TestCase):
         filename = "./testFiles/slha/higgsinoStop.slha"
         model = Model(BSMList,SMList)
         model.updateParticles(filename,promptWidth = 1e-12*GeV) #Make sure C1 and N2 are treated as prompt
+        ## model.describe()
 
         topoList = decomposer.decompose(model, sigcut= 0.1*fb, doCompress=True, doInvisible=True, minmassgap= 5*GeV)
         resultlist = database.getExpResults(analysisIDs=['*:8*TeV','CMS-PAS-SUS-15-002','CMS-PAS-SUS-16-024'])
         
         for res in resultlist:
+            if 'EXO' in res.globalInfo.id: continue #skip EXO searches
             theorypredictions = theoryPredictionsFor(res, topoList)
             if not theorypredictions: continue
             self.assertEqual(len(theorypredictions),1)
             tpIDs = theorypredictions[0].IDs 
-            if 'EXO' in res.globalInfo.id: continue #skip EXO searches
             self.assertEqual(sorted(tpIDs),sorted(listOfIDs[res.globalInfo.id]))            
 
 if __name__ == "__main__":
