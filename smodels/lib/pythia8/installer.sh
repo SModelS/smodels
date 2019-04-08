3495210ff9b788ca3153662d1008ad6196b68b29
Wolfgang Waltenberger <wwaltenberger@hepgpu01.hephy.oeaw.ac.at>
2019-04-08 15:54:01 +0000

    multicore compilation

--- smodels/lib/pythia8/installer.sh
+++ smodels/lib/pythia8/installer.sh
@@ -4,6 +4,9 @@ VER='8226'
 [ -z "$1" ] && { echo "pythia8 version is not given, use pythia$VER"; } || 
 		{ VER="$1"; echo "set to version pythia$VER"; }
 
+NCORES="4"
+command -v nproc && NCORES=`nproc`
+
 TARBALL="pythia$VER.tgz"
 URL=http://home.thep.lu.se/~torbjorn/pythia8/$TARBALL
 #TARBALL="pythia${VER}_fixed.tgz"
@@ -13,4 +16,4 @@ test -e .$TARBALL && { echo "[installer] tarball $TARBALL exists"; } || { echo "
 
 test -e pythia$VER && { echo "[installer] pythia$VER directory exists"; } || { echo "[installer] $TARBALL"; tar xvf .$TARBALL; };
 
-test -e pythia$VER/lib/libpythia8.a && { echo "[installer] libpythia8.a exists"; } || { echo "[installer] building pythia8"; cd pythia$VER; ./configure; make; };
+test -e pythia$VER/lib/libpythia8.a && { echo "[installer] libpythia8.a exists"; } || { echo "[installer] building pythia8"; cd pythia$VER; ./configure; make -j $NCORES; };
