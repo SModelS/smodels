49c6af21ade6b71e75946a89ded747a1cb27192c
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-08-20 18:38:28 +0200

    imp -> importlib

--- test/testCombined.py
+++ test/testCombined.py
@@ -9,7 +9,8 @@
  
 """
  
-import sys,os,imp
+import sys,os
+import importlib
 sys.path.insert(0,"../")
 import unittest
 from unitTestHelpers import equalObjs, runMain
@@ -22,9 +23,12 @@ class CombinedTest(unittest.TestCase):
         Use with care! """
         filename = "./testFiles/slha/gluino_squarks.slha"
         outputfile = runMain(filename,inifile="testParameters_agg.ini", suppressStdout=True )
-        with open( outputfile, 'rb') as fp: ## imports file with dots in name
-            output_module = imp.load_module("output",fp,outputfile, 
-                                            ('.py', 'rb', imp.PY_SOURCE) )
+        #with open( outputfile, 'rb') as fp: ## imports file with dots in name
+        #    output_module = imp.load_module("output",fp,outputfile, 
+        #                                    ('.py', 'rb', imp.PY_SOURCE) )
+        spec = importlib.util.spec_from_file_location( "output", outputfile)
+        output_module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(output_module)
         smodelsOutput = output_module.smodelsOutput
         f=open("gluino_squarks_default_agg.py","w")
         f.write ( "smodelsOutputDefault = %s\n" % smodelsOutput )
@@ -33,19 +37,21 @@ class CombinedTest(unittest.TestCase):
     def testCombinedResult(self):
         filename = "./testFiles/slha/gluino_squarks.slha"
         outputfile = runMain(filename,inifile="testParameters_agg.ini", suppressStdout=True )
-        with open( outputfile, 'rb') as fp: ## imports file with dots in name
-            output_module = imp.load_module("output",fp,outputfile, 
-                                            ('.py', 'rb', imp.PY_SOURCE) )
-            smodelsOutput = output_module.smodelsOutput
-            from gluino_squarks_default_agg import smodelsOutputDefault
-            ignoreFields = ['input file','smodels version', 'ncpus', 'database version']
-            smodelsOutputDefault['ExptRes'] = sorted(smodelsOutputDefault['ExptRes'],
-                        key=lambda res: res['r'], reverse=True)
-            equals = equalObjs(smodelsOutput,smodelsOutputDefault,allowedDiff=0.02,
-                               ignore=ignoreFields)
-            if equals != True:
-                logger.error ( "%s differs from %s!" % ( "gluino_squarks_default_agg.py", outputfile) ) 
-            self.assertTrue(equals)
+        # output_module = imp.load_module("output",fp,outputfile, 
+        #                                ('.py', 'rb', imp.PY_SOURCE) )
+        spec = importlib.util.spec_from_file_location( "output", outputfile)
+        output_module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(output_module)
+        smodelsOutput = output_module.smodelsOutput
+        from gluino_squarks_default_agg import smodelsOutputDefault
+        ignoreFields = ['input file','smodels version', 'ncpus', 'database version']
+        smodelsOutputDefault['ExptRes'] = sorted(smodelsOutputDefault['ExptRes'],
+                    key=lambda res: res['r'], reverse=True)
+        equals = equalObjs(smodelsOutput,smodelsOutputDefault,allowedDiff=0.02,
+                           ignore=ignoreFields)
+        if equals != True:
+            logger.error ( "%s differs from %s!" % ( "gluino_squarks_default_agg.py", outputfile) ) 
+        self.assertTrue(equals)
         for i in [ outputfile, outputfile.replace(".py",".pyc") ]:
             if os.path.exists ( i ):
                 os.remove ( i )
