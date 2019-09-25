e7ec4e832bc78c73c5dfcb2fdc2b9d23c3b60abd
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-09-25 08:56:38 +0200

    updated C++ section of docs

--- docs/manual/source/Cpp.rst
+++ docs/manual/source/Cpp.rst
@@ -7,11 +7,12 @@ C++ Interface
 
 
 Since v1.1.1, a simple C++ interface is provided, see
-the smodels/cpp directory in the source code.
+the *smodels/cpp* directory in the source code.
 
 Its usage is documented in run.cpp:
 
 .. literalinclude:: /examples/run.cpp
 
-A sample Makefile is also provided. The python header 
-files have to be installed.
+A sample Makefile is also provided in the same directory. The python header
+files have to be installed; on debian-based systems the package name is
+*libpython3-dev*, on rpm-based systems it is usually called *python3-devel*.
--- docs/manual/source/examples/run.cpp
+++ docs/manual/source/examples/run.cpp
@@ -8,9 +8,13 @@ using namespace std;
 
 int main( int argc, char * argv[] )
 {
-  /** initialise SModelS, load database, second argument (installdir) must point to smodels 
-   *  installation top-level directory */
+  /** initialise SModelS, load database. The first argument 
+   *  points to the SModelS config file, 
+   *  the second argument (``smodelsdir'') must point to the 
+   *  top-level directory of the SModelS installation */
   SModelS smodels ( "./parameters.ini", "../" ); 
   /** run over one single file or directory */
   int ret = smodels.run ( "test.slha" );
+  /* return value, zero if succesful */
+  return ret;
 }
