b36069c48beebfa17ab079491ef67dfc1db3f821
walten <wolfgang.waltenberger@gmail.com>
2020-06-05 17:46:04 +0200

    test run smodels importModule doesnt exist

--- test/testRunSModelS.py
+++ test/testRunSModelS.py
@@ -68,7 +68,10 @@ class RunSModelSTest(unittest.TestCase):
         filename = "./testFiles/slha/T6bbHH_pyhf.slha"
         inifile = "./testParameters_pyhf.ini"
         outputfile = runMain(filename, inifile=inifile)
-        smodelsOutput = importModule(outputfile)
+        # smodelsOutput = importModule(outputfile)
+        with open( outputfile, 'rb') as fp: ## imports file with dots in name
+            output_module = imp.load_module("output",fp,outputfile, ('.py', 'rb', imp.PY_SOURCE) )
+            smodelsOutput = output_module.smodelsOutput
         from pyhf_default import smodelsOutputDefault
         ignoreFields = ['input file','smodels version', 'ncpus', 'database version']
         equals = equalObjs(smodelsOutput,smodelsOutputDefault,allowedDiff=0.02,
