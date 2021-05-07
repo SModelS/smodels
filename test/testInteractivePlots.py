6025078fb328f14f720ecb52fd038789cb911a1c
walten <wolfgang.waltenberger@gmail.com>
2021-05-07 13:39:00 +0200

    cleanup after test

--- test/testInteractivePlots.py
+++ test/testInteractivePlots.py
@@ -48,10 +48,11 @@ class RunInteractivePlotSTest(unittest.TestCase):
         self.assertEqual(run,outFolder)        
         self.assertEqual(sorted(os.listdir(outFolder)), sorted(os.listdir(defaultFolder)))
 
-#         if os.path.isdir(outFolder):
-#             shutil.rmtree(outFolder)
-#         if os.path.exists("all_data_frame.txt"):
-#             os.unlink("all_data_frame.txt")
+        if os.path.isdir(outFolder):
+            shutil.rmtree(outFolder)
+
+        if os.path.exists ( "all_data_frame.txt" ):
+            os.unlink( "all_data_frame.txt" )
 
 
     # this test corresponds to calling
@@ -82,6 +83,11 @@ class RunInteractivePlotSTest(unittest.TestCase):
         self.assertEqual(run,outFolder)
         self.assertEqual(sorted(os.listdir(outFolder)), sorted(os.listdir(defaultFolder)))
 
+        if os.path.isdir(outFolder):
+            shutil.rmtree(outFolder)
+
+        if os.path.exists ( "all_data_frame.txt" ):
+            os.unlink ( "all_data_frame.txt" )
 
 if __name__ == "__main__":
     unittest.main()
