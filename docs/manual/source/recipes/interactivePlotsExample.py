00f543b480ff33eea7456949f303036ca35d42a7
walten <wolfgang.waltenberger@gmail.com>
2021-05-09 13:00:31 +0200

    cleanup

--- docs/manual/source/recipes/interactivePlotsExample.py
+++ docs/manual/source/recipes/interactivePlotsExample.py
@@ -8,14 +8,11 @@
 
 # Set up the path to SModelS installation folder
 import sys,os
-
 sys.path.append(".")
 
 import smodels_paths
-from smodels.tools.interactivePlots import PlotMaster
+from smodels.tools.interactivePlots import Plotter
 from smodels.installation import installDirectory
-import smodels
-from types import SimpleNamespace
 
 
 # ### Define the input folders, the parameters file and where to store the plots
@@ -23,11 +20,11 @@ from types import SimpleNamespace
 # In[2]:
 
 
-smodelsOutput = f'{installDirectory()}/inputFiles/scanExample/smodels-output.tar.gz'
-slhaFiles = f'{installDirectory()}/inputFiles/scanExample/slhas.tar.gz'
+smodelsOutput = './inputFiles/scanExample/smodels-output.tar.gz'
+slhaFiles = './inputFiles/scanExample/slhas.tar.gz'
 parameters = './iplots_parameters.py'
 indexfile = 'main.html'
-modelfile='{installDirectory()}/smodels/share/models/mssm.py'
+modelfile=f'{installDirectory()}/smodels/share/models/mssm.py'
 outputFolder = './'
 npoints = -1 #If negative will use all the points in the folders
 
@@ -37,9 +34,9 @@ npoints = -1 #If negative will use all the points in the folders
 # In[3]:
 
 
-plotMaster = PlotMaster(smodelsOutput,slhaFiles,parameters,indexfile)
-loadData = plotMaster.loadData(npoints)
-plotMaster.makePlots(outputFolder)  
+plotter = Plotter(smodelsOutput,slhaFiles,parameters,modelfile)
+plotter.loadData(npoints)
+plotter.plot(outputFolder,indexfile)
 
 
 # ### Display the output
@@ -47,14 +44,5 @@ plotMaster.makePlots(outputFolder)
 # In[4]:
 
 
-from IPython.core.display import display,HTML
-
-
-# In[5]:
-
-
-#Main page (to open the plots go to the plots output folder and open them with the browser)
-mainFile = open(outputFolder + indexfile,'r')
-display(HTML(mainFile.read()))
-mainFile.close()
+plotter.display()
 
