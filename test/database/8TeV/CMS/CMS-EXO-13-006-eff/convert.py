db765bdd9dbed545ade5d73f661fd9b46a685152
Andre Lessa <lessa.a.p@gmail.com>
2022-05-22 20:52:32 +0200

    Fixing test database

--- test/database/8TeV/CMS/CMS-EXO-13-006-eff/convert.py
+++ test/database/8TeV/CMS/CMS-EXO-13-006-eff/convert.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 .. module:: convert
@@ -10,31 +10,37 @@ import os
 import argparse
 import types
 
-argparser = argparse.ArgumentParser(description =  
-'create info.txt, txname.txt, twiki.txt and sms.py')
-argparser.add_argument ('-utilsPath', '--utilsPath', 
-help = 'path to the package smodels_utils',\
-type = types.StringType)
-argparser.add_argument ('-smodelsPath', '--smodelsPath', 
-help = 'path to the package smodels_utils',\
-type = types.StringType)
+argparser = argparse.ArgumentParser(description =
+    'create info.txt, txname.txt, twiki.txt and sms.py')
+argparser.add_argument ('-utilsPath', '--utilsPath',
+    help = 'path to the package smodels_utils',\
+    type = str, default = '~/smodels-utils' )
+argparser.add_argument ('-smodelsPath', '--smodelsPath',
+    help = 'path to the package smodels_utils',\
+    type = str, default = '~/smodels' )
+argparser.add_argument ('-no', '--noUpdate',
+    help = 'do not update the lastUpdate field.',\
+    action= "store_true" )
+argparser.add_argument ('-r', '--resetValidation',
+    help = 'reset the validation flag',\
+    action= "store_true" )
+
 args = argparser.parse_args()
 
-if args.utilsPath:
-    utilsPath = args.utilsPath
-else:
-    databaseRoot = '../../../'
-    sys.path.append(os.path.abspath(databaseRoot))
-    from utilsPath import utilsPath
-    utilsPath = databaseRoot + utilsPath
-if args.smodelsPath:
-    sys.path.append(os.path.abspath(args.smodelsPath))
+if args.noUpdate:
+    os.environ["SMODELS_NOUPDATE"]="1"
 
-sys.path.append(os.path.abspath(utilsPath))
+if args.resetValidation:
+    os.environ["SMODELS_RESETVALIDATION"]="1"
+
+utilsPath = args.utilsPath
+sys.path.append(os.path.abspath(os.path.expanduser(utilsPath)))
+sys.path.append(os.path.abspath(os.path.expanduser(args.smodelsPath)))
+
+
 from smodels_utils.dataPreparation.inputObjects import MetaInfoInput,DataSetInput
 from smodels_utils.dataPreparation.databaseCreation import databaseCreator
-from smodels_utils.dataPreparation.massPlaneObjects import x, y, z
-
+from smodels_utils.dataPreparation.massPlaneObjects import x, y, z, w
 
 #+++++++ global info block ++++++++++++++
 info = MetaInfoInput('CMS-EXO-13-006')
@@ -44,130 +50,139 @@ info.lumi = 18.8
 info.prettyName ='hscp search'
 info.private = False
 info.arxiv = 'https://arxiv.org/abs/1502.02522'
-info.contact ='Andre Lessa <lessa.a.p@gmail.com>; Jan Heisig <heisig@physik.rwth-aachen.de>; SModelS'
+info.contact ='SModelS'
+info.Leff_inner = 1e-3
+info.Leff_outer = 7.0
 info.publication ='https://cds.cern.ch/record/1987723/files/arXiv:1502.02522.pdf'
-info.comment ='Search for long-lived charged particles implemented in arXiv:1509.00473. For the topologies with mixed MET-HSCP branches, the MET branch is irrelevant and wildcards are used.'
+info.comment ='Search for long-lived charged particles implemented in arXiv:1509.00473. For the topologies with mixed MET-HSCP branches, the MET branch is irrelevant and wildcards are used. The topologies THSCPM1b and THSCPM2b refer to staus, while in 1509.00473 charginos were used. All topologies do not include intermediate BSM states in the production process. The THSCPM5, THSCPM6 and THSCPM7 topologies do not include width information'
 info.supersedes =''
+particlesFile = os.path.abspath('../../../databaseParticles.py')
 
 #++Define list of datasets++
 datasetNames = ['c000','c100','c200','c300']
 observedNs = [42,7,0,0]
-expectedBGs = [44.,5.6,0.56,0.02]
-bgErrors = [9.,1.1,0.11,0.004]
-obsUpperLimits = ['1.15*fb','0.441*fb','0.16*fb','0.159*fb']
-expUpperLimits = ['1.23*fb','0.338*fb','0.16*fb','0.159*fb']
-for i,name in enumerate(datasetNames):
+expectedBGs = [44.,5.6,0.56,0.09]
+bgErrors = [9.,1.1,0.11,0.02]
+#SR Upper limits from arxiv:1502.02522 (Table 2). Note that the SR UL are given by obs_UL*eff (from the table)
+obsUpperLimits = ['1.02*fb','0.45*fb','0.17*fb','0.16*fb']
+#SR Expected Upper limits from arxiv:1502.02522 (Table 2). Note that the SR UL are given by exp_UL*eff (from the table)
+expUpperLimits = ['1.09*fb','0.37*fb','0.22*fb','0.17*fb']
+
+
+
+# Inclusive topologies. We include all possible 1-step decays: X -> SM + MET, X -> SM,SM + MET,X -> SM,SM,SM + MET 
+# and all possible 2-tep decays: Y -> SM + X, Y -> SM,SM + X, Y -> SM,SM,SM + X,  with X -> SM + MET, X -> SM,SM + MET,X -> SM,SM,SM + MET 
+THSCPM2b_constList = ["[[['*']],[]]", 
+                      "[[['*','*']],[]]", 
+                      "[[['*','*', '*']],[]]", 
+                      "[[['*'],['*']],[]]", 
+                      "[[['*'],['*','*']],[]]", 
+                      "[[['*'],['*','*', '*']],[]]", 
+                      "[[['*','*'],['*']],[]]", 
+                      "[[['*','*'],['*','*']],[]]", 
+                      "[[['*','*'],['*','*', '*']],[]]", 
+                      "[[['*','*', '*'],['*']],[]]", 
+                      "[[['*','*', '*'],['*','*']],[]]", 
+                      "[[['*','*', '*'],['*','*', '*']],[]]"]
+
+
+THSCPM4_constList =  ["[[['*']],[['*']]]", 
+                      "[[['*','*']],[['*']]]", 
+                      "[[['*','*', '*']],[['*']]]", 
+                      "[[['*'],['*']],[['*']]]", 
+                      "[[['*'],['*','*']],[['*']]]", 
+                      "[[['*'],['*','*', '*']],[['*']]]", 
+                      "[[['*','*'],['*']],[['*']]]", 
+                      "[[['*','*'],['*','*']],[['*']]]", 
+                      "[[['*','*'],['*','*', '*']],[['*']]]", 
+                      "[[['*','*', '*'],['*']],[['*']]]", 
+                      "[[['*','*', '*'],['*','*']],[['*']]]", 
+                      "[[['*','*', '*'],['*','*', '*']],[['*']]]"]
+
+
+THSCPM6_constList =  ["[[['*']],[['*'],[ '*']]]", 
+                      "[[['*','*']],[['*'],[ '*']]]", 
+                      "[[['*','*', '*']],[['*'],[ '*']]]", 
+                      "[[['*'],['*']],[['*'],[ '*']]]", 
+                      "[[['*'],['*','*']],[['*'],[ '*']]]", 
+                      "[[['*'],['*','*', '*']],[['*'],[ '*']]]", 
+                      "[[['*','*'],['*']],[['*'],[ '*']]]", 
+                      "[[['*','*'],['*','*']],[['*'],[ '*']]]", 
+                      "[[['*','*'],['*','*', '*']],[['*'],[ '*']]]", 
+                      "[[['*','*', '*'],['*']],[['*'],[ '*']]]", 
+                      "[[['*','*', '*'],['*','*']],[['*'],[ '*']]]", 
+                      "[[['*','*', '*'],['*','*', '*']],[['*'],[ '*']]]"]
+                      
+                      
+THSCPM9_constList =   ["[[['*']],[['*','*']]]", 
+                      "[[['*','*']],[['*','*']]]", 
+                      "[[['*','*', '*']],[['*','*']]]", 
+                      "[[['*'],['*']],[['*','*']]]", 
+                      "[[['*'],['*','*']],[['*','*']]]", 
+                      "[[['*'],['*','*', '*']],[['*','*']]]", 
+                      "[[['*','*'],['*']],[['*','*']]]", 
+                      "[[['*','*'],['*','*']],[['*','*']]]", 
+                      "[[['*','*'],['*','*', '*']],[['*','*']]]", 
+                      "[[['*','*', '*'],['*']],[['*','*']]]", 
+                      "[[['*','*', '*'],['*','*']],[['*','*']]]", 
+                      "[[['*','*', '*'],['*','*', '*']],[['*','*']]]"]
+
+
+#Txname data:
+txnames = {
+    'THSCPM1b' : {'constraint' : "[[],[]]", 'finalState' : ['HSCP','HSCP'], 'massPlane' : [[(x,y)]]*2,
+                        'coordinates' : {x: 0, y:1, 'value' : 2}},
+    'THSCPM2b' : {'constraint' : "+".join(THSCPM2b_constList), 'finalState' : ['MET','HSCP'], 'massPlane' : [['*'],[(x,y)]],
+                        'coordinates' : {x: 0, y:1, 'value' : 2}},
+    'THSCPM3' : {'constraint' : "[[['*']],[['*']]]", 'finalState' : ['HSCP','HSCP'], 'massPlane' : [[x,(y,w)]]*2,
+                        'coordinates' : {x: 0, y:1, w: 2, 'value' : 3}},
+    'THSCPM4' : {'constraint' : "+".join(THSCPM4_constList), 'finalState' : ['MET','HSCP'], 'massPlane' : [['*'],[x,(y,w)]],
+                        'coordinates' : {x: 0, y:1, w: 2, 'value' : 3}},
+    'THSCPM5' : {'constraint' :"[[['*'],['*']],[['*'],['*']]]", 'finalState' : ['HSCP','HSCP'], 'massPlane' : [[x,y,z]]*2,
+                        'coordinates' : {x: 0, y:1, z: 2, 'value' : 4}},
+    'THSCPM6' : {'constraint' : "+".join(THSCPM6_constList), 'finalState' : ['MET','HSCP'], 'massPlane' : [['*'],[x,y,z]],
+                        'coordinates' : {x: 0, y:1, z: 2, 'value' : 4}},
+   'THSCPM7' : {'constraint' :  "[[['*'],['*']],[['*']]]", 'finalState' : ['HSCP','HSCP'], 'massPlane' : [[x,y,z],[x,z]],
+                        'coordinates' : {x: 0, y:1, z: 2, 'value' : 4}},
+    'THSCPM8' : {'constraint' : "[[['*','*']],[['*','*']]]", 'finalState' : ['HSCP','HSCP'], 'massPlane' : [[x,(y,w)]]*2,
+                        'coordinates' : {x: 0, y:1, w: 2, 'value' : 3}},
+   'THSCPM9' : {'constraint' : "+".join(THSCPM9_constList), 'finalState' : ['MET','HSCP'], 'massPlane' : [['*'],[x,(y,w)]],
+                        'coordinates' : {x: 0, y:1, w: 2, 'value' : 3}}
+}
+
+
+for idataset,name in enumerate(datasetNames):
 #+++++++ dataset block ++++++++++++++
     dataset = DataSetInput(name)
-    dataset.setInfo(dataType = 'efficiencyMap', dataId = name, 
-                    observedN=observedNs[i], expectedBG=expectedBGs[i], bgError=bgErrors[i],
-                    upperLimit = obsUpperLimits[i], expectedUpperLimit = expUpperLimits[i])
+    dataset.setInfo(dataType = 'efficiencyMap', dataId = name,
+            observedN=observedNs[idataset], expectedBG=expectedBGs[idataset], bgError=bgErrors[idataset],
+            upperLimit = obsUpperLimits[idataset], expectedUpperLimit = expUpperLimits[idataset])
 
     #+++++++ txnames ++++++++++++++++++++
-    #+++++++ next txName block ++++++++++++++
-    HSCPM1 = dataset.addTxName('THSCPM1')
-    HSCPM1.checked =''
-    HSCPM1.constraint = "[[],[]]"
-    HSCPM1.condition =None
-    HSCPM1.finalState = ['HSCP','HSCP']
-    HSCPM1.massConstraints = None
-    HSCPM1.dataUrl = None
-    HSCPM1.source = 'SModelS'
-    #+++++++ next mass plane block ++++++++++++++
-    plane = HSCPM1.addMassPlane([[x],[x]])
-    plane.setSources(dataLabels= ['efficiencyMap'],dataFiles=['orig/eff_HSCPM1_'+name+'.txt'], dataFormats=['txt'])
-    #+++++++ next txName block ++++++++++++++
-    HSCPM3 = dataset.addTxName('THSCPM3')
-    HSCPM3.checked =''
-    HSCPM3.constraint = "[[['*']],[['*']]]"  ##Here '*' represents any (single) even particle
-    HSCPM3.condition =None
-    HSCPM3.conditionDescription =None
-    HSCPM3.finalState = ['HSCP','HSCP']
-    HSCPM3.massConstraints = None
-    HSCPM3.dataUrl = None
-    HSCPM3.source = 'SModelS'
-    #+++++++ next mass plane block ++++++++++++++
-    plane = HSCPM3.addMassPlane([[x,y],[x,y]])
-    plane.setSources(dataLabels= ['efficiencyMap'],dataFiles=['orig/eff_HSCPM3_'+name+'.txt'], dataFormats=['txt'])
-    #+++++++ next txName block ++++++++++++++
-    HSCPM5 = dataset.addTxName('THSCPM5')
-    HSCPM5.checked =''
-    HSCPM5.constraint = "[[['*'],['*']],[['*'],['*']]]" ##Here '*' represents any (single) even particle
-    HSCPM5.condition =None
-    HSCPM5.conditionDescription =None
-    HSCPM5.finalState = ['HSCP','HSCP']
-    HSCPM5.massConstraints = None
-    HSCPM5.dataUrl = None
-    HSCPM5.source = 'SModelS'
-    #+++++++ next mass plane block ++++++++++++++
-    plane = HSCPM5.addMassPlane([[x,y,z]]*2)
-    plane.setSources(dataLabels= ['efficiencyMap'],dataFiles=['orig/eff_HSCPM5_'+name+'.txt'], dataFormats=['txt'])
-    #+++++++ next txName block ++++++++++++++
-    HSCPM7 = dataset.addTxName('THSCPM7')
-    HSCPM7.checked =''
-    HSCPM7.constraint = "[[['*']],[['*'],['*']]]" ##Here '*' represents any (single) even particle
-    HSCPM7.condition =None
-    HSCPM7.conditionDescription =None
-    HSCPM7.finalState = ['HSCP','HSCP']
-    HSCPM7.massConstraints = None
-    HSCPM7.dataUrl = None
-    HSCPM7.source = 'SModelS'
-    #+++++++ next mass plane block ++++++++++++++
-    plane = HSCPM7.addMassPlane([[x,z],[x,y,z]])
-    plane.setSources(dataLabels= ['efficiencyMap'],dataFiles=['orig/eff_HSCPM7_'+name+'.txt'], dataFormats=['txt'])
-    #+++++++ next txName block ++++++++++++++
-    HSCPM8 = dataset.addTxName('THSCPM8')
-    HSCPM8.checked =''
-    HSCPM8.constraint = "[[['*','*']],[['*','*']]]" ##Here '*','*' represents any pair of even particles
-    HSCPM8.condition =None
-    HSCPM8.conditionDescription =None
-    HSCPM8.finalState = ['HSCP','HSCP']
-    HSCPM8.massConstraints = None
-    HSCPM8.dataUrl = None
-    HSCPM8.source = 'SModelS'
-    #+++++++ next mass plane block ++++++++++++++
-    plane = HSCPM8.addMassPlane([[x,y],[x,y]])
-    plane.setSources(dataLabels= ['efficiencyMap'],dataFiles=['orig/eff_HSCPM8_'+name+'.txt'], dataFormats=['txt'])
-    #+++++++ next txName block ++++++++++++++
-    HSCPM2 = dataset.addTxName('THSCPM2')
-    HSCPM2.checked =''
-    HSCPM2.constraint = "[[*],[]]" ##Here [*] represents one branch with any list of vertices
-    HSCPM2.condition =None
-    HSCPM2.conditionDescription =None
-    HSCPM2.finalState = ['MET','HSCP']
-    HSCPM2.massConstraints = None
-    HSCPM2.dataUrl = None
-    HSCPM2.source = 'SModelS'
-    #+++++++ next mass plane block ++++++++++++++
-    plane = HSCPM2.addMassPlane([['*'],[x]]) ##Here ['*'] represents a mass array with any length
-    plane.setSources(dataLabels= ['efficiencyMap'],dataFiles=['orig/eff_HSCPM2_'+name+'.txt'], dataFormats=['txt'])
-    #+++++++ next txName block ++++++++++++++
-    HSCPM4 = dataset.addTxName('THSCPM4')
-    HSCPM4.checked =''
-    HSCPM4.constraint = "[[*],[['*']]]" ##Here [*] represents one branch with any list of vertices
-    HSCPM4.condition =None
-    HSCPM4.conditionDescription =None
-    HSCPM4.finalState = ['MET','HSCP']
-    HSCPM4.massConstraints = None
-    HSCPM4.dataUrl = None
-    HSCPM4.source = 'SModelS'
-    #+++++++ next mass plane block ++++++++++++++
-    plane = HSCPM4.addMassPlane([['*'],[x,y]]) ##Here ['*'] represents a mass array with any length
-    plane.setSources(dataLabels= ['efficiencyMap'],dataFiles=['orig/eff_HSCPM4_'+name+'.txt'], dataFormats=['txt'])
-    #+++++++ next txName block ++++++++++++++
-    HSCPM6 = dataset.addTxName('THSCPM6')
-    HSCPM6.checked =''
-    HSCPM6.constraint = "[[*],[['*'],[ '*']]]" ##Here [*] represents one branch with any list of vertices
-    HSCPM6.condition =None
-    HSCPM6.conditionDescription =None
-    HSCPM6.finalState = ['MET','HSCP']
-    HSCPM6.massConstraints = None
-    HSCPM6.dataUrl = None
-    HSCPM6.source = 'SModelS'
-    #+++++++ next mass plane block ++++++++++++++
-    plane = HSCPM6.addMassPlane([['*'],[x,y,z]]) ##Here ['*'] represents a mass array with any length
-    plane.setSources(dataLabels= ['efficiencyMap'],dataFiles=['orig/eff_HSCPM6_'+name+'.txt'], dataFormats=['txt'])
-
+    for tx in txnames:
+        #+++++++ next txName block ++++++++++++++
+        Txname = dataset.addTxName(tx)
+        Txname.checked =''
+        Txname.constraint = txnames[tx]['constraint']
+        Txname.condition =None
+        Txname.finalState = txnames[tx]['finalState']
+        Txname.massConstraints = None
+        Txname.dataUrl = None
+        Txname.source = 'SModelS'
+        Txname.setParticlesFromFile(particlesFile)
+        #+++++++ next mass plane block ++++++++++++++
+        plane = Txname.addMassPlane(txnames[tx]['massPlane'])
+        if not (tx in ['THSCPM1b','THSCPM2b','THSCPM3','THSCPM4','THSCPM8','THSCPM9']):
+            dataFile = 'orig/%s_efficiencyMaps_%s_noWidth.dat' %(tx,name) #Use maps without width info
+        else:
+            dataFile = 'orig/%s_efficiencyMaps_%s_trimmedwidth.dat' %(tx,name)
+        plane.addSource(dataLabel = 'efficiencyMap',
+                    dataFile = dataFile,
+                    coordinateMap = txnames[tx]['coordinates'],
+                    dataFormat = 'txt')
+        #++++++ exclusion (only for THSCPM1b) ++++
+        if tx == 'THSCPM1b':
+            plane.addSource(dataLabel='obsExclusion',dataFile='orig/Stau_ExclusionObs.csv',
+                            coordinateMap = {x : 0, y: 1, 'value' : None}, dataFormat = 'csv')
 
 databaseCreator.create()
-
