574f7f0c5dae70f5451b5f4ef38eeb46deb79191
Andre Lessa <lessa.a.p@gmail.com>
2022-07-18 15:33:25 -0300

    Coverage fixes. FIXED unit tests

--- smodels/tools/coverage.py
+++ smodels/tools/coverage.py
@@ -11,20 +11,20 @@ from smodels.tools.physicsUnits import fb
 from smodels.experiment.reweighting import reweightFactorFor
 from bisect import bisect
 from smodels.theory.exceptions import SModelSTheoryError as SModelSError
-from smodels.theory.element import Element
+from smodels.theory.theorySMS import TheorySMS
 from smodels.experiment.defaultFinalStates import (WList, lList, tList, taList, nuList, jetList,
                                                    MET, HSCP, RHadronG, RHadronQ)
 
 
 #  Default definitions for the uncovered topology categories/groups:
-#  Element filters for each group:
-#  (it should be a function which takes an Element object as input
-#  and returns True if the element belongs to the group and False otherwise)
-filtersDefault = {'missing (prompt)': lambda el: not ('prompt' in el.coveredBy),
-                  'missing (displaced)': lambda el: not ('displaced' in el.coveredBy),
+#  SMS filters for each group:
+#  (it should be a function which takes an SMS object as input
+#  and returns True if the SMS belongs to the group and False otherwise)
+filtersDefault = {'missing (prompt)': lambda sms: not ('prompt' in sms.coveredBy),
+                  'missing (displaced)': lambda sms: not ('displaced' in sms.coveredBy),
                   # 'missing (long cascade)' : lambda el: (not el.coveredBy) and el._getLength() > 3,
-                  'missing (all)': lambda el: (not el.coveredBy),
-                  'outsideGrid (all)': lambda el: (el.coveredBy and not el.testedBy)}
+                  'missing (all)': lambda sms: (not sms.coveredBy),
+                  'outsideGrid (all)': lambda sms: (sms.coveredBy and not sms.testedBy)}
 
 # Description for each group (optional and only used for printing):
 # (if not defined, the group label will be used instead)
@@ -41,8 +41,8 @@ bsmDefault = [MET, HSCP, RHadronG, RHadronQ]
 smDefault = [WList, lList, tList, taList, nuList, jetList]
 
 # Weight factors for each group:
-# (it should be a function which takes an Element object as input
-# and returns the reweighting factor to be applied to the element weight. It is relevant if only
+# (it should be a function which takes an SMS object as input
+# and returns the reweighting factor to be applied to the SMS weight. It is relevant if only
 # the fraction of the weight going into prompt or displaced decays is required)
 factorsDefault = {}
 for key in filtersDefault:
@@ -74,21 +74,21 @@ class Uncovered(object):
         """
         Inititalize the object.
 
-        :param topDict: TopologyDict object used to select elements from.
+        :param topDict: TopologyDict object used to select SMS from.
         :param sqrts: Value (with units) for the center of mass energy used to compute the missing cross sections.
                      If not specified the largest value available will be used.
         :param sigmacut: Minimum cross-section/weight value (after applying the reweight factor)
-                       for an element to be included. The value should in fb (unitless)
+                       for an SMS to be included. The value should in fb (unitless)
         :param groupFilters: Dictionary containing the groups' labels and the method for selecting
-                            elements.
+                            SMS.
         :param groupFactors: Dictionary containing the groups' labels and the method for reweighting
                             cross sections.
         :param groupdDescriptions: Dictionary containing the groups' labels and strings describing the group
                                   (used for printout)
         :param smFinalStates: List of (inclusive) Particle or MultiParticle objects used for grouping SM
-                              particles when creating FinalStateElements.
+                              particles when creating FinalStateSMS.
         :param bsmFinalSates: List of (inclusive) Particle or MultiParticle objects used for grouping
-                              BSM particles when creating FinalStateElements.
+                              BSM particles when creating FinalStateSMS.
         """
 
         # Sanity checks:
@@ -106,19 +106,19 @@ class Uncovered(object):
         else:
             sqrts = sqrts
 
-        # Store the relevant element cross-sections to improve performance:
-        for el in topDict.getElements():
-            xsec = el.weightList.getXsecsFor(sqrts)
+        # Store the relevant SMS cross-sections to improve performance:
+        for sms in topDict.getSMSList():
+            xsec = sms.weightList.getXsecsFor(sqrts)
             if xsec:
-                el._totalXsec = xsec[0].value.asNumber(fb)
+                sms._totalXsec = xsec[0].value.asNumber(fb)
             else:
-                el._totalXsec = 0.
+                sms._totalXsec = 0.
 
         self.groups = []
         # Create each uncovered group and get the topologies from topDict
         for gLabel, gFilter in groupFilters.items():
             # Initialize the uncovered topology list:
-            uncoveredTopos = UncoveredGroup(label=gLabel, elementFilter=gFilter,
+            uncoveredTopos = UncoveredGroup(label=gLabel, smsFilter=gFilter,
                                             reweightFactor=groupFactors[gLabel],
                                             smFinalStates=smFinalStates,
                                             bsmFinalStates=bsmFinalStates,
@@ -127,8 +127,8 @@ class Uncovered(object):
                 uncoveredTopos.description = groupdDescriptions[gLabel]
             else:
                 uncoveredTopos.description = gLabel
-            # Fill the list with the elements in topDict:
-            uncoveredTopos.getElementsFrom(topDict)
+            # Fill the list with the SMS in topDict:
+            uncoveredTopos.getSMSFrom(topDict)
             self.groups.append(uncoveredTopos)
 
     def getGroup(self, label):
@@ -149,18 +149,18 @@ class Uncovered(object):
 
 class UncoveredGroup(object):
     """
-    Holds information about a single coverage group: criteria for selecting and grouping elements,
+    Holds information about a single coverage group: criteria for selecting and grouping SMS,
     function for reweighting cross sections, etc.
     """
 
-    def __init__(self, label, elementFilter, reweightFactor,
+    def __init__(self, label, smsFilter, reweightFactor,
                  smFinalStates, bsmFinalStates, sqrts, sigmacut=0.):
         """
         :param label: Group label
-        :param elementFilter: Function which takes an element as argument and returns True (False) if
-                             the element should (not) be selected.
-        :param reweightFactor: Function which takes an element as argument and returns the reweighting
-                              factor to be applied to the element weight.
+        :param smsFilter: Function which takes an SMS as argument and returns True (False) if
+                             the SMS should (not) be selected.
+        :param reweightFactor: Function which takes an SMS as argument and returns the reweighting
+                              factor to be applied to the SMS weight.
         :param smFinalStates: List of Particle/MultiParticle objects used to group Z2-even particles appearing
                             in the final state
         :param bsmFinalStates: List of Particle/MultiParticle objects used to group Z2-odd particles appearing
@@ -168,16 +168,16 @@ class UncoveredGroup(object):
         :param sqrts: Value (with units) for the center of mass energy used to compute the missing cross sections.
                      If not specified the largest value available will be used.
         :param sigmacut: Minimum cross-section/weight value (after applying the reweight factor)
-                       for an element to be included. The value should in fb (unitless)
+                       for an SMS to be included. The value should in fb (unitless)
         """
 
-        self.finalStateElements = []
+        self.finalStateSMS = []
         self.smFinalStates = smFinalStates
         self.bsmFinalStates = bsmFinalStates
         self.sqrts = sqrts
         self.sigmacut = sigmacut
         self.label = label
-        self.elementFilter = elementFilter
+        self.smsFilter = smsFilter
         self.reweightFactor = reweightFactor
 
     def __str__(self):
@@ -186,83 +186,83 @@ class UncoveredGroup(object):
     def __repr__(self):
         return str(self)
 
-    def getElementsFrom(self, topDict):
+    def getSMSFrom(self, topDict):
         """
-        Select the elements from topDict according to self.elementFilter
-        and build FinalStateElements from the selected elements.
-        The FinalStateElements weights corresponds to the missing cross-section
-        with double counting from compressed elements already accounted for.
+        Select the SMS from topDict according to self.smsFilter
+        and build FinalStateSMS from the selected SMS.
+        The FinalStateSMS weights corresponds to the missing cross-section
+        with double counting from compressed SMS already accounted for.
         """
 
-        # First select all elements according to the filter (type of uncovered/missing topology):
-        elementList = [el for el in topDict.getElements() if self.elementFilter(el)]
+        # First select all SMS according to the filter (type of uncovered/missing topology):
+        smsList = [sms for sms in topDict.getSMSList() if self.smsFilter(sms)]
         # Get missing xsections including the reweight factor:
-        missingXandEls = [(self.getMissingX(el)*self.reweightFactor(el), el) for el in elementList]
-        # Only keep the elements above sigmacut:
-        missingXandEls = [x for x in missingXandEls[:] if x[0] > self.sigmacut]
+        missingXandSMS = [(self.getMissingX(sms)*self.reweightFactor(sms), sms) for sms in smsList]
+        # Only keep the SMS above sigmacut:
+        missingXandSMS = [x for x in missingXandSMS[:] if x[0] > self.sigmacut]
         # Sort according to largest missingX, smallest size and largest ID
-        missingXandEls = sorted(missingXandEls, key=lambda pt: (pt[0], -pt[1].canonName, -pt[1].elID), reverse=True)
+        missingXandSMS = sorted(missingXandSMS, key=lambda pt: (pt[0], -pt[1].canonName, -pt[1].smsID), reverse=True)
 
-        # Split lists of elements and missingX:
-        missingXsecs = [pt[0] for pt in missingXandEls]
-        elementList = [pt[1] for pt in missingXandEls]
+        # Split lists of SMS and missingX:
+        missingXsecs = [pt[0] for pt in missingXandSMS]
+        smsList = [pt[1] for pt in missingXandSMS]
 
-        # Remove all elements which are related to each other in order to avoid double counting
+        # Remove all SMS which are related to each other in order to avoid double counting
         # (keep always the first appearance in the list, so we always keep the ones with largest missing xsec)
-        elementListUnique = []
+        smsListUnique = []
         missingXsecsUnique = []
-        ancestors = set()  # Keep track of all the ancestor ids of the elements in the unique list
-        for i, element in enumerate(elementList):
+        ancestors = set()  # Keep track of all the ancestor ids of the SMS in the unique list
+        for isms, sms in enumerate(smsList):
             # Get ancestor object ids
-            # (safer than element.elID since some elements can share elID = 0 if they
+            # (safer than sms.smsID since some SMS can share smsID = 0 if they
             # were never directly inserted in the TopologyList)
-            ancestorsIDs = set([id(element)] + [id(el) for el in element.getAncestors()])
-            # If the element has any common ancestor with any of the previous elements
-            # or if it is an ancestor of any of the previous elements
+            ancestorsIDs = set([id(sms)] + [id(sms) for sms in sms.getAncestors()])
+            # If the SMS has any common ancestor with any of the previous SMS
+            # or if it is an ancestor of any of the previous SMS
             # skip it to avoid double counting
             if ancestors.intersection(ancestorsIDs):
                 continue
-            elementListUnique.append(element)
-            missingXsecsUnique.append(missingXsecs[i])
+            smsListUnique.append(sms)
+            missingXsecsUnique.append(missingXsecs[isms])
             ancestors = ancestors.union(ancestorsIDs)
 
-        # Now that we only have unique elements with their effective missing cross-sections
-        # we create General Elements out of them
-        for iel, el in enumerate(elementListUnique):
-            missingX = missingXsecsUnique[iel]
+        # Now that we only have unique SMS with their effective missing cross-sections
+        # we create General SMS out of them
+        for isms, sms in enumerate(smsListUnique):
+            missingX = missingXsecsUnique[isms]
             if not missingX:
                 continue
-            self.addToFSElements(el, missingX)
+            self.addToFinalStateSMS(sms, missingX)
 
-        # Finally sort general elements by their missing cross-section:
-        self.finalStateElements = sorted(self.finalStateElements, key=lambda fsEl: fsEl.missingX, reverse=True)
+        # Finally sort general SMS by their missing cross-section:
+        self.finalStateSMS = sorted(self.finalStateSMS, key=lambda fsEl: fsEl.missingX, reverse=True)
 
-    def getMissingX(self, element):
+    def getMissingX(self, sms):
         """
-        Calculate total missing cross section of an element, by recursively checking if its
+        Calculate total missing cross section of an SMS, by recursively checking if its
         mothers already appear in the list.
-        :param element: Element object
+        :param sms: SMS object
 
         :returns: missing cross section without units (in fb)
         """
 
-        ancestorList = element.getAncestors()
-        alreadyChecked = []  # keep track of which elements have already been checked
-        # Get the (pre-loaded) total element weight in fb:
-        missingX = element._totalXsec
+        ancestorList = sms.getAncestors()
+        alreadyChecked = []  # keep track of which SMS have already been checked
+        # Get the (pre-loaded) total SMS weight in fb:
+        missingX = sms._totalXsec
         if not missingX:
             return 0.
         overlapXsec = 0.
         for ancestor in ancestorList:  # check all ancestors (ancestorList is sorted by generation)
-            # Skip entries which correspond to the element itself
-            if ancestor is element:
+            # Skip entries which correspond to the SMS itself
+            if ancestor is sms:
                 continue
             # Make sure we do not subtract the same mother twice
-            if any(ancestor is el for el in alreadyChecked):
+            if any(ancestor is smsB for smsB in alreadyChecked):
                 continue
             alreadyChecked.append(ancestor)
             # Check if ancestor passes the group filter (if it has been covered/tested or not):
-            if self.elementFilter(ancestor):
+            if self.smsFilter(ancestor):
                 continue
             # Subtract the weight of the ancestor and skip checking for all the older family tree of the ancestor
             # (avoid subtracting the same weightList twice from mother and grandmother for instance)
@@ -284,65 +284,74 @@ class UncoveredGroup(object):
         xsec = 0.
         if not sqrts:
             sqrts = self.sqrts
-        for fsEl in self.finalStateElements:
+        for fsEl in self.finalStateSMS:
             xsec += fsEl.missingX
         return xsec
 
-    def addToFSElements(self, el, missingX):
+    def addToFinalStateSMS(self, sms, missingX):
         """
-        Adds an element to the list of missing topologies = final state elements.
-        If the element contributes to an element that is already in the list, add element and weight to
-        the element.
-        :parameter el: element to be added
-        :parameter missingX: missing cross-section for the element (in fb)
+        Adds an SMS to the list of missing topologies = final state SMS.
+        If the SMS contributes to an SMS that is already in the list, add SMS and weight to
+        the SMS.
+        :parameter sms: SMS object to be added
+        :parameter missingX: missing cross-section for the SMS (in fb)
         """
 
-        newGenEl = FinalStateElement(el, missingX, self.smFinalStates, self.bsmFinalStates)
+        newGenEl = FinalStateSMS(sms, missingX, self.smFinalStates, self.bsmFinalStates)
 
-        index = bisect(self.finalStateElements, newGenEl)
-        if index != len(self.finalStateElements) and self.finalStateElements[index] == newGenEl:
-            self.finalStateElements[index]._contributingElements.append(el)
-            self.finalStateElements[index].missingX += missingX
+        index = bisect(self.finalStateSMS, newGenEl)
+        if index != len(self.finalStateSMS) and self.finalStateSMS[index] == newGenEl:
+            self.finalStateSMS[index]._contributingSMS.append(sms)
+            self.finalStateSMS[index].missingX += missingX
         else:
-            self.finalStateElements.insert(index, newGenEl)
+            self.finalStateSMS.insert(index, newGenEl)
 
 
-class FinalStateElement(Element):
+class FinalStateSMS(TheorySMS):
     """
-    This class represents a simplified element which does
+    This class represents a simplified SMS which does
     only holds information about the final states. It holds a simple
     tree with one root (PV), having the final state nodes as its
     daughters.
     """
 
-    def __new__(self, el, missingX=None,
+    def __new__(self, sms, missingX=None,
                 smFinalStates=smDefault,
                 bsmFinalStates=bsmDefault):
 
         if missingX is None:
-            missingX = el.weightList.getMaxXsec()
+            missingX = sms.weightList.getMaxXsec()
 
-        # Get an element holding only the final states
-        finalStatesEl = el.compressToFinalStates()
-        finalStatesEl.missingX = missingX
+        # Get an SMS holding only the final states
+        finalStateSMS = sms.compressToFinalStates()
+        finalStateSMS.missingX = missingX
         # Replace particles by inclusive particles, if possible:
-        for node in finalStatesEl.tree.nodes:
-            if node == finalStatesEl.tree.root:
+        nodeDict = {}
+        for nodeIndex in finalStateSMS.nodeIndices:
+            if nodeIndex == finalStateSMS.rootIndex:
                 continue
+            node = finalStateSMS.indexToNode(nodeIndex)
             ptc = node.particle
+            newParticle = None
             if ptc.isSM:
                 for smFS in smFinalStates:
                     if ptc == smFS:
-                        node.particle = smFS
+                        newParticle = smFS
                         break
             else:
                 for bsmFS in bsmFinalStates:
                     if ptc == bsmFS:
-                        node.particle = bsmFS
+                        newParticle = bsmFS
                         break
+            if newParticle is not None:
+                newNode = node.copy()
+                newNode.particle = newParticle
+                nodeDict[nodeIndex] = newNode
 
-        finalStatesEl.tree.sort(force=True)
-        finalStatesEl.tree.setGlobalProperties()
-        finalStatesEl._contributingElements = [el]
+        finalStateSMS.updateNodeObjects(nodeDict)
 
-        return finalStatesEl
+        finalStateSMS.sort(force=True)
+        finalStateSMS.setGlobalProperties(weight=False)
+        finalStateSMS._contributingSMS = [sms]
+
+        return finalStateSMS
