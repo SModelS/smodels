33e7b1e5717eac96b23fffb99226b4f96fb08ba9
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2022-09-30 13:17:01 +0200

    dy

--- cpp/find_libpython.py
+++ cpp/find_libpython.py
@@ -5,6 +5,7 @@ def find():
         import sysconfig
         ldlib = sysconfig.get_config_vars()["LDLIBRARY"]
         ldlib = ldlib.replace("lib","").replace(".so","").replace(".dll","")
+        ldlib = ldlib.replace(".dy","")
         print ( f"-l{ldlib}" )
     except Exception as e:
         import sys
