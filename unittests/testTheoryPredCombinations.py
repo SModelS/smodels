daa2df568d9995836178e3fd66ba1a11cdafc36c
Andre Lessa <lessa.a.p@gmail.com>
2022-10-21 13:54:00 -0300

    Added unittestextra pickle

--- unittests/testTheoryPredCombinations.py
+++ unittests/testTheoryPredCombinations.py
@@ -163,7 +163,7 @@ class CombinedTheoryPredsTest(unittest.TestCase):
           'CMS-SUS-12-028',
           'ATLAS-SUSY-2018-12',
           'ATLAS-SUSY-2016-15','ATLAS-SUSY-2019-09']
-        db = Database('unittest+./database_extra')
+        db = Database('unittest+unittestextra')
         slhafile = "testFiles/slha/gluino_squarks.slha"
         exp_results = db.getExpResults()
         model = Model(BSMparticles=BSMList, SMparticles=SMList)
