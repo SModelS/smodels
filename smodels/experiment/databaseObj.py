534ba3dd72e33ab24ef52962dbbd6198aeea0810
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-02-22 17:11:49 +0100

    when merging experimental results use globalInfo of the latter not the first

--- smodels/experiment/databaseObj.py
+++ smodels/experiment/databaseObj.py
@@ -120,6 +120,7 @@ class Database(object):
     def mergeERs ( self, o1, r2 ):
         """ merge the content of exp res r1 and r2 """
         r1 = copy.deepcopy ( o1 )
+        r1.globalInfo = r2.globalInfo
         dids = [ x.getID() for x in o1.datasets ]
         for ds in r2.datasets:
             if not ds.getID() in dids: ## completely new dataset
