02d3233a0b74da19a862b74303e3a46e4aac2afd
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-04-25 11:37:35 +0200

    debug with timestamp

--- test/debug.py
+++ test/debug.py
@@ -2,15 +2,15 @@
 .. module:: debug
    :synopsis: just a simple method to help with debugging
 
-.. moduleauthor:: Andre Lessa <lessa.a.p@gmail.com>
 .. moduleauthor:: Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
 
 """
 
 def printTo (  *args  ):
-    with open ( "check.txt", "at" ) as f:
+    import time
+    with open ( "debug.txt", "at" ) as f:
         # f.write ( text + "\n" )
         line = " ".join ( map ( str,  args ) )
         print ( line )
-        f.write ( line + "\n" )
+        f.write ( f"{time.asctime()}: {line}\n" )
         f.close()
