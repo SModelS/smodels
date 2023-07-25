1ea08fd38841cedbcfa2b6e536626b0f3cfaaee0
Supersymmetry <supersymmetry@calculator.myguest.virtualbox.org>
2023-07-25 13:51:37 +0200

    remove thing (again)

--- smodels/lib/resummino/smodels/lib/resummino/resummino-3.1.2/external/include/gsl_all.h
+++
@@ -1,9 +0,0 @@
-#include "gsl/gsl_blas.h"
-#include "gsl/gsl_integration.h"
-#include "gsl/gsl_math.h"
-#include "gsl/gsl_matrix.h"
-#include "gsl/gsl_monte_vegas.h"
-#include "gsl/gsl_multifit_nlin.h"
-#include "gsl/gsl_sf.h"
-#include "gsl/gsl_vector.h"
-#include <gsl/gsl_version.h>
--- smodels/lib/resummino/smodels/lib/resummino/resummino-3.1.2/external/lib/LoopTools-2.13/src/include/ltnames.h
+++
@@ -1,5 +0,0 @@
-	character*6 paraname(Pee,1:5)
-	common /ltparanames/ paraname
-
-	character*10 coeffname(Nee,1:5)
-	common /ltcoeffnames/ coeffname
