3683af467fa10c6a362692e37747261a19ec4f7a
Supersymmetry <supersymmetry@calculator.myguest.virtualbox.org>
2023-07-25 13:54:35 +0200

    delete all other things

--- smodels/lib/resummino/resummino-3.1.2/src/gaugino_squark/generated/h/ddb_UXub.h
+++
@@ -1,1 +0,0 @@
-#define TEST_SC_OSSUB13
--- smodels/lib/resummino/resummino-3.1.2/src/main.cc
+++
@@ -1,8 +0,0 @@
-#include "resummino.h"
-int main(int argc, char *argv[]) {
-  print_banner();
-  Args args;
-  parse_args(argc, argv, args);
-  Results r = compute(args);
-  print_results(r, args);
-}
