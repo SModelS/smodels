ca663c780e54a56756337ef547faf6f2bf3a2f57
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-09-13 15:51:10 +0200

    rephrased

--- smodels/etc/resummino.py
+++ smodels/etc/resummino.py
@@ -3,18 +3,19 @@
     # particles to consider for productions
     # names of keys are used for debugging only
     "channels" : {"1" : [1000023,1000025], "2" : [1000024,-1000024],
-                "3" : [1000023,1000023]
+                  "3" : [1000023,1000023]
     },
-    # mode: If you want to calculate all the channel you chose at every order,
+    # mode: If you want to calculate all the channels you choose at every order,
     # use 'all', if you want to check at LO before calculating NLO, use 'check'
-    # instead. The limit for the NLO calculation is determined by the
-    # 'mode_limit' variable, below this value (in pb), no NLO cross-section are
-    # calculated.
+    # instead. 
     "mode" : "check",
+    # The limit for the NLO calculation is determined by the 'mode_limit' variable, 
+    # below this value (in pb), no NLO cross-section are
+    # calculated.
     "mode_limit" : 0.00001,
-    # pdfs: use this to change the parton distribution function used for the
-    # cross-section calculation.
-    # We used PDFLHC2021 by default, but any pdf can be used. Be sure to check
+    # pdfs (case-sensitive): use this to change the parton distribution
+    # function used for the cross-section calculation.
+    # PDFLHC2021 is default, but any pdf can be used. Be sure to check
     # the name on the lhapdf website before putting any value here.
     "pdfs" : {
         "pdf_lo" : "cteq66",
