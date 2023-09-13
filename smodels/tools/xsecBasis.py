5654a8e9211430afb1f9fae71344292efcbe6618
Ryudoro <93795941+Ryudoro@users.noreply.github.com>
2023-09-13 20:55:15 +0200

    Update xsecBasis.py

--- smodels/tools/xsecBasis.py
+++ smodels/tools/xsecBasis.py
@@ -37,7 +37,7 @@ except ImportError as e:
 
 
 
-class XSecBasis:
+class XSecBase:
     """ cross section computer class, what else? """
     def __init__ ( self, maxOrder,slha_folder_name, maycompile=True):
         """
@@ -197,7 +197,7 @@ class ArgsStandardizer:
 
     def getjson ( self, args ):
         """ retrieve the path to the json file from argument list """
-        json = args.json
+        json = args.conf
 
         if json == 'default':
             return None
