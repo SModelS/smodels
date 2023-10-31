bd687bc1e25b5281482bae593eb1131072896ac7
Supersymmetry <supersymmetry@calculator.myguest.virtualbox.org>
2023-10-31 16:31:15 +0100

    change comments on resummino.py

--- smodels/etc/resummino.py
+++ smodels/etc/resummino.py
@@ -2,15 +2,17 @@
     # channels: values (as python lists) are the pdg codes of the mother
     # particles to consider for productions
     # names of keys are used for debugging only
-    "channels" : {"1" : [1000023,1000025], "2" : [1000024,-1000024],
-                  "3" : [1000023,1000023]
+    #Defaut channels here are for one of the Bino-like scenarios, with M_1 < M_2, mu
+    "channels" : {"1" : [1000023,1000024],"2" : [1000023, -1000024], "3" : [1000024,-1000024]
     },
+    # ------------------------------------ MODE PARAMETERS IS NOT USED IN THE CURRENT RESUMMINO-INTERFACE VERSION
     # mode: If you want to calculate all the channels you choose at every order,
     # use 'all', if you want to check at LO before calculating NLO, use 'check'
     # instead. 
     ### For now, we choose to check by default, if you want to calculate all, put xsec_limit to 0.
     #"mode" : "check", 
-    # The limit for the NLO calculation is determined by the 'mode_limit' variable, 
+    # -------------------------------------
+    # The limit for the NLO calculation is determined by the 'xsec_limit' variable, 
     # below this value (in pb), no NLO cross-section are
     # calculated.
     "xsec_limit" : 0.00001,
