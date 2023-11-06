1e38e297bd41f94c74355c68d79797f5500be4f4
Ryudoro <93795941+Ryudoro@users.noreply.github.com>
2023-11-06 12:18:52 +0100

    Update install.sh
    change install to check resummino and lhapdf existence

--- smodels/lib/resummino/install.sh
+++ smodels/lib/resummino/install.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-
 install_dir=$PWD
 LHAPDF_VERSION="6.5.4"
 RESUMMINO_VERSION="3.1.2"
 
-
 mkdir -p $install_dir
-
 cd $install_dir
-
 
 get_cpu_cores() {
   local num_cores=$(nproc)
@@ -22,31 +18,31 @@ get_cpu_cores() {
 
 num_cores_to_use=$(get_cpu_cores)
 
+# Vérification de l'existence de LHAPDF
+if [ ! -d "$install_dir/lhapdf" ]; then
+    wget https://lhapdf.hepforge.org/downloads/?f=LHAPDF-$LHAPDF_VERSION.tar.gz -O "LHAPDF-$LHAPDF_VERSION.tar.gz"
+    tar xf LHAPDF-$LHAPDF_VERSION.tar.gz
+    cd LHAPDF-$LHAPDF_VERSION
+    ./configure --prefix=$install_dir/lhapdf --disable-python
+    make -j"$num_cores_to_use"
+    make install
+    cd ..
+    cd ..
+    wget http://lhapdfsets.web.cern.ch/lhapdfsets/current/PDF4LHC21_40.tar.gz -O- | tar xz -C $install_dir/lhapdf/share/LHAPDF
+    cd $install_dir
+fi
 
-
-wget https://lhapdf.hepforge.org/downloads/?f=LHAPDF-$LHAPDF_VERSION.tar.gz -O "LHAPDF-$LHAPDF_VERSION.tar.gz"
-tar xf LHAPDF-$LHAPDF_VERSION.tar.gz
-cd LHAPDF-$LHAPDF_VERSION
-./configure --prefix=$install_dir/lhapdf --disable-python
-make -j"$num_cores_to_use"
-make install
-cd ..
-cd ..
-
-wget http://lhapdfsets.web.cern.ch/lhapdfsets/current/PDF4LHC21_40.tar.gz -O- | tar xz -C $install_dir/lhapdf/share/LHAPDF
-cd $install_dir
-
-wget https://resummino.hepforge.org/downloads/?f=resummino-$RESUMMINO_VERSION.zip -O resummino-$RESUMMINO_VERSION.zip
-
-
-unzip resummino-$RESUMMINO_VERSION.zip
-mkdir -p resummino_install
-
-cd resummino-$RESUMMINO_VERSION
-cmake . -DLHAPDF=$install_dir/lhapdf -DCMAKE_INSTALL_PREFIX=$install_dir/resummino_install
-make -j"$num_cores_to_use"
-make install
-cd ..
+# Vérification de l'existence de RESUMMINO
+if [ ! -d "$install_dir/resummino_install" ]; then
+    wget https://resummino.hepforge.org/downloads/?f=resummino-$RESUMMINO_VERSION.zip -O resummino-$RESUMMINO_VERSION.zip
+    unzip resummino-$RESUMMINO_VERSION.zip
+    mkdir -p resummino_install
+    cd resummino-$RESUMMINO_VERSION
+    cmake . -DLHAPDF=$install_dir/lhapdf -DCMAKE_INSTALL_PREFIX=$install_dir/resummino_install
+    make -j"$num_cores_to_use"
+    make install
+    cd ..
+fi
 
 echo "LHAPDF_version = $LHAPDF_VERSION" > versions.txt
 echo "resummino_version = $RESUMMINO_VERSION" >> versions.txt
