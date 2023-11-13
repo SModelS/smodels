bb06803832bce9650a2f6d0b07d08fd998b7cb1d
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-11-13 10:53:54 +0100

    remove echo statement

--- smodels/lib/resummino/clean.sh
+++ smodels/lib/resummino/clean.sh
@@ -5,7 +5,7 @@ install_dir=$PWD
 cd $install_dir
 
 LINES=$(ls | grep -v '\.sh$' | grep -v Makefile)
-echo "lines x${LINES}x"
+# echo "lines x${LINES}x"
 [ -z "$LINES" ] || {
     echo $LINES | xargs rm -r;
 }
