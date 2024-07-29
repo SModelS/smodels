68c792595c2383ec1ce0391ea584eab09e0ef633
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-07-29 18:21:45 +0200

    filter_deprecation

--- unittests/testPyhf.py
+++ unittests/testPyhf.py
@@ -15,6 +15,7 @@ import json
 import jsonpatch
 from smodels.statistics.pyhfInterface import PyhfData, PyhfUpperLimitComputer, pyhf
 from smodels.base.smodelsLogging import logger
+import warnings
 
 class PyhfTest(unittest.TestCase):
 
@@ -374,6 +375,7 @@ class PyhfTest(unittest.TestCase):
         import os
         from smodels.decomposition import decomposer
         from smodels.matching.theoryPrediction import _getDataSetPredictions, _getCombinedResultFor, TheoryPredictionList
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         database = Database('./database/')
         runtime.modelFile = "smodels.share.models.mssm"
