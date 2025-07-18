cf67962c3259ad0d427991c27d82c7407698f3fc
Andre Lessa <lessa.a.p@gmail.com>
2025-07-18 18:32:25 -0300

    Fixed and extended testCaching

--- unittests/testCaching.py
+++ unittests/testCaching.py
@@ -12,15 +12,15 @@
 import sys
 sys.path.insert(0,"../")
 import unittest
-from smodels.tools.caching import Cache
+from smodels.tools.caching import roundCache
 from smodels.base.physicsUnits import GeV, fb
 from databaseLoader import database
 import numpy as np
 
 class CachingTest(unittest.TestCase):
+
     def testCache(self):
-        Cache.n_stored = 10
-        Cache.reset()
+
         database.selectExpResults(analysisIDs=["ATLAS-SUSY-2013-05"], 
                     datasetIDs=[None], txnames=["T2bb" ] )
         expRes = database.expResultList
@@ -31,11 +31,29 @@ class CachingTest(unittest.TestCase):
         for masses in massesvec:
             txname.txnameData.getValueFor( np.array([masses,masses]).flatten())
 
-        self.assertEqual ( Cache.size(), 7 )
         m = [ 270.0, 100.0, 270.0, 100.0 ]
         result=txname.txnameData.getValueFor(m)
         result = result*txname.y_unit
         self.assertAlmostEqual(result.asNumber(fb) , 459.658) 
+
+    def testRoundCache(self):
+
+        @roundCache(argname='x',argpos=0,digits=1)
+        def dummyFunc(x, y):
+            return x*y
+                        
+        self.assertEqual(dummyFunc(1.234,1.0),1.2,3)
+        self.assertEqual(dummyFunc(1.234,y=1.0),1.2,3)
+        self.assertEqual(dummyFunc(x=1.234,y=1.0),1.2,3)
+        self.assertEqual(dummyFunc(1.29,1.0),1.3,3)
+
+        @roundCache(argname='y',argpos=1,digits=2)
+        def dummyFunc2(x, y):
+            return x*y
+        
+        self.assertEqual(dummyFunc2(2.,1.2344),2.46,3)
+        self.assertEqual(dummyFunc2(x=2.0,y=1.299),2.6,3)
+
 
 if __name__ == "__main__":
     unittest.main()
