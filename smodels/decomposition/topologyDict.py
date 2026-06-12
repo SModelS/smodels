778b716ca9500e3e274f9cb0229e408335586bfd
Andre Lessa <lessa.a.p@gmail.com>
2026-06-12 10:50:20 -0300

    Removed unused appendSMS

--- smodels/decomposition/topologyDict.py
+++ smodels/decomposition/topologyDict.py
@@ -22,34 +22,6 @@ class TopologyDict(OrderedDict):
 
         self.__dict__ = {}
         self._keysDict = {}
-
-    def appendSMS(self, newSMS):
-
-        if not isinstance(newSMS, TheorySMS):
-            return False
-        
-        canonName = newSMS.canonName
-        key = newSMS.treeToString(removeIndicesFrom='all')
-        if canonName not in self:
-            self[canonName] = [newSMS]
-            self._keysDict[canonName] = [key]
-        else:
-            smsList = self[canonName]
-            keysList = self._keysDict[canonName]
-            if key in keysList:
-                index = keysList.index(key)
-                smsList[index].prodXSec = smsList[index].prodXSec + newSMS.prodXSec
-                smsList[index].ancestors = smsList[index].ancestors[:] + newSMS.ancestors[:]
-                smsList[index].weightList = smsList[index].weightList + newSMS.weightList
-                smsList[index].maxWeight = smsList[index].maxWeight + newSMS.maxWeight
-                smsList[index].decayBRs = None
-            else:
-                smsList.append(newSMS)
-                keysList.append(key)
-            self[canonName] = smsList[:]
-            self._keysDict[canonName] = keysList[:]
-            
-        return True
         
     def addSMS(self, newSMS):
 
