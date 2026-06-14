03f5e3299ce9677e7255afefc202138decb7ee99
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-06-14 20:47:10 +0200

    set logger

--- unittests/testAdditiveDB.py
+++ unittests/testAdditiveDB.py
@@ -12,12 +12,12 @@ import sys
 sys.path.insert(0,"../")
 from smodels.experiment.databaseObj import Database
 import unittest
-import logging.config
 import os
 
 class AdditiveDBTest(unittest.TestCase):
     # use different logging config for the unit tests.
-    # logging.config.fileConfig( "./logging.conf" )
+    import logging.config
+    logging.config.fileConfig( "./logging.conf" )
     from smodels.base.smodelsLogging import logger
 
     def testAddingSubset(self):
