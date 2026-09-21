13697d59ab96996e8b2838832912b7116153c0b4
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-09-21 15:01:04 +0200

    allow for bigger deviation for nll0

--- smodels/statistics/nnInterface.py
+++ smodels/statistics/nnInterface.py
@@ -220,7 +220,7 @@ class NNUpperLimitComputer:
         """ when getting predictions for bkg_yields (SRs) and obs_yields (CRs),
         nll_*_mu0 == nll_*_mu1. check for this.
         """
-        tolerance = 1e-2 # FIXME eventually we need to lower this number
+        tolerance = 3e-2 # FIXME eventually we need to lower this number
         nlls = self._actual_nll ( poi_test = 0. )
         errors = {}
         for label in [ "nll_exp", "nll_obs", "nllA_exp", "nllA_obs" ]:
